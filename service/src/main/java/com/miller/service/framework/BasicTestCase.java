e3f9b2fb8c73416f043cdd8105ba7b062446abaa
Miller Shan <miller.shan@aliyun.com>
2023-10-18 14:29:06 +0800

    feat: 测试框架基类

---
+++ service/src/main/java/com/miller/service/framework/BasicTestCase.java
@@ -0,0 +1,19 @@
+package com.miller.service.framework;
+
+import com.miller.service.framework.annotation.TestFramework;
+
+/**
+ * 测试用例基类
+ *
+ * <p>
+ * 所有测试用例的父类.可以使用{@link TestFramework @TestFramework}替代。
+ * </p>
+ *
+ * @author Miller Shan
+ * @version 1.0
+ * @since 2023/10/18 11:01:01
+ */
+@TestFramework
+@Deprecated
+public class BasicTestCase {
+}
