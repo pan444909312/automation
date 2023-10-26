ffd8a1f910b334698b86224c20a29f8286d56dd8
Miller Shan <miller.shan@aliyun.com>
2023-10-26 16:48:11 +0800

    feat: 数据库操作工具

---
+++ service/src/main/java/com/miller/service/framework/db/DBUtils.java
@@ -0,0 +1,138 @@
+package com.miller.service.framework.db;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.lang.Nullable;
+
+import javax.sql.DataSource;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 数据库操作工具，使用方法参考单元测试.
+ *
+ * @author Miller Shan
+ * @version 1.0.0
+ * @since 2023/10/25 20:03:44
+ */
+@Slf4j
+public class DBUtils {
+    private DataSource dataSource;
+    /**
+     * 对 Spring 的 {@link org.springframework.jdbc.core.JdbcTemplate}进行继承，保留了原始功能,并对外开放。
+     */
+    private JdbcTemplate jdbcTemplate;
+
+    public DBUtils() {
+    }
+
+    /**
+     * @see #getDataSourceByDruid(String, String, String)
+     */
+    public DBUtils(String mySqlUrl, String userName, String passWord) {
+        getDataSourceByDruid(mySqlUrl, userName, passWord);
+    }
+
+    /**
+     * @see JdbcUtilsByDruid#getDataSource(String, String, String)
+     */
+    private DataSource getDataSourceByDruid(String mySqlUrl, String userName, String passWord) {
+        // 使用 Druid 实现
+        JdbcUtilsByDruid jdbcUtilsByDruid = new JdbcUtilsByDruid();
+        dataSource = jdbcUtilsByDruid.getDataSource(mySqlUrl, userName, passWord);
+        jdbcTemplate = new JdbcTemplate(dataSource);
+
+        return dataSource;
+    }
+
+    public JdbcTemplate getJdbcTemplate() {
+        return jdbcTemplate;
+    }
+
+    /**
+     * 执行 sql 语句，返回影响的记录行数
+     *
+     * @param sql  sql语句
+     * @param args sql语句中使用占位符?替代的参数
+     * @return 返回影响的记录行
+     */
+    public Integer executeInsertOrUpdateOrDelete(String sql, Object... args) {
+        int update = jdbcTemplate.update(sql, args);
+        return update;
+    }
+
+    /**
+     * 查询一条记录，返回一个Java Bean对象
+     *
+     * @param sql        待执行的sql语句
+     * @param returnType 入参的类型，单独的“T”表示限制传入的参数类型
+     * @param args       sql语句中的参数列表
+     * @param <T>        占位符 <T> T 表示“返回值”是一个泛型，传入什么类型，就返回什么类型。<T>表示是泛型。
+     * @return 表示返回的是T类型的数据, 如果无数据则返回 null
+     * @throws DataAccessException
+     */
+    public <T> T queryOneObjectReturnObject(String sql, Class<T> returnType, @Nullable Object... args) throws DataAccessException {
+        T t = null;
+        try {
+            t = jdbcTemplate.queryForObject(sql, new BeanPropertyRowMapper<>(returnType), args);
+        } catch (EmptyResultDataAccessException emptyResultDataAccessException) {
+            //emptyResultDataAccessException.printStackTrace();
+            log.warn("查询结果小于1. {}", emptyResultDataAccessException.getMessage());
+        }
+        return t;
+    }
+
+    /**
+     * 查询多条记录，返回列表
+     *
+     * @param sql        待执行的sql语句
+     * @param returnType 入参的类型，单独的“T”表示限制传入的参数类型
+     * @param args       sql语句中的参数列表
+     * @param <T>        占位符 <T> T 表示“返回值”是一个泛型，传入什么类型，就返回什么类型。<T>表示是泛型。
+     * @return 表示返回的是T类型的数据
+     * @throws DataAccessException
+     */
+    public <T> List<T> queryMultiObjectReturnList(String sql, Class<T> returnType, @Nullable Object... args) throws DataAccessException {
+        List<T> queryForList = jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(returnType), args);
+        return queryForList;
+    }
+
+    /**
+     * 查询一条记录，返回一个Map
+     *
+     * @param sql  待执行的sql语句
+     * @param args sql语句中的参数列表
+     * @return 数据库中的列名、列值
+     */
+    public Map<String, Object> queryOneObjectReturnMap(String sql, @Nullable Object... args) {
+        Map<String, Object> columnAndValueMap = null;
+        try {
+            columnAndValueMap = jdbcTemplate.queryForMap(sql, args);
+        } catch (EmptyResultDataAccessException emptyResultDataAccessException) {
+            //emptyResultDataAccessException.printStackTrace();
+            log.warn("查询结果小于1. {}", emptyResultDataAccessException.getMessage());
+        }
+        return columnAndValueMap;
+    }
+
+    /**
+     * 转换 List 为 String，用于SQL语句中包含多个参数。不保证SQL注入问题。
+     * 比如: ... where id in (1, 2, "3", "4")
+     *
+     * @param ids
+     * @return 逗号分隔的字符串，如:'1', '2', 'a,', 'b'
+     */
+    public String transformListToString(List<String> ids) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (String element : ids) {
+            stringBuilder.append("'");
+            stringBuilder.append(element);
+            stringBuilder.append("',");
+        }
+        String result = stringBuilder.substring(0, stringBuilder.length() - 1);
+        return result;
+    }
+
+}
---
+++ service/src/test/java/com/miller/service/framework/db/DBUtilsTest.java
@@ -0,0 +1,149 @@
+package com.miller.service.framework.db;
+
+import com.miller.common.util.ULIDUtils;
+import com.miller.service.framework.dto.IssuesDTO;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.nullValue;
+
+/**
+ * 测试用例集
+ *
+ * @author Miller Shan
+ * @see DBUtils
+ */
+public class DBUtilsTest {
+    private static String mySqlUrl = "jdbc:mysql://121.43.119.63:3306/ct_dev";
+    private static String userName = "root";
+    private static String passWord = "autotest#123";
+    private static DBUtils dbUtils;
+
+    @BeforeAll
+    public static void beforeAll() {
+        // 初始化，链接数据库
+        dbUtils = new DBUtils(mySqlUrl, userName, passWord);
+    }
+
+    @DisplayName("Query multiple records from DB then return List")
+    @Test
+    public void testQueryMultiRecodes() {
+        // Given
+        String sql = "SELECT * FROM issues";
+        // When
+        List<IssuesDTO> issues = dbUtils.queryMultiObjectReturnList(sql, IssuesDTO.class);
+        // Then
+        // issues.forEach(System.out::println);
+        assertThat(issues, notNullValue());
+    }
+
+    @DisplayName("Query one recode from DB then return Java Bean")
+    @Test
+    public void testQueryOneRecodeByObject() {
+        // Given
+        String sql = "SELECT * FROM issues where issue_id = ?";
+        // When
+        IssuesDTO issuesDTO = dbUtils.queryOneObjectReturnObject(sql, IssuesDTO.class, "asdf");
+        // Then
+        assertThat(issuesDTO, nullValue());
+    }
+
+    @DisplayName("Query one recode from DB then return Map ")
+    @Test
+    public void testQueryOneRecodeByMap() {
+        // Given
+        String sql = "SELECT * FROM issues where issue_id = ?";
+        // When
+        Map<String, Object> columnAndValueMap = dbUtils.queryOneObjectReturnMap(sql, "asdf");
+        // Then
+        assertThat(columnAndValueMap, nullValue());
+    }
+
+    @DisplayName("Query multiple records from DB, but sql has multiple values and then return List")
+    @Test
+    public void testQueryMultiRecodesByList() {
+        // Given
+        List<String> ids = new ArrayList<>();
+        ids.add("3989b361-4e7d-4e0f-aba5-5986e6816f51");
+        ids.add("76b83db6-2e31-4a9d-8ce7-b0c780a90608");
+        ids.add("986515b0-f89b-41ac-88c2-be8e83b8cba7");
+
+        String sql = "SELECT * FROM issues where issue_id in (" + dbUtils.transformListToString(ids) + ")";
+
+        // When
+        List<IssuesDTO> issues = dbUtils.queryMultiObjectReturnList(sql, IssuesDTO.class);
+        // Then
+        // issues.forEach(System.out::println);
+        assertThat(issues, notNullValue());
+    }
+
+    @DisplayName("Test execute insert sql")
+    @Test
+    public void testExecuteInsertSql() {
+        // Given
+        String sql = null;
+        sql = "INSERT INTO `issues` (`issue_id`, `title`, `description`, `status`, `create_time`, `update_time`, `creator`, `update_user`, `project_id`, `handler`) " + "VALUES (?, 'aaa', 'aaa', '1', '1676609583092', '1676609583092', 'admin@aliyun.com', " + "'admin@aliyun.com', 'c0c42460-5945-464b-b8da-f869e979ca28', 'miller.shan@aliyun.com')";
+
+        // When. sql 语句中？占位符的值
+        Integer updateResult = dbUtils.executeInsertOrUpdateOrDelete(sql, ULIDUtils.generateULID());
+
+        // Then
+        assertThat(updateResult, Matchers.greaterThan(0));
+    }
+
+    @DisplayName("Test execute update sql")
+    @Test
+    public void testExecuteUpdateSql() {
+        // Given
+        String sql = "UPDATE `issues` SET `title` = 'test' WHERE (`issue_id` = ?)";
+
+        // When
+        Integer updateResult = dbUtils.executeInsertOrUpdateOrDelete(sql, ULIDUtils.generateULID());
+
+        // Then
+        assertThat(updateResult, Matchers.greaterThanOrEqualTo(0));
+    }
+
+    @DisplayName("Test execute delete sql")
+    @Test
+    public void testDeleteUpdateSql() {
+        // Given
+        String sql = "DELETE FROM `issues` WHERE (`issue_id` = ?)";
+        // When
+        Integer updateResult = dbUtils.executeInsertOrUpdateOrDelete(sql, ULIDUtils.generateULID());
+        // Then
+        assertThat(updateResult, Matchers.greaterThanOrEqualTo(0));
+    }
+
+    @DisplayName("Test invoke spring JdbcTemplate method")
+    @Test
+    public void testInvokeSpringJdbcTemplateMethod() {
+        // Given
+        String sql = "UPDATE `issues` SET `title` = 'test' WHERE (`issue_id` = 'test')";
+        // When
+        Integer updateResult = dbUtils.getJdbcTemplate().update(sql);
+        // Then
+        assertThat(updateResult, Matchers.greaterThanOrEqualTo(0));
+    }
+
+    @DisplayName("Test invoked Spring JdbcTemplate method but Method is Override")
+    @Test
+    public void testInvokedSpringJdbcTemplateMethodButMethodIsOverride() {
+        // Given
+        String sql = "UPDATE `ct_dev`.`issues` SET `title` = 'test' WHERE (`issue_id` = ?)";
+        // When
+        int[] ints = dbUtils.getJdbcTemplate().batchUpdate(sql, Stream.of(new Object[]{"111"}, new Object[]{"222"}).collect(Collectors.toList()));
+        // Then
+        assertThat(ints, notNullValue());
+    }
+}
