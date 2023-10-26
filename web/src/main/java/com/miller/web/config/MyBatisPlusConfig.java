6c2a2973ffbe0efe5a7f8ffa92903613579d55b6
Miller Shan <miller.shan@aliyun.com>
2023-10-26 16:49:19 +0800

    feat: 添加配置

---
+++ web/src/main/java/com/miller/web/config/MyBatisPlusConfig.java
@@ -0,0 +1,26 @@
+package com.miller.web.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * MyBatisPlus 配置类
+ *
+ * @author Miller Shan
+ * @version 1.0.0
+ */
+@Configuration
+public class MyBatisPlusConfig {
+    /**
+     * MyBatisPlus 分页插件，不添加的话会出现 getTotal() 为 0
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
