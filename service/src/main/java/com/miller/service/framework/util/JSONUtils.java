78e1eb007067e9696de28bff5eefcb18fb39aa6e
Miller Shan <miller.shan@aliyun.com>
2023-12-06 18:09:15 +0800

    refactor: 去掉gson代码。

--- service/src/main/java/com/miller/service/framework/util/JSONUtils.java
+++ service/src/main/java/com/miller/service/framework/util/JSONUtils.java
@@ -3,16 +3,6 @@ package com.miller.service.framework.util;
 
 import com.alibaba.fastjson2.JSON;
 import com.alibaba.fastjson2.JSONException;
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-
-import java.util.Map;
-
 /**
  * JSON 工具类
  *
@@ -24,41 +14,41 @@ public class JSONUtils {
     private JSONUtils() {
     }
 
-    private static ObjectMapper defaultMapperCreator() {
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.registerModules(ObjectMapper.findModules());
-
-        // 设置为false表示不检测失败字段映射, 在反序列化成 Java 对象时忽略不存在的属性。有些时候由于请求的数据不同返回的对象数据结构不一定每次都固定。
-        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, /* 默认值true */false);
-        // 设置序列化时如果对象的属性值为 null 则不参与序列化和反序列化。
-        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-        // 启用了 FAIL_ON_TRAILING_TOKENS 选项，以确保如果有效JSON后面除了空白之外还有任何文本，则验证将失败。
-        mapper.enable(DeserializationFeature.FAIL_ON_TRAILING_TOKENS);
-
-        //mapper.configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
-
-
-        //是否允许使用注释
-        mapper.configure(JsonParser.Feature.ALLOW_COMMENTS, false);
-        //字段允许去除引号
-        mapper.configure(JsonParser.Feature.ALLOW_UNQUOTED_FIELD_NAMES, false);
-        //允许单引号
-        mapper.configure(JsonParser.Feature.ALLOW_SINGLE_QUOTES, false);
-        //严格重复检测
-        mapper.configure(JsonParser.Feature.STRICT_DUPLICATE_DETECTION, false);
-        //时间字段输出时间戳
-        mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, true);
-        //时间输出为毫秒而非纳秒
-        mapper.configure(SerializationFeature.WRITE_DATE_TIMESTAMPS_AS_NANOSECONDS, true);
-        //时间读取为毫秒而非纳秒
-        mapper.configure(DeserializationFeature.READ_DATE_TIMESTAMPS_AS_NANOSECONDS, true);
-        //空对象不出错
-        mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, true);
-        //不输出空值字段
-        // mapper.setDefaultPropertyInclusion(JsonInclude.Include.NON_NULL);
-        // mapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
-        return mapper;
-    }
+//    private static ObjectMapper defaultMapperCreator() {
+//        ObjectMapper mapper = new ObjectMapper();
+//        mapper.registerModules(ObjectMapper.findModules());
+//
+//        // 设置为false表示不检测失败字段映射, 在反序列化成 Java 对象时忽略不存在的属性。有些时候由于请求的数据不同返回的对象数据结构不一定每次都固定。
+//        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, /* 默认值true */false);
+//        // 设置序列化时如果对象的属性值为 null 则不参与序列化和反序列化。
+//        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+//        // 启用了 FAIL_ON_TRAILING_TOKENS 选项，以确保如果有效JSON后面除了空白之外还有任何文本，则验证将失败。
+//        mapper.enable(DeserializationFeature.FAIL_ON_TRAILING_TOKENS);
+//
+//        //mapper.configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
+//
+//
+//        //是否允许使用注释
+//        mapper.configure(JsonParser.Feature.ALLOW_COMMENTS, false);
+//        //字段允许去除引号
+//        mapper.configure(JsonParser.Feature.ALLOW_UNQUOTED_FIELD_NAMES, false);
+//        //允许单引号
+//        mapper.configure(JsonParser.Feature.ALLOW_SINGLE_QUOTES, false);
+//        //严格重复检测
+//        mapper.configure(JsonParser.Feature.STRICT_DUPLICATE_DETECTION, false);
+//        //时间字段输出时间戳
+//        mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, true);
+//        //时间输出为毫秒而非纳秒
+//        mapper.configure(SerializationFeature.WRITE_DATE_TIMESTAMPS_AS_NANOSECONDS, true);
+//        //时间读取为毫秒而非纳秒
+//        mapper.configure(DeserializationFeature.READ_DATE_TIMESTAMPS_AS_NANOSECONDS, true);
+//        //空对象不出错
+//        mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, true);
+//        //不输出空值字段
+//        // mapper.setDefaultPropertyInclusion(JsonInclude.Include.NON_NULL);
+//        // mapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+//        return mapper;
+//    }
 
     /**
      * 判断字符串是否是合法的 JSON
