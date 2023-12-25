0996e5874520724da895168f27ec2904c233926d
Miller Shan <miller.shan@aliyun.com>
2023-12-25 11:20:30 +0800

    feat: 用户确认订单已送达测试用例

---
+++ user-app/src/main/java/com/miller/userapp/order/confirm/provider/ConfirmOrderStatusDataProvider.java
@@ -0,0 +1,28 @@
+package com.miller.userapp.order.confirm.provider;
+
+import com.miller.data.center.user.TestCaseDataConstant;
+import com.miller.service.framework.cache.CacheUtils;
+import com.miller.userapp.order.confirm.request.ConfirmOrderStatusRequestDTO;
+import com.miller.userapp.order.create.response.CreateOrderResponseDTO;
+import org.junit.jupiter.params.provider.Arguments;
+
+import java.util.stream.Stream;
+
+/**
+ * 数据提供者：用户确认订单已送到
+ *
+ * @author Miller Shan
+ * @version 1.0
+ * @since 2023/12/25 11:14:33
+ */
+@SuppressWarnings("unused")
+public class ConfirmOrderStatusDataProvider {
+
+    static Stream<Arguments> confirmOrderStatusIsReceived() {
+        ConfirmOrderStatusRequestDTO confirmOrderStatusRequestDTO = new ConfirmOrderStatusRequestDTO();
+        String orderSn = CacheUtils.get(TestCaseDataConstant.ORDER_ID_OBJECT_KEY, CreateOrderResponseDTO.class).getResult().getOrderSn();
+        confirmOrderStatusRequestDTO.setOrderSn(orderSn);
+
+        return Stream.of(Arguments.of(confirmOrderStatusRequestDTO));
+    }
+}
---
+++ user-app/src/main/java/com/miller/userapp/order/confirm/response/ConfirmOrderStatusResponseDTO.java
@@ -0,0 +1,17 @@
+package com.miller.userapp.order.confirm.response;
+
+import com.miller.userapp.dto.BasicResponseDTO;
+
+/**
+ * 响应对象-用户确认订单已送到
+ * <p>
+ * 这里我直接引用了开发的代码，减少维护成本，使用统一响应对象。
+ * </p>
+ *
+ * @author Miller Shan
+ * @version 1.0
+ * @since 2023/12/25 10:59:18
+ */
+public class ConfirmOrderStatusResponseDTO extends BasicResponseDTO<Object> {
+    // 测试用例应该基本不需要添加额外的响应信息
+}
