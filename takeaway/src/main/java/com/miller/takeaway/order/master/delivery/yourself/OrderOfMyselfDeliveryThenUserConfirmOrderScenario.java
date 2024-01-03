76c42077700a417544b53317d4a0e19d06932cf8
Miller Shan <miller.shan@aliyun.com>
2024-01-03 14:51:22 +0800

    feat: 【主干场景】【用户自取】用户选择配送方式为自取，支付成功后直接确认取餐，订单主流程结束

---
+++ takeaway/src/main/java/com/miller/takeaway/order/master/delivery/yourself/OrderOfMyselfDeliveryThenUserConfirmOrderScenario.java
@@ -0,0 +1,50 @@
+package com.miller.takeaway.order.master.delivery.yourself;
+
+import com.miller.merchant.order.waiting.receiving.ReceivingOrderTests;
+import com.miller.service.framework.annotation.TestCase;
+import com.miller.userapp.order.confirm.ConfirmOrderStatusTests;
+import com.miller.userapp.order.create.CreateOrderByMyselfDeliveryTests;
+import com.miller.userapp.pay.balance.PayByBalanceTests;
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.SuiteDisplayName;
+
+/**
+ * 【主干场景】【用户自取】用户选择配送方式为自取，支付成功后直接确认取餐，订单主流程结束
+ * <p>
+ * 流程：用户下单->自取->支付成功->待接单->等待商家接单（7）->接单并备餐（9）->备餐中（10）->用户确认取餐
+ * ->订单主流程结束（22）
+ * </p>
+ * <p>
+ *     <ul>覆盖路径
+ *         <li>路径: 1，3， 4， 5， 7，9，10，11，12， 13， 14， 15， 22</li>
+ *     </ul>
+ * </p>
+ *
+ * @author Miller Shan
+ * @version 1.0
+ * @since 2024/1/3 14:50:28
+ */
+@SelectClasses({
+        // 用户登录
+        com.miller.userapp.login.LoginTests.class,
+        // 创建订单-用户自取
+        CreateOrderByMyselfDeliveryTests.class,
+        // 支付订单
+        PayByBalanceTests.class,
+
+        // 商家登录APP
+        com.miller.merchant.login.LoginTests.class,
+        // 接单并备餐
+        ReceivingOrderTests.class,
+
+        // 用户确认订单已送达。注意：需要订单被【骑手/商家】派送完成之后执行，用户自取除外。
+        ConfirmOrderStatusTests.class,
+
+        // 订单主流程结束
+})
+@Suite
+@SuiteDisplayName("【主干场景】【用户自取】用户选择配送方式为自取，支付成功后直接确认取餐，订单主流程结束")
+@TestCase(testCaseID = "01HJQY9HHYMR1R38TTSM7Y885S", name = "【主干场景】【用户自取】用户选择配送方式为自取，支付成功后直接确认取餐，订单主流程结束")
+public class OrderOfMyselfDeliveryThenUserConfirmOrderScenario {
+}
