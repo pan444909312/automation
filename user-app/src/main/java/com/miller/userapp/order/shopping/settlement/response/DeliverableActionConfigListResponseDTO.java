0d96cf5bc94eacd8a56fe5be1a8e0dc7e80dfdce
Miller Shan <miller.shan@aliyun.com>
2024-03-22 10:58:43 +0800

    feat: 添加响应对象_交付方式集合

---
+++ user-app/src/main/java/com/miller/userapp/order/shopping/settlement/response/DeliverableActionConfigListResponseDTO.java
@@ -0,0 +1,18 @@
+package com.miller.userapp.order.shopping.settlement.response;
+
+import com.hungrypanda.app.server.dto.address.DeliverableActionConfigListDTO;
+import com.miller.userapp.dto.BasicResponseDTO;
+
+/**
+ * 响应对象_交付方式集合
+ * <p>
+ * 这里我直接引用了开发的代码，减少维护成本，使用统一响应对象。
+ * </p>
+ *
+ * @author Miller Shan
+ * @version 1.0
+ * @since 2024/3/12 10:58:18
+ */
+public class DeliverableActionConfigListResponseDTO extends BasicResponseDTO<DeliverableActionConfigListDTO> {
+    // 测试用例应该基本不需要添加额外的响应信息
+}
