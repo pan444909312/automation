d5275ae75b60b1be01a34d0e60d684552026dd94
panjuxiang <panjuxiang@hungrypandagroup.com>
2024-03-29 11:08:41 +0800

    新增收货地址模块主流程用例（列表、新增、编辑、删除）

---
+++ user-app/src/main/java/com/miller/userapp/address/create/flow/AddressEditFlow.java
@@ -0,0 +1,74 @@
+package com.miller.userapp.address.create.flow;
+
+import com.jayway.jsonpath.JsonPath;
+import com.miller.service.framework.cache.CacheUtils;
+import com.miller.service.framework.http.HttpUtils;
+import com.miller.service.framework.util.JSONUtils;
+import com.miller.userapp.address.create.request.AddressRequestDTO;
+import com.miller.userapp.address.create.response.AddressResponseDTO;
+import com.miller.userapp.constants.BusinessConstant;
+import com.miller.userapp.util.RequestUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 目前里面包含了新增、编辑、删除的场景，
+ * todo 待之后明确结构划分后在调整结构和命名
+ * @author panjuxiang
+ * @since 2024/3/22 18:30
+ */
+public class AddressEditFlow {
+
+    private String saveOrUpdateUrl = BusinessConstant.DOMAIN + "/api/app/user/v1/address/edit";
+    private String deleteUrl = BusinessConstant.DOMAIN + "/api/app/user/v1/address/edit";
+
+    private Map<String,Object> headers;
+
+    public Map<String, Object> setCommonHeaders() {
+
+        if (headers != null){
+            return headers;
+        }
+        headers = new HashMap<>();
+
+        RequestUtils.setHeaders(headers);
+
+        headers.put("Content-Type","application/json");
+        headers.put("Authorization", "aa12137192fcf7ae6a77b86db8fc4228");
+        return headers;
+    }
+
+    public AddressResponseDTO addAddress(AddressRequestDTO addressRequestDTO) {
+
+        Map<String, Object> headers = setCommonHeaders();
+
+        AddressResponseDTO addressResponseDTO = HttpUtils.sendPostRequestReturnJavaObject(saveOrUpdateUrl, null, headers, addressRequestDTO, null, AddressResponseDTO.class);
+
+        String addressEditResponseDTOJson = JSONUtils.toJSONString(addressResponseDTO);
+
+        Object addressId = JsonPath.read(addressEditResponseDTOJson, "$.result.addressId");
+
+        CacheUtils.set("addressId",addressId);
+
+        return addressResponseDTO;
+    }
+
+    public AddressResponseDTO editAddress(AddressRequestDTO addressRequestDTO){
+        Map<String, Object> headers = setCommonHeaders();
+
+        AddressResponseDTO addressResponseDTO = HttpUtils.sendPostRequestReturnJavaObject(saveOrUpdateUrl, null, headers, addressRequestDTO, null, AddressResponseDTO.class);
+
+        return addressResponseDTO;
+    }
+
+    public AddressResponseDTO deleteAddress(AddressRequestDTO addressRequestDTO){
+        Map<String, Object> headers = setCommonHeaders();
+
+        AddressResponseDTO addressResponseDTO = HttpUtils.sendPostRequestReturnJavaObject(deleteUrl, null, headers, addressRequestDTO, null, AddressResponseDTO.class);
+
+
+        return addressResponseDTO;
+    }
+
+}
---
+++ user-app/src/main/java/com/miller/userapp/address/list/request/AddressListRequestDTO.java
@@ -0,0 +1,8 @@
+package com.miller.userapp.address.list.request;
+
+/**
+ * @author panjuxiang
+ * @since 2024/3/21 19:53
+ */
+public class AddressListRequestDTO {
+}
