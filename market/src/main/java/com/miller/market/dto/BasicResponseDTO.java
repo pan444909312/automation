d7b019e0b796494c7fe57934430dc048d3296c26
zhangpei <zhangpei@hungrypandagroup.com>
2024-05-09 16:28:34 +0800

    新增pf用户登录用例

---
+++ market/src/main/java/com/miller/market/dto/BasicResponseDTO.java
@@ -0,0 +1,31 @@
+package com.miller.market.dto;
+
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.panda.market.common.base.Result;
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * 基础_响应实体类
+ *
+ * @see com.panda.market.common.base.Result
+ */
+@Data
+public class BasicResponseDTO<T> extends Result<T> implements Serializable {
+    /*
+    开发代码使用的是 AppResult , 但是使用这个对象会导致偶发性JSON序列化报错
+    "com.alibaba.fastjson.JSONException: No enum ordinal com.panda.common.constants.PResultCode.1000"
+     而且发现这个对象里面多处两个字段blankList，blankObj 但是这两个字段开发代码中又没有用上，不知道写的有啥用。
+
+     */
+    /**
+     * 空列表集
+     */
+    public static JSONArray blankList = new JSONArray();
+    /**
+     * 空对象
+     */
+    public static JSONObject blankObj = new JSONObject();
+}
---
+++ market/src/main/java/com/miller/market/login/request/MarketLoginRequestDTO.java
@@ -0,0 +1,9 @@
+package com.miller.market.login.request;
+
+import com.panda.market.dal.vo.user.UserLoginVO;
+
+/**
+ * 登录请求数据
+ */
+public class MarketLoginRequestDTO extends UserLoginVO {
+}
