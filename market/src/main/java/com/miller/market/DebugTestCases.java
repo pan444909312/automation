e3759c204f361394d319cefa267cf0743715b14d
zhangpei <zhangpei@hungrypandagroup.com>
2024-05-16 15:46:35 +0800

    注释修改

--- market/src/main/java/com/miller/market/DebugTestCases.java
+++ market/src/main/java/com/miller/market/DebugTestCases.java
@@ -24,27 +24,27 @@ import org.junit.platform.suite.api.SuiteDisplayName;
 @SelectClasses({
         // 用户登录
         MarketLoginTests.class,
-        // 收货地址
+        // 收货地址（登录-收货地址）
         MarketGetAddressTests.class,
-        //获取配送时间(需获取配送地址后调用)
+        //获取配送时间(登录-收货地址-配送时间)
 //        MarketDeliveryTimeTests.class,
-        //获取自取时间
+        //获取自取时间（登录-自取时间）
         MarketTakesTimeTests.class,
-        // 加购
+        // 加购（登录-加购）
         MarketAddShopCartTests.class,
-//        // 获取购物车列表（加购后调用）
+//        // 获取购物车列表（登录-加购-购物车列表）
 //        MarketGetShopCartListTests.class,
-//        // 购物车结算（加购后调用）
+//        // 购物车结算（登录-加购-结算）
         MarketSettleShopCartTests.class,
-        //预订单-配送-有时间（加购后调用）
+        //预订单-配送-有时间（登录-获取收货地址-获取配送时间-加购-预订单）
 //        MarketGetPreOrderInfoDeliveryTimeTests.class,
-//        //预订单-配送-无时间（加购后调用）
+//        //预订单-配送-无时间（登录-加购-预订单）
 //        MarketGetPreOrderInfoDeliveryWithoutTimeTests.class,
-//        //预订单-自取-无时间（加购后调用）
+//        //预订单-自取-无时间（登录-加购-预订单）
 //        MarketGetPreOrderInfoTakeWithoutTimeTests.class,
-        //预订单-自取-有时间（加购后调用）
+        //预订单-自取-有时间（登录-获取自取时间-加购-预订单）
         MarketGetPreOrderInfoTakeTimeTests.class,
-//        // 删除购物车（加购后调用）
+//        // 删除购物车（登录-加购-删除购物车）
 //        MarketDeleteShopCartTests.class,
         // 配送下单(登录-获取收货地址-获取配送时间-加购-结算-预订单（配送-有时间）-下单)
 //        MarketCreateOrderDeliveryTimeTests.class,
