a3b001a59ba1e565a20373cd1958a61d650d9b24
zhangpei <zhangpei@hungrypandagroup.com>
2024-05-16 16:53:25 +0800

    修改

--- market/src/main/java/com/miller/market/DebugTestCases.java
+++ market/src/main/java/com/miller/market/DebugTestCases.java
@@ -27,7 +27,7 @@ import org.junit.platform.suite.api.SuiteDisplayName;
         // 收货地址（登录-收货地址）
         MarketGetAddressTests.class,
         //获取配送时间(登录-收货地址-配送时间)
-//        MarketDeliveryTimeTests.class,
+        MarketDeliveryTimeTests.class,
         //获取自取时间（登录-自取时间）
         MarketTakesTimeTests.class,
         // 加购（登录-加购）
@@ -37,19 +37,19 @@ import org.junit.platform.suite.api.SuiteDisplayName;
 //        // 购物车结算（登录-加购-结算）
         MarketSettleShopCartTests.class,
         //预订单-配送-有时间（登录-获取收货地址-获取配送时间-加购-预订单）
-//        MarketGetPreOrderInfoDeliveryTimeTests.class,
+        MarketGetPreOrderInfoDeliveryTimeTests.class,
 //        //预订单-配送-无时间（登录-加购-预订单）
 //        MarketGetPreOrderInfoDeliveryWithoutTimeTests.class,
 //        //预订单-自取-无时间（登录-加购-预订单）
 //        MarketGetPreOrderInfoTakeWithoutTimeTests.class,
         //预订单-自取-有时间（登录-获取自取时间-加购-预订单）
-        MarketGetPreOrderInfoTakeTimeTests.class,
+//        MarketGetPreOrderInfoTakeTimeTests.class,
 //        // 删除购物车（登录-加购-删除购物车）
 //        MarketDeleteShopCartTests.class,
         // 配送下单(登录-获取收货地址-获取配送时间-加购-结算-预订单（配送-有时间）-下单)
-//        MarketCreateOrderDeliveryTimeTests.class,
+        MarketCreateOrderDeliveryTimeTests.class,
         // 自取下单(登录-获取自取时间-加购-结算-预订单（自取-有时间）-下单)
-        MarketCreateOrderTakeTimeTests.class,
+//        MarketCreateOrderTakeTimeTests.class,
 })
 @SuiteDisplayName("调试多个测试用例")
 @Suite
--- market/src/main/java/com/miller/market/order/getPreOrderInfo/MarketGetPreOrderInfoDeliveryTimeTests.java
+++ market/src/main/java/com/miller/market/order/getPreOrderInfo/MarketGetPreOrderInfoDeliveryTimeTests.java
@@ -4,12 +4,20 @@ import com.miller.market.constants.ResponseConstant;
 import com.miller.market.order.getPreOrderInfo.flow.MarketGetPreOrderInfoFlow;
 import com.miller.market.order.getPreOrderInfo.request.MarketGetPreOrderInfoRequestDTO;
 import com.miller.market.order.getPreOrderInfo.response.MarketGetPreOrderInfoResponseDTO;
+import com.miller.service.framework.annotation.EnvTag;
+import com.miller.service.framework.annotation.TestFramework;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+/**
+ * 中超预订单接口
+ */
+@EnvTag.Test
+@TestFramework
+@DisplayName("预订单接口")
 public class MarketGetPreOrderInfoDeliveryTimeTests {
     @MethodSource("com.miller.market.order.getPreOrderInfo.provider.MarketGetPreOrderInfoDataProvider#marketDeliveryTimeDataProvider")
     @ParameterizedTest
