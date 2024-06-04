4314fe6508393a294eae6ee9d66a16b1a175116c
单东东 <shandongdong@hungrypandagroup.com>
2024-06-04 11:37:27 +0800

    feat: 测试 MyBatisPlus 的SQL语句通过xml文件编写

--- demo/src/main/java/com/miller/demo/loginv4/mapper/UserMapper.java
+++ demo/src/main/java/com/miller/demo/loginv4/mapper/UserMapper.java
@@ -2,6 +2,9 @@ package com.miller.demo.loginv4.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.miller.demo.loginv2.request.LoginV2RequestDTO;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 /**
  * 添加对数据库 user 表的操作
@@ -11,4 +14,12 @@ import com.miller.demo.loginv2.request.LoginV2RequestDTO;
  * @since 2024/5/27 13:58:40
  */
 public interface UserMapper extends BaseMapper<LoginV2RequestDTO> {
+
+    /**
+     * 根据参数执行动态SQL语句，SQL语句在对应的xml文件中定义
+     *
+     * @param loginV2RequestDTO {@link LoginV2RequestDTO}
+     * @return 记录集
+     */
+    List<LoginV2RequestDTO> selectByCondition(@Param("user") LoginV2RequestDTO loginV2RequestDTO);
 }
