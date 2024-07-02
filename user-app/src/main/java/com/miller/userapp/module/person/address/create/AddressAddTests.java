695d9defd16955d6ac252a5979f80641a669ff60
单东东 <shandongdong@hungrypandagroup.com>
2024-07-02 10:22:33 +0800

    refactor: 将 data provider 放到测试用例中

--- user-app/src/main/java/com/miller/userapp/module/person/address/create/AddressAddTests.java
+++ user-app/src/main/java/com/miller/userapp/module/person/address/create/AddressAddTests.java
@@ -3,12 +3,16 @@ package com.miller.userapp.module.person.address.create;
 import com.hungrypanda.app.server.api.common.ResultCode;
 import com.miller.service.framework.annotation.EnvTag;
 import com.miller.service.framework.annotation.TestFramework;
+import com.miller.service.framework.util.JSONUtils;
 import com.miller.userapp.module.person.address.create.flow.AddressEditFlow;
 import com.miller.userapp.module.person.address.create.request.AddressRequestDTO;
 import com.miller.userapp.module.person.address.create.response.AddressResponseDTO;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 /**
  * 目前各个场景只先做了一个主流程的 添加、编辑、删除，以及仅对code码做了校验
@@ -23,7 +27,7 @@ import org.junit.jupiter.params.provider.MethodSource;
 public class AddressAddTests {
 
 
-    @MethodSource("com.miller.userapp.module.person.address.create.provider.AddressDataProvider#addressAddData")
+    @MethodSource("addressAddData")
     @ParameterizedTest
     @DisplayName("正常流程_创建收货地址")
     public void shouldAddAddressSuccessfully(AddressRequestDTO addressRequestDTO){
@@ -31,6 +35,33 @@ public class AddressAddTests {
 
         assert addressResponseDTO.getResultCode() == ResultCode.SUCCESS.getCode();
 
+    }
+
+    static Stream<Arguments> addressAddData() {
+
+        String str = "{\n" +
+                "    \"address\": \"China, Zhejiang, Hangzhou, Binjiang District, Feihong Road, 奥体博览中心主体育场\",\n" +
+                "    \"addressRemark\": \"\",\n" +
+                "    \"gender\": 0,\n" +
+                "    \"latitude\": \"30.227698\",\n" +
+                "    \"postcode\": \"000000\",\n" +
+                "    \"telephone\": \"13960000003\",\n" +
+                "    \"addTag\": 0,\n" +
+                "    \"type\": 1,\n" +
+                "    \"buildingName\": \"奥体博览中心主体育场\",\n" +
+                "    \"isDefault\": 0,\n" +
+                "    \"countryCode\": \"86\",\n" +
+                "    \"verify\": 1,\n" +
+                "    \"shopId\": 0,\n" +
+                "    \"contacts\": \"auto-name\",\n" +
+                "    \"houseNum\": \"1号\",\n" +
+                "    \"longitude\": \"120.227514\"\n" +
+                "}";
+
+        AddressRequestDTO addressRequestDTO = JSONUtils.jsonToObject(str, AddressRequestDTO.class);
+
+        return Stream.of(
+                Arguments.of(addressRequestDTO));
     }
 
 
