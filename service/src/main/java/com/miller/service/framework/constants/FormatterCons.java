fa1f561ee3043556a951ffd289a7e087d74575a9
luwei <luwei@hungrypandagroup.com>
2024-07-02 14:51:09 +0800

    订单预约单 -第一个当天的预约单时间 > 当前时间+出餐时间+配送时间平均值

--- service/src/main/java/com/miller/service/framework/constants/FormatterCons.java
+++ service/src/main/java/com/miller/service/framework/constants/FormatterCons.java
@@ -3,4 +3,5 @@ package com.miller.service.framework.constants;
 public class FormatterCons {
     public static final String TIMEFormatter = "HH:mm";
     public static final String YEARDATEFormatter = "yyyy-MM-dd";
+    public static final String TIMESecFormatter = "HH:mm:ss";
 }
