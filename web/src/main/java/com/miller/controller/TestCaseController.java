8cb3259a4ad14eb41eed05ca765428f4ed820bea
单东东 <shandongdong@hungrypandagroup.com>
2024-07-19 15:10:12 +0800

    test: 测试非Spring环境下执行测试用例。

--- web/src/main/java/com/miller/controller/TestCaseController.java
+++ web/src/main/java/com/miller/controller/TestCaseController.java
@@ -1,10 +1,16 @@
 package com.miller.controller;
 
 import com.miller.service.TestCaseService;
+import com.miller.service.framework.launcher.TestCaseRunnerLauncher;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
+import org.junit.platform.launcher.LauncherDiscoveryRequest;
+import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import static org.junit.platform.engine.discovery.ClassNameFilter.includeClassNamePatterns;
+import static org.junit.platform.engine.discovery.DiscoverySelectors.selectPackage;
 
 /**
  * 测试用例管理接口
@@ -28,5 +34,19 @@ public class TestCaseController {
         Long testsFoundCount = testCaseService.runTestCase(packageName);
 
         return String.valueOf(testsFoundCount);
+    }
+
+    /**
+     * 测试非Spring环境下执行测试用例。
+     */
+    public static void main(String[] args) {
+        LauncherDiscoveryRequest request = LauncherDiscoveryRequestBuilder.request()
+                .selectors(
+                        selectPackage("com.miller.userapp.module.home.login")
+                ).filters(
+                        includeClassNamePatterns(".*Scenario[s]?Test[s]?")
+                ).build();
+        new TestCaseRunnerLauncher().executeRequest(request);
+
     }
 }
