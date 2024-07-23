99988b0e3d465f4af9e6cbac7d154f5473a88e8f
单东东 <shandongdong@hungrypandagroup.com>
2024-07-23 18:35:38 +0800

    refactor: 修改使用原生JDBC方式为MyBatisPlus

---
+++ user-app/src/main/java/com/miller/userapp/mapper/redpacket/UserCdKeyMapper.java
@@ -0,0 +1,20 @@
+package com.miller.userapp.mapper.redpacket;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.hungrypanda.app.server.entity.redpacket.UserCdKeyEntity;
+
+/**
+ *
+ * 用户-兑换码实体类
+ * <p>
+ * 查询数据库表与Java实体对象字段的映射关系，这样就可以自动将数据库的表字段映射到Java对象中了。
+ * 查找开发代码中的对应Java实体对象方法: 在代码工程中中搜索 @TableName("表名")，例如: @TableName("用户-兑换码实体类)。
+ * 包命名可以参考开发代码工程，开发代码工程包路径一般在 com.xxx.entity.xxx
+ * </p>
+ *
+ * @author Miller Shan
+ * @version 1.0
+ * @since 2024/7/23 20:00:26
+ */
+public interface UserCdKeyMapper extends BaseMapper<UserCdKeyEntity> {
+}
