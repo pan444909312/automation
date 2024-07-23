5355d62fa04f751d0acbd615d9e51c53fe3cc95f
WXH <wangxiaohao@hungrypanda.onaliyun.com>
2024-07-23 21:07:32 +0800

    添加菜单的：列表，详情，编辑，排序接口的定义，和基础运行的case

--- pos/src/main/java/com/miller/pos/menu/add/AddMenuTests.java
+++ pos/src/main/java/com/miller/pos/menu/add/AddMenuTests.java
@@ -1,6 +1,5 @@
 package com.miller.pos.menu.add;
 
-import com.alibaba.fastjson.JSONObject;
 import com.miller.pos.menu.add.flow.AddMenuFlow;
 import com.miller.pos.menu.add.request.AddMenuRequestDTO;
 import com.miller.pos.menu.add.response.AddMenuResponseDTO;
@@ -14,7 +13,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import static org.assertj.core.api.Assertions.assertThat;
 
 @EnvTag.Test
 @TestFramework
