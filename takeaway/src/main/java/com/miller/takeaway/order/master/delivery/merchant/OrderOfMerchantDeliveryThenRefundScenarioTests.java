1407dc563ca7e31343e98cf3aa2f4ccd28ce37e8
单东东 <shandongdong@hungrypandagroup.com>
2024-07-27 21:31:07 +0800

    feat: 删除重复的id

--- takeaway/src/main/java/com/miller/takeaway/order/master/delivery/merchant/OrderOfMerchantDeliveryThenRefundScenarioTests.java
+++ takeaway/src/main/java/com/miller/takeaway/order/master/delivery/merchant/OrderOfMerchantDeliveryThenRefundScenarioTests.java
@@ -69,6 +69,6 @@ import org.junit.platform.suite.api.SuiteDisplayName;
 })
 @Suite
 @SuiteDisplayName("【主干场景】【商家配送】用户选择配送方式为配送，支付成功，商家配送，用户确认已送达，申请售后，订单结束")
-@TestCase(testCaseID = "01HJQY9HH260WG8SH4S19CQY4Q", name = "【主干场景】【商家配送】用户选择配送方式为配送，支付成功，商家配送，用户确认已送达，申请售后，订单结束")
+@TestCase(testCaseID = "01HJQY9HH260WG8SH4S19CQY4J", name = "【主干场景】【商家配送】用户选择配送方式为配送，支付成功，商家配送，用户确认已送达，申请售后，订单结束")
 public class OrderOfMerchantDeliveryThenRefundScenarioTests {
 }
