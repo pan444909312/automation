c6d235595fad015ea2a7007fb9e282e408c6d3d7
yangqinqin <yangqinqin@hungrypandagroup.com>
2024-08-02 16:41:34 +0800

    首页-商卡二期：会员权益32-会员运费免减"

---
+++ user-app/src/main/java/com/miller/userapp/mapper/shop/ShopMemberCityMapper.java
@@ -0,0 +1,11 @@
+package com.miller.userapp.mapper.shop;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.hungrypanda.app.server.entity.member.MemberCityEntity;
+
+
+/**
+ * 会员
+ */
+public interface ShopMemberCityMapper extends BaseMapper<MemberCityEntity> {
+}
