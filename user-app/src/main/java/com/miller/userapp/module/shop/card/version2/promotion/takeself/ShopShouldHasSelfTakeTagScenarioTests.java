6574af0368bc539053be8288e0a1e5de05672b3f
yancancan <yancancan@hungrypandagroup.com>
2024-08-02 18:46:41 +0800

    自动化商卡标签测试--修改测试文件名称、displayName与用例名一致

--- user-app/src/main/java/com/miller/userapp/module/shop/card/version2/promotion/takeself/ShopShouldHasSelfTakeTagScenarioTests.java
+++ user-app/src/main/java/com/miller/userapp/module/shop/card/version2/promotion/takeself/ShopShouldHasSelfTakeTagScenarioTests.java
@@ -4,6 +4,7 @@ import com.hungrypanda.app.server.vo.index.ShopIndexVO;
 import com.hungrypanda.app.server.vo.index.ShopPromoteVO;
 import com.miller.service.framework.annotation.EnvTag;
 import com.miller.service.framework.annotation.TestFramework;
+import com.miller.service.framework.util.PropertiesUtils;
 import com.miller.userapp.module.shop.card.version2.promotion.takeself.flow.ShopListFlow;
 import com.miller.userapp.module.shop.card.version2.promotion.takeself.request.ShopListRequestDTO;
 import com.miller.userapp.module.shop.card.version2.promotion.takeself.response.ShopListResponseDTO;
@@ -11,30 +12,29 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-
+//import com.hungrypanda.app.server.vo.index;
 import java.util.List;
 import java.util.stream.Stream;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 @EnvTag.Test
 @TestFramework
-@DisplayName("用户-首页店铺流-商卡(中文)-普通店铺配送商卡-优惠标签-自取折扣-展示")
-public class ShopShouldHasSelfTakeDiscountTagTests {
-//    采用店铺2的数据，标签类型：30，自取折扣
-    private final Long shopId = Long.parseLong("59750820");
-    private final Integer type=30;
+@DisplayName("用户-首页店铺流-商卡(中文)-普通店铺配送商卡-优惠标签-可自取-首页-商卡二期：可自取33")
+public class ShopShouldHasSelfTakeTagScenarioTests {
+//    测试店铺：店铺1,测试标签类型：33，content：可自取
+    private final Long shopId = Long.parseLong(new PropertiesUtils().getProperty(this.getClass(),"user.app.for.test.shop.card.version2.shopId"));
+    private final Integer type=33;
 
-    @DisplayName("用户-首页店铺流-商卡(中文)-普通店铺配送商卡-优惠标签-自取折扣-正常展示自取折扣标签")
+    @DisplayName("用户-首页店铺流-商卡(中文)-普通店铺配送商卡-优惠标签-可自取-正常展示自取标签")
     @MethodSource("showLabelDataProvider")
     @ParameterizedTest
     void hasSelfTakeTag(ShopListRequestDTO ShopListRequestdto){
         ShopListResponseDTO ShopListResponsedto= ShopListFlow.getShopList(ShopListRequestdto);
         List<ShopPromoteVO> shopPromoteList =ShopListResponsedto.getResult().getShopList().stream().filter(item -> item.getShopId().equals(shopId)).findFirst().map( ShopIndexVO::getShopPromoteList).orElseThrow();
         String showContent=shopPromoteList.stream().filter(item -> item.getType().equals(type)).findFirst().map( ShopPromoteVO::getShowContent).orElseThrow();
-        assertThat(showContent).isEqualTo("自取再享9折");
+        assertThat(showContent).isEqualTo("可自取");
     }
-    //    DataProvider改为在测试用例文件里写,提供测试数据
+//    DataProvider改为在测试用例文件里写,提供测试数据
     static Stream<Arguments> showLabelDataProvider() {
         ShopListRequestDTO shopListRequestDTO = new ShopListRequestDTO();
         // 可以不用传参数
