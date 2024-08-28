2e2aa48196235186d07a2f781d47a69b15ff029a
panjuxiang <panjuxiang@hungrypandagroup.com>
2024-08-28 20:02:55 +0800

    营销标用例新增

---
+++ user-app/src/main/java/com/miller/userapp/constants/ShopFeatureTypeConstant.java
@@ -0,0 +1,39 @@
+package com.miller.userapp.constants;
+
+/**
+ * 商品营销标type值
+ * @author panjuxiang
+ * @since 2024/8/23 9:33
+ */
+public class ShopFeatureTypeConstant {
+
+    /**
+     * 店铺榜单
+     */
+    public static final Integer SHOP_RANK = 1;
+    /**
+     * 营销文案
+     */
+    public static final Integer EVALUATION = 2;
+    /**
+     * 回头客
+     */
+    public static final Integer RETURNED_VISITOR = 3;
+    /**
+     * 堂食同价
+     */
+    public static final Integer DINE_IN_SAME = 4;
+    /**
+     * 特惠商品
+     */
+    public static final Integer SPECIAL_PROMOTE = 5;
+    /**
+     * 出餐准时
+     */
+    public static final Integer MEALS_ONT_IME = 6;
+    /**
+     * 快速出餐
+     */
+    public static final Integer FAST_FOOD = 7;
+
+}
---
+++ user-app/src/main/java/com/miller/userapp/mapper/shop/SysAppConfigMapper.java
@@ -0,0 +1,12 @@
+package com.miller.userapp.mapper.shop;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.hungrypanda.app.server.entity.config.SysAppConfigEntity;
+import org.apache.ibatis.type.Alias;
+
+/**
+ * @author panjuxiang
+ * @since 2024/8/28 18:18
+ */
+public interface SysAppConfigMapper extends BaseMapper<SysAppConfigEntity> {
+}
