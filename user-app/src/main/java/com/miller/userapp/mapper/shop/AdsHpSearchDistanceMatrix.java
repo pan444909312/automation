edcebd13f90623aa49e3b154d143f19c57197787
heyuan <heyuan@hungrypandagroup.com>
2024-09-27 14:22:42 +0800

    商卡-配距离+配送时间

---
+++ user-app/src/main/java/com/miller/userapp/mapper/shop/AdsHpSearchDistanceMatrix.java
@@ -0,0 +1,14 @@
+package com.miller.userapp.mapper.shop;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.hungrypanda.app.server.entity.data.AdsSearchDistanceMatrixEntity;
+
+/**
+ *
+ *两坐标距离表
+ * @author heyuan
+ * @version 1.0
+ * @since 2024/9/25 20:07
+ */
+public interface AdsHpSearchDistanceMatrix extends BaseMapper<AdsSearchDistanceMatrixEntity> {
+}
