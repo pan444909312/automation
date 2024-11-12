7d36fcd05e048ef154e33d075f16939a59933fd9
yangqinqin <yangqinqin@hungrypandagroup.com>
2024-11-12 13:12:06 +0800

    修改神券路径

--- user-app/src/main/java/com/miller/userapp/module/shop/card/version2/home/promotion/godCoupon/ShopShouldHasGodCouponTests.java
+++ user-app/src/main/java/com/miller/userapp/module/shop/card/version2/home/promotion/godCoupon/ShopShouldHasGodCouponTests.java
@@ -1,4 +1,4 @@
-package com.miller.userapp.module.shop.card.version2.promotion.godCoupon;
+package com.miller.userapp.module.shop.card.version2.home.promotion.godCoupon;
 import com.hungrypanda.app.server.common.enums.ShopPromoteEnum;
 import com.hungrypanda.app.server.vo.index.ShopIndexVO;
 import com.hungrypanda.app.server.vo.index.ShopPromoteVO;
@@ -8,10 +8,9 @@ import com.miller.service.framework.annotation.Scenario;
 import com.miller.service.framework.util.PropertiesUtils;
 import com.miller.userapp.module.home.login.flow.UserLoginFlow;
 import com.miller.userapp.module.home.login.request.UserLoginRequestDTO;
-import com.miller.userapp.module.shop.card.version2.baseinfo.flow.ShopListFlow;
-import com.miller.userapp.module.shop.card.version2.baseinfo.response.ShopListResponseDTO;
-import com.miller.userapp.module.shop.card.version2.promotion.memberBenefit.request.ShopListRequestDTO;
-
+import com.miller.userapp.module.shop.card.version2.home.flow.ShopListFlow;
+import com.miller.userapp.module.shop.card.version2.home.request.ShopListRequestDTO;
+import com.miller.userapp.module.shop.card.version2.home.response.ShopListResponseDTO;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -46,11 +45,10 @@ public class ShopShouldHasGodCouponTests {
      @MethodSource("staticDataProvider")
     @ParameterizedTest
     @DisplayName("普通店铺配送商卡_优惠标签_新会员优惠标签_首页-商卡二期：神券标签")
-     void couponGodDsicount(com.miller.userapp.module.shop.card.version2.baseinfo.request.ShopListRequestDTO shopListRequestDTO) {
+     void couponGodDsicount(ShopListRequestDTO shopListRequestDTO) {
           ShopListResponseDTO shopList = ShopListFlow.getShopList(shopListRequestDTO);
         ShopIndexVO shopIndexVO = shopList.getResult().getShopList().stream()
                 .filter(item -> item.getShopId().equals(shopId)).findFirst().get();
-
 
         //遍历店铺的ShopPromoteList列表，
         ShopPromoteVO memberPacket = shopIndexVO.getShopPromoteList().stream().
