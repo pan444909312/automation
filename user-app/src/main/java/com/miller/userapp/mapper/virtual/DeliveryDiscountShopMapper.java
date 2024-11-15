313e5177cdcdcf266401827186294354f300f3a9
ZhangCheng <zhangcheng@hungrypandagroup.com>
2024-11-15 16:48:58 +0800

    结算-店铺基础运费和加价

---
+++ user-app/src/main/java/com/miller/userapp/mapper/virtual/DeliveryDiscountShopMapper.java
@@ -0,0 +1,8 @@
+package com.miller.userapp.mapper.virtual;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import lombok.Data;
+
+public interface DeliveryDiscountShopMapper extends BaseMapper<DeliveryDiscountShopEntity> {
+}
+
