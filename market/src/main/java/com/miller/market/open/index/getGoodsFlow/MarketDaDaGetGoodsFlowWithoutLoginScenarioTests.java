49cd3abe006f56534c8d9f6233c0311e51fee753
zhangpei <zhangpei@hungrypandagroup.com>
2024-11-26 15:46:25 +0800

    达达首页、分类页相关接口

--- market/src/main/java/com/miller/market/open/index/getGoodsFlow/MarketDaDaGetGoodsFlowWithoutLoginScenarioTests.java
+++ market/src/main/java/com/miller/market/open/index/getGoodsFlow/MarketDaDaGetGoodsFlowWithoutLoginScenarioTests.java
@@ -1,23 +1,17 @@
 package com.miller.market.open.index.getGoodsFlow;
 
 import com.miller.market.constants.ResponseConstant;
-import com.miller.market.index.getIndex.flow.MarketGetIndexWithoutLoginFlow;
-import com.miller.market.index.getIndex.response.MarketGetIndexResponseDTO;
 import com.miller.market.open.index.getGoodsFlow.flow.MarketGetGoodsFlowWithoutLoginFlow;
 import com.miller.market.open.index.getGoodsFlow.request.MarketGetGoodsFlowRequestDTO;
 import com.miller.market.open.index.getGoodsFlow.response.MarketGetGoodsFlowResponseDTO;
 import com.miller.service.framework.annotation.EnvTag;
 import com.miller.service.framework.annotation.Scenario;
-import com.panda.market.dal.dto.IndexListDTO;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.stream.Stream;
 
 
@@ -29,15 +23,7 @@ import java.util.stream.Stream;
         developmentTime = 30, maintenanceTime = 0, manualTestTime = 15)
 @EnvTag.Test
 @DisplayName("达达首页商品流")
-public class MarketGetGoodsFlowWithoutLoginScenarioTests {
-    private static MarketGetIndexResponseDTO marketGetIndexResponseDTO = new MarketGetIndexResponseDTO();
-    private static List<IndexListDTO>  indexList = new ArrayList<>();
-    @BeforeAll
-    static void beforeAll() {
-        //调用首页接口
-        marketGetIndexResponseDTO = MarketGetIndexWithoutLoginFlow.getIndex();
-
-    }
+public class MarketDaDaGetGoodsFlowWithoutLoginScenarioTests {
 
     @MethodSource("staticGetGoodsListDataProvider")
     @ParameterizedTest
@@ -51,7 +37,6 @@ public class MarketGetGoodsFlowWithoutLoginScenarioTests {
 
         //过滤烟
         requestDTO.setIsFilterTobaccoGoods(true);
-        Stream.of(Arguments.of(requestDTO));
         responseDTO= MarketGetGoodsFlowWithoutLoginFlow.getGoodsList(requestDTO);
 
         Assertions.assertThat(responseDTO.getCode()).isEqualTo(ResponseConstant.code);
