4143e26450e3aa0fd03d68c253cfd92961fdb464
单东东 <shandongdong@hungrypandagroup.com>
2024-11-29 17:26:59 +0800

    feat: add apifox scheduled task

--- service/src/main/java/com/miller/service/job/ApiFoxScheduled.java
+++ service/src/main/java/com/miller/service/job/ApiFoxScheduled.java
@@ -1,4 +1,4 @@
-package com.miller.service.business.scheduled;
+package com.miller.service.job;
 
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -6,26 +6,25 @@ import org.springframework.stereotype.Component;
 import java.io.*;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.time.Instant;
 import java.util.Date;
 
 /**
- * 定时任务
+ * ApiFox 定时任务
  *
  * @author Miller Shan
  * @version 1.0
  * @since 2024/11/6 10:23:44
  */
 @Component
-public class ApiFox {
+public class ApiFoxScheduled {
     private static final String runApiFoxCommand = "apifox run https://apifox.hungrypanda.it/api/v1/projects/345145/api-test/ci-config/345113/detail?token=x6O0_WS0DlzSRJNhr5Lh63 -r html,cli --database-connection ./database-connections.json  --upload-report";
 
     // "0 15 10 * * ?" 每天上午10:15触发
     @Scheduled(cron = "0 10 1 * * ?")
     public void scheduledTask() {
         JavaShellUtil.executeShell(runApiFoxCommand);
-//        JavaShellUtil.executeShell("ls -l");
-//        System.out.println("任务执行时间：" + Date.from(Instant.now()));
+        //        JavaShellUtil.executeShell("ls -l");
+        //        System.out.println("任务执行时间：" + Date.from(Instant.now()));
     }
 
     static class JavaShellUtil {
@@ -36,7 +35,7 @@ public class ApiFox {
         private static final String executeShellLogFile = basePath + "executeShell.log";
 
         // 发送文件到系统的Shell的文件名(绝对路径)
-//        private static final String sendShellName = basePath + "test.sh";
+        //        private static final String sendShellName = basePath + "test.sh";
         public static int executeShell(String shellCommand) {
             System.out.println("shellCommand:" + shellCommand);
             int success = 0;
@@ -58,13 +57,13 @@ public class ApiFox {
 
                     // bufferedReader用于读取Shell的输出内容
                     bufferedReader = new BufferedReader(new InputStreamReader(pid.getInputStream()));
-//读到标准出错的信息
+                    //读到标准出错的信息
                     stdError = new BufferedReader(new InputStreamReader(pid.getErrorStream()));
-//这个是或得脚本执行的返回值
+                    //这个是或得脚本执行的返回值
                     int status = pid.waitFor();
 
 
-//如果脚本执行的返回值不是0,则表示脚本执行失败，否则（值为0）脚本执行成功。
+                    //如果脚本执行的返回值不是0,则表示脚本执行失败，否则（值为0）脚本执行成功。
                     if (status != 0) {
                         stringBuffer.append("shell脚本执行失败！");
                     } else {
