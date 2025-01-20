9c95abf824e8457db38f29a38c714d74f45c94ac
panjuxiang <panjuxiang@hungrypandagroup.com>
2025-01-20 16:57:32 +0800

    constants补充

---
+++ entity/src/main/java/com/miller/entity/constant/SysConfigConstants.java
@@ -0,0 +1,10 @@
+package com.miller.entity.constant;
+
+/**
+ * @author panjuxiang
+ * @since 2025/1/16 20:38
+ */
+public class SysConfigConstants {
+
+    public static final String DEFAULT_FUTURE_DATE = "DEFAULT_FUTURE_DATE";
+}
