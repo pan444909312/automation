80e66c1ddfc29d9c330e422a5fa5acce04ef4810
luwei <luwei@hungrypandagroup.com>
2025-02-06 14:38:33 +0800

    满减/折红包

---
+++ user-app/src/main/java/com/miller/userapp/module/shop/card/version2/redPacket/base/response/FetchShopRedPacketRetResponseDTO.java
@@ -0,0 +1,6 @@
+package com.miller.userapp.module.shop.card.version2.redPacket.base.response;
+
+import com.hungrypanda.app.server.api.res.member.FetchShopRedPacketRetRes;
+
+public class FetchShopRedPacketRetResponseDTO extends FetchShopRedPacketRetRes {
+}
