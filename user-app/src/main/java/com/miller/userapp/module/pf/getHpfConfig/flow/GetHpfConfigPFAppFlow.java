7c3899d9e29bdc83c98f189693b3eb5a8a3fe345
zhangpei <zhangpei@hungrypandagroup.com>
2025-04-01 15:08:34 +0800

    pfapp融合开关接口

---
+++ user-app/src/main/java/com/miller/userapp/module/pf/getHpfConfig/flow/GetHpfConfigPFAppFlow.java
@@ -0,0 +1,26 @@
+package com.miller.userapp.module.pf.getHpfConfig.flow;
+
+import com.miller.service.framework.http.HttpUtils;
+import com.miller.userapp.constants.BusinessConstant;
+import com.miller.userapp.module.pf.getHpfConfig.response.GetHpfConfigResponseDTO;
+import com.miller.userapp.util.RequestUtils;
+
+import java.util.HashMap;
+
+/**
+ * 融合开关接口请求
+ */
+public class GetHpfConfigPFAppFlow {
+    static String uri= BusinessConstant.DOMAIN + "/api/user/supermarket/getHpfConfig";
+    public static GetHpfConfigResponseDTO getHpfConfigFlow(){
+        var myheaders = new HashMap<String, Object>();
+        myheaders.put("Content-Type", "application/json");
+        myheaders.put("latitude","30.20111");
+        myheaders.put("longitude","120.22136");
+        myheaders.put("appTypeId","20");
+        RequestUtils.setHeaders(myheaders);
+        return HttpUtils.sendGetRequestReturnJavaObject(uri, null,RequestUtils.getHeaders(),null,
+                GetHpfConfigResponseDTO.class );
+
+    }
+}
