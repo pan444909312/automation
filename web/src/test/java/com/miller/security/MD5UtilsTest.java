7e6fd51c430115f094580c0227f0cecc11116a05
单东东 <shandongdong@hungrypandagroup.com>
2025-05-14 21:35:09 +0800

    test： change password

--- web/src/test/java/com/miller/security/MD5UtilsTest.java
+++ web/src/test/java/com/miller/security/MD5UtilsTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 class MD5UtilsTest {
 
-    @DisplayName("生成 MD5 密码")
+    @DisplayName("生成 MD5 密码,用于修改用户忘记密码重置密码")
     @Test
     void testMD5ForPassword() {
         String s = MD5Utils.md5("admin@aliyun.com" + "12345678");
