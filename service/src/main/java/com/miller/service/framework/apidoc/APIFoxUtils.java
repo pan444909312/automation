55e11895079cdfadf3fed44594d2dc594ff239f9
单东东 <shandongdong@hungrypandagroup.com>
2025-05-20 17:11:27 +0800

    feat: APIFoxUtils

---
+++ service/src/main/java/com/miller/service/framework/apidoc/APIFoxUtils.java
@@ -0,0 +1,19 @@
+package com.miller.service.framework.apidoc;
+
+/**
+ * APIFoxUtils
+ *
+ * @author Miller Shan
+ * @version 1.0
+ * @since 2025/5/19 15:37:41
+ */
+public class APIFoxUtils {
+    private static final String APIFOX_URL = "https://api.apifox.cn/api/v1/projects/";
+    private static final String APIFOX_TOKEN = "APS-H8goSkw2cEK3izTLXcuZsekvNnVnzLIk";
+    private static final String testGetApiInfoByUrl = "https://apifox.hungrypanda.it/link/project/345093/apis/api-3478346";
+    
+
+
+
+
+}
