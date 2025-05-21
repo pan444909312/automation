48ccb36ae7a13eba04dd48693b48b5b5110eabc4
单东东 <shandongdong@hungrypandagroup.com>
2025-05-21 15:39:19 +0800

    feat: 重构表结构，增加更多请求、响应数据字段

--- web/src/main/resources/db/migration/V4_add_table.sql
+++ web/src/main/resources/db/migration/V4_add_table.sql
@@ -1,22 +1,25 @@
 create table if not exists automation_coverage_api
 (
     id                        bigint auto_increment comment '主键id',
-    host                      varchar(64)  default '' not null comment '域名',
-    method                    varchar(16)  default '' not null comment 'HTTP 请求方法',
-    path                      varchar(255) default '' not null comment '请求路径',
-    requests_times_production bigint       default 0  not null comment '线上请求次数',
-    country                   varchar(64)  default '' not null comment '国家',
-    is_automation             tinyint      default 0  not null comment '是否已经实现自动化,0:否、1:是',
-    last_execute_time         bigint       default 0  not null comment '最后一次执行时间',
-    executor                  varchar(64)  default '' not null comment '执行人员',
-    is_delete                 tinyint      default 0  not null comment '是否删除',
-    test_case_path            varchar(255) default '' not null comment '测试用例的url参数',
-    test_case_request_last    varchar(1024) default '' not null comment '最后一次测试用例的请求数据',
-    test_case_response_last   varchar(1024) default '' not null comment '最后一次测试用例的响应结果',
+    host                      varchar(64)   default '' not null comment '域名',
+    method                    varchar(16)   default '' not null comment 'HTTP 请求方法',
+    path                      varchar(255)  default '' not null comment '请求路径',
+    requests_times_production bigint        default 0  not null comment '线上请求次数',
+    country                   varchar(64)   default '' not null comment '国家',
+    is_automation             tinyint       default 0  not null comment '是否已经实现自动化,0:否、1:是',
+    last_execute_time         bigint        default 0  not null comment '最后一次执行时间',
+    executor                  varchar(64)   default '' not null comment '执行人员',
+    is_delete                 tinyint       default 0  not null comment '是否删除',
+    test_case_request_path    varchar(255)  default '' not null comment '测试用例的url参数',
+    test_case_response_body   varchar(1024) default '' not null comment '最后一次测试用例的响应结果',
+    test_case_request_method  varchar(16)   default '' not null comment '测试用例请求Method',
+    test_case_request_uri     varchar(256)  default '' not null comment '测试用例请求uri',
+    test_case_request_headers varchar(512)  default '' not null comment '测试用例请求头',
+    test_case_request_body    varchar(1024) default '' not null comment '测试用例请求体',
     primary key (id),
-    index                     idx_path (path) comment '接口路径',
-    index                     idx_requests_times (requests_times_production) comment '线上请求次数'
-) comment='自动化测试接口覆盖率';
+    index idx_path (path) comment '接口路径',
+    index idx_requests_times (requests_times_production) comment '线上请求次数'
+) comment ='自动化测试接口覆盖率';
 
 -- # uk 线上阿里云日志导出的数据
 -- # update automation_coverage_api set country = 'uk' where id <= 46705;
