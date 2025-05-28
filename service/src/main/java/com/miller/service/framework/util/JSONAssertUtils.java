09d9dfb114ddbfcde033808f62b7a138a3588936
单东东 <shandongdong@hungrypandagroup.com>
2025-05-28 14:41:08 +0800

    feat: deprecated unused class

--- service/src/main/java/com/miller/service/framework/util/JSONAssertUtils.java
+++ service/src/main/java/com/miller/service/framework/util/JSONAssertUtils.java
@@ -1,11 +1,12 @@
 package com.miller.service.framework.util;
 
 /**
- * JSONAssert 断言工具
+ * JSONAssert 断言工具,已废弃。使用 JsonUnit 可替代
  *
  * @author Miller Shan
  * @version 1.0
  * @since 2024/6/7 18:31:21
  */
+@Deprecated
 public class JSONAssertUtils {
 }
