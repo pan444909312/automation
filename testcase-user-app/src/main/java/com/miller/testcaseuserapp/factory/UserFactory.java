a2b247b8a3a052a721417c88f144f8f1dbf4130b
单东东 <shandongdong@hungrypandagroup.com>
2025-05-30 16:35:46 +0800

    feat: 工厂类，先占位

---
+++ testcase-user-app/src/main/java/com/miller/testcaseuserapp/factory/UserFactory.java
@@ -0,0 +1,11 @@
+package com.miller.testcaseuserapp.factory;
+
+/**
+ * 造数据工厂。提供创造各种用户的能力。
+ *
+ * @author Miller Shan
+ * @version 1.0
+ * @since 2025/5/27 23:03:37
+ */
+public class UserFactory {
+}
