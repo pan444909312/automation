6bf2e1345b1960ca1b784e119bab0f540353a617
单东东 <shandongdong@hungrypandagroup.com>
2025-05-30 11:46:32 +0800

    feat: 1. remove unused code 2. replace HttpUtils to TestcaseHelpful

--- testcase-user-app/src/main/java/com/miller/testcaseuserapp/module/home/module/mobilebanner/ModuleMobileBannerScenarioTests.java
+++ testcase-user-app/src/main/java/com/miller/testcaseuserapp/module/home/module/mobilebanner/ModuleMobileBannerScenarioTests.java
@@ -1,27 +1,23 @@
 package com.miller.testcaseuserapp.module.home.module.mobilebanner;
 
-import com.miller.service.framework.annotation.EnvTag;
 import com.miller.service.framework.annotation.Scenario;
-import com.miller.service.framework.annotation.TestFramework;
-import com.miller.service.framework.http.HttpUtils;
 import com.miller.testcaseuserapp.config.TestcaseConfig;
 import com.miller.testcaseuserapp.utils.TestCaseHelpful;
 import net.javacrumbs.jsonunit.core.Option;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-@EnvTag.Test
-@TestFramework
 @Scenario(scenarioID = "01JWCZVXDASV9FCE7F774FJR2Z", scenarioName = "m站-广告banner展示"
         , author = "yancancan@hungrypandagroup.com", developmentTime = 30, maintenanceTime = 0, manualTestTime = 15)
 public class ModuleMobileBannerScenarioTests {
-    private static final String uri = TestcaseConfig.HostMobile+"/api/app/user/web/banner/image";
+    private static final String uri = TestcaseConfig.HostMobile + "/api/app/user/web/banner/image";
+
     @DisplayName("m站-广告banner展示")
     @Test
-    public void shouldReturnBannerSuccessfully(){
+    void shouldReturnBannerSuccessfully() {
         var headers = TestCaseHelpful.getHeaders("module/headers_mobile.json");
-        var requestBody=TestCaseHelpful.getJsonRequestBody("module/home/module/mobilebanner/request/success.json");
-        var responseBody = HttpUtils.sendPostRequestReturnBody(uri, null, headers, requestBody, null);
+        var requestBody = TestCaseHelpful.getJsonRequestBody("module/home/module/mobilebanner/request/success.json");
+        var responseBody = TestCaseHelpful.sendRequest("POST", uri, null, headers, requestBody);
         var expectedStr = TestCaseHelpful.getFileContent("module/home/module/mobilebanner/response/assert_some_fields.json");
         TestCaseHelpful.assertThatJson(responseBody).when(Option.IGNORING_EXTRA_FIELDS).isEqualTo(expectedStr);
 
--- testcase-user-app/src/main/java/com/miller/testcaseuserapp/module/redpacket/ShopRedpacketScenarioTests.java
+++ testcase-user-app/src/main/java/com/miller/testcaseuserapp/module/redpacket/ShopRedpacketScenarioTests.java
@@ -1,8 +1,6 @@
 package com.miller.testcaseuserapp.module.redpacket;
 
-import com.miller.service.framework.annotation.EnvTag;
 import com.miller.service.framework.annotation.Scenario;
-import com.miller.service.framework.annotation.TestFramework;
 import com.miller.service.framework.http.HttpUtils;
 import com.miller.testcaseuserapp.config.TestcaseConfig;
 import com.miller.testcaseuserapp.utils.TestCaseHelpful;
@@ -10,15 +8,13 @@ import net.javacrumbs.jsonunit.core.Option;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-@EnvTag.Test
-@TestFramework
 @Scenario(scenarioID = "01JWCZVXDASV9FCE7F774FJR31", scenarioName = "未登录用户-获取店铺可用红包"
         , author = "yancancan@hungrypandagroup.com", developmentTime = 30, maintenanceTime = 0, manualTestTime = 15)
 public class ShopRedpacketScenarioTests {
     private static final String uri = TestcaseConfig.HOST+"/api/user/shop/redPacket";
     @DisplayName("未登录用户-获取店铺可用红包-店铺红包成功")
     @Test
-    public void shouldReturnRedpacketSuccessfully(){
+    void shouldReturnRedpacketSuccessfully(){
         var headers = TestCaseHelpful.getHeaders("module/headers.json");
         var requestBody=TestCaseHelpful.getJsonRequestBody("module/redpacket/request/success.json");
         var responseBody = HttpUtils.sendPostRequestReturnBody(uri, null, headers, requestBody, null);
