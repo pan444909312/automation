f5c8c3f0eef7a779e12ba1703a69291219c5b65b
yaoqianhu <yaoqianhu@hungrypandagroup.com>
2025-06-10 20:52:20 +0800

    新增用例

---
+++ testcase-user-app/src/main/java/com/miller/testcase/module/home/shop/MenuProductPageList_delivery.java
@@ -0,0 +1,44 @@
+package com.miller.testcase.module.home.shop;
+import com.miller.service.framework.annotation.Scenario;
+import com.miller.testcase.config.TestcaseConfig;
+import com.miller.testcase.utils.TestCaseHelpful;
+import net.javacrumbs.jsonunit.core.Option;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@Scenario(
+        scenarioID = "01JXCEKBSYKKF5NFMHWHBGZA36",
+        scenarioName = "进入店铺获取配送菜单的商品分页列表",
+        author = "yaoqianhu@hungrypandagroup.com",
+        developmentTime = 20, maintenanceTime = 0, manualTestTime = 5)
+@DisplayName("进入店铺获取配送菜单的商品分页列表")
+public class MenuProductPageList_delivery {
+    // 接口请求的 path
+    String uri = TestcaseConfig.HOST + "/api/app/user/v1/shop/menu/productPageList";
+    // 请求方式
+    String method = "POST";
+    // 请求头
+    String headers = "module/headers.json";
+    // 请求体。如果没有传 null 即可（body = null）。比如 GET 请求
+    String body = "module/home/shop/request/delivery_ProductPageListReq.json";
+    // 断言
+    String assert2 = "module/home/shop/response/ProductPageListResp.json";
+
+    @DisplayName("正向流程")
+    @Test
+    public void getMenuListSuccessfully() {
+        // 步骤1: 设置请求头。基本固定写法，不需要修改
+        var requestHeaders = TestCaseHelpful.getHeaders(headers);
+
+        // 步骤2: 设置请求体。基本固定写法，不需要修改
+        var requestBody = TestCaseHelpful.getJsonRequestBody(body);
+
+        // 步骤3: 发起请求,并获取响应结果。基本固定写法，不需要修改
+        var responseBody = TestCaseHelpful.sendRequest(method, uri, null, requestHeaders, requestBody);
+
+        // 步骤4: 断言响应结果，直接拷贝抓包响应结果作为断言。基本固定写法，不需要修改
+       // 方式二：全匹配，断言 实际结果 包含 预期结果,排除掉额外字段。固定写法，不需要修改
+        var expectedStr = TestCaseHelpful.getFileContent(assert2);
+        TestCaseHelpful.assertThatJson(responseBody).when(Option.IGNORING_EXTRA_FIELDS,Option.IGNORING_EXTRA_ARRAY_ITEMS).isEqualTo(expectedStr);
+    }
+}
