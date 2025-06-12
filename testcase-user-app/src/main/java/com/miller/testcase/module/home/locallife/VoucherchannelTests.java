b6b5e74cabb4f565334f44a2991dbf3b1394b073
yaoqianhu <yaoqianhu@hungrypandagroup.com>
2025-06-12 11:18:56 +0800

    新增用例

--- testcase-user-app/src/main/java/com/miller/testcase/module/home/locallife/VoucherchannelTests.java
+++ testcase-user-app/src/main/java/com/miller/testcase/module/home/locallife/VoucherchannelTests.java
@@ -1,4 +1,4 @@
-package com.miller.testcase.module.home.locallife.getvoucherlocationlist;
+package com.miller.testcase.module.home.locallife;
 
 import com.miller.service.framework.annotation.Scenario;
 import com.miller.testcase.config.TestcaseConfig;
@@ -8,31 +8,31 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 /**
- * getVoucherLocationList
+ * VoucherChannel
  *
  * @author yaoqianhu
  * @version 2.0
- * @since 2025/06/11 15:44:09
+ * @since 2025/06/11 14:15:27
  */
 @Scenario(
-        scenarioID = "01JXEZ4J5SK1X91DZPBVS7BEB4", // 自动生成，不要修改
-        scenarioName = "获取团购地段列表",
+        scenarioID = "01JXET20SY4RNA5MPJTX9ZBBR6", // 自动生成，不要修改
+        scenarioName = "团购首页频道流",
         author = "yaoqianhu@hungrypandagroup.com", // 配置本机 Git email 后可自动生成
         developmentTime = 30, maintenanceTime = 0, manualTestTime = 3)
-@DisplayName("获取团购地段列表")
-public class GetvoucherlocationlistTests {
+@DisplayName("团购首页频道流")
+public class VoucherchannelTests {
     // TestcaseConfig.HOST 是接口的请求域名。 后面的 + "是接口的请求路径"
-    String uri = TestcaseConfig.Host_Mobile + "/api/app/user/voucher/location/list";
+    String uri = TestcaseConfig.Host_Mobile + "/api/app/user/voucher/channel";
     // 接口请求方式。如： GET、POST、PUT、DELETE
     String method = "POST";
     // 请求头。默认从 resources 目录下读取文件。
-    String headers = "module/home/locallife/getvoucherlocationlist/request/headers.json";
+    String headers = "module/home/locallife/voucherchannel/request/headers.json";
     // 请求参数。如果没有传 null 即可（params = null）。比如 POST 请求通常没有 params 参数
-    String params = "module/home/locallife/getvoucherlocationlist/request/params.json";
+    String params = null;
     // 请求体。如果没有传 null 即可（body = null）。比如 GET 请求可能没有请求体。作用同请求头
-    String body ="module/home/locallife/getvoucherlocationlist/request/should_success.json";
+    String body = "module/home/locallife/voucherchannel/request/should_success.json";
     // 断言。默认从resources目录下读取文件。下面的代码表示从 resource 的 module/xxx/response/assert_full_field.json 读取文件内容作为断言
-    String assert1 = "module/home/locallife/getvoucherlocationlist/response/assert_full_field.json";
+    String assert1 = "module/home/locallife/voucherchannel/response/assert_full_field.json";
 
     @DisplayName("正向流程")
     @Test
@@ -51,7 +51,7 @@ public class GetvoucherlocationlistTests {
         // 步骤4: 断言响应结果，直接拷贝抓包响应结果作为断言。基本固定写法，不需要修改
         // 方式二：全匹配，断言 实际结果 包含 预期结果,排除掉额外字段。固定写法，不需要修改
         var expectedStr = TestCaseHelpful.getFileContent(assert1);
-        TestCaseHelpful.assertThatJson(responseBody).when(Option.IGNORING_EXTRA_FIELDS).isEqualTo(expectedStr);
+        TestCaseHelpful.assertThatJson(responseBody).when(Option.IGNORING_EXTRA_FIELDS,Option.IGNORING_EXTRA_ARRAY_ITEMS).isEqualTo(expectedStr);
 
     }
 } 
