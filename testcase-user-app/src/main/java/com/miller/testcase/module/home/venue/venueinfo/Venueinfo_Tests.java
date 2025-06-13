7a7de9d12626f1ac5b2ac134d82c204b96b6c696
yaoqianhu <yaoqianhu@hungrypandagroup.com>
2025-06-13 14:53:46 +0800

    新增用例

--- testcase-user-app/src/main/java/com/miller/testcase/module/home/venue/venueinfo/Venueinfo_Tests.java
+++ testcase-user-app/src/main/java/com/miller/testcase/module/home/venue/venueinfo/Venueinfo_Tests.java
@@ -1,4 +1,4 @@
-package com.miller.testcase.module.venueinfo;
+package com.miller.testcase.module.home.venue.venueinfo;
 
 import com.miller.service.framework.annotation.Scenario;
 import com.miller.testcase.config.TestcaseConfig;
@@ -26,13 +26,13 @@ public class Venueinfo_Tests {
     // 接口请求方式。如： GET、POST、PUT、DELETE
     String method = "POST";
     // 请求头。默认从 resources 目录下读取文件。
-    String headers = "module/venueinfo/request/headers.json";
+    String headers = "module/home/venue/venueinfo/request/headers.json";
     // 请求参数。如果没有传 null 即可（params = null）。比如 POST 请求通常没有 params 参数
     String params = null;
     // 请求体。如果没有传 null 即可（body = null）。比如 GET 请求可能没有请求体。作用同请求头
-    String body = "module/venueinfo/request/should_success.json";
+    String body = "module/home/venue/venueinfo/request/should_success.json";
     // 断言。默认从resources目录下读取文件。下面的代码表示从 resource 的 module/xxx/response/assert_full_field.json 读取文件内容作为断言
-    String assert1 = "module/venueinfo/response/assert_full_field.json";
+    String assert1 = "module/home/venue/venueinfo/response/assert_full_field.json";
 
     @DisplayName("正向流程")
     @Test
