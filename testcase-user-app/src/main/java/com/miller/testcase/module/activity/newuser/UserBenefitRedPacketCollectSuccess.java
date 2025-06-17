c1494490650df2c86db8a06324f0e45b626d55b0
panjuxiang <panjuxiang@hungrypandagroup.com>
2025-06-17 10:44:54 +0800

    自动化用例新增5条

---
+++ testcase-user-app/src/main/java/com/miller/testcase/module/activity/newuser/UserBenefitRedPacketCollectSuccess.java
@@ -0,0 +1,32 @@
+package com.miller.testcase.module.activity.newuser;
+
+import com.miller.service.framework.annotation.Scenario;
+import com.miller.testcase.config.TestcaseConfig;
+import com.miller.testcase.utils.TestCaseHelpful;
+import net.javacrumbs.jsonunit.core.Option;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+
+@Scenario(scenarioID = "01JVKR6DPY3AY792BTB6AE5DDA",
+        scenarioName = "领取新人权益红包-权益不存在",
+        author = "panjuxiang@hungrypandagroup.com", developmentTime = 20, maintenanceTime = 0, manualTestTime = 10)
+@DisplayName("/api/user/benefit/redPacket/collect")
+public class UserBenefitRedPacketCollectSuccess {
+    private static final String uri = TestcaseConfig.HOST_APP + "/api/user/benefit/redPacket/collect";
+
+    @DisplayName("领取新人权益红包-权益不存在")
+    @Test
+    void shouldReturnSuccessfully() {
+        Map<String, Object> headers = TestCaseHelpful.getHeaders("module/headers.json");
+        // 给请求头添加数据，例如这里添加token
+        headers.put("Authorization", TestCaseHelpful.login("13999900003", "123456"));
+        String requestBody = TestCaseHelpful.getJsonRequestBody("module/activity/newuser/request/UserBenefitRedPacketCollectReq.json");
+        String responseBody = TestCaseHelpful.sendRequest("POST", uri, null, headers, requestBody);
+        String expectedStr = TestCaseHelpful.getFileContent("module/activity/newuser/response/UserBenefitRedPacketCollectResp.json");
+
+        TestCaseHelpful.assertThatJson(responseBody).when(Option.IGNORING_EXTRA_FIELDS).isEqualTo(expectedStr);
+    }
+}
