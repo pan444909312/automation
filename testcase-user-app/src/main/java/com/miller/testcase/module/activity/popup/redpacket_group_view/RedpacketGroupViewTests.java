c3bf301ae7cb33166c6ea8924334aa4a24831ecb
yancancan <yancancan@hungrypandagroup.com>
2025-06-17 16:47:47 +0800

    1.新增自动化接口用例 2.增加备注

--- testcase-user-app/src/main/java/com/miller/testcase/module/activity/popup/redpacket_group_view/RedpacketGroupViewTests.java
+++ testcase-user-app/src/main/java/com/miller/testcase/module/activity/popup/redpacket_group_view/RedpacketGroupViewTests.java
@@ -1,62 +1,47 @@
-package com.miller.testcase.module.activity.edu_verify;
+package com.miller.testcase.module.activity.popup.redpacket_group_view;
 
 import com.miller.service.framework.annotation.Scenario;
 import com.miller.testcase.config.TestcaseConfig;
-import com.miller.testcase.module.activity.edu_get_captcha.EduGetCaptchaTests;
-import com.miller.testcase.utils.PandaTestDBHelpful;
 import com.miller.testcase.utils.TestCaseHelpful;
 import net.javacrumbs.jsonunit.core.Option;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-
-import java.util.Map;
-
 /**
- * edu_verify
+ * redpacket group view
  *
  * @author yancancan
  * @version 2.0
- * @since 2025/06/13 17:01:01
+ * @since 2025/06/17 16:44:03
  */
 @Scenario(
-        scenarioID = "01JXM8AQWAD7ZNMTFYK1K91BJ3", // 自动生成，不要修改
-        scenarioName = "edu_verify",
+        scenarioID = "01JXYGYHF5TSF5XV22RENFKA7M", // 自动生成，不要修改
+        scenarioName = "redpacket group view",
         author = "yancancan@hungrypandagroup.com", // 配置本机 Git email 后可自动生成
         developmentTime = 10, maintenanceTime = 0, manualTestTime = 3)
-@DisplayName("edu_verify:学生认证接口")
-public class EduVerifyTests {
+@DisplayName("redpacket group view:英文版天降弹窗点击红包")
+public class RedpacketGroupViewTests {
     // TestcaseConfig.HOST 是接口的请求域名。 后面的 + "是接口的请求路径"
-    String uri = TestcaseConfig.HOST_APP + "/api/app/user/edu/verify";
+    String uri = TestcaseConfig.HOST_APP + "/api/user/redPacket/group/view";
     // 接口请求方式。如： GET、POST、PUT、DELETE
     String method = "POST";
     // 请求头。默认从 resources 目录下读取文件。
-    String headers = "module/activity/edu_verify/request/headers.json";
+    String headers = "module/redpacket_group_view/request/headers.json";
     // 请求参数。如果没有传 null 即可（params = null）。比如 POST 请求通常没有 params 参数
     String params = null;
     // 请求体。如果没有传 null 即可（body = null）。比如 GET 请求可能没有请求体。作用同请求头
-    String body = "module/activity/edu_verify/request/should_success.json";
+    String body = "module/redpacket_group_view/request/should_success.json";
     // 断言。默认从resources目录下读取文件。下面的代码表示从 resource 的 module/xxx/response/assert_full_field.json 读取文件内容作为断言
-    String assert1 = "module/activity/edu_verify/response/assert_full_field.json";
-    @BeforeAll
-    public static void beforeAll() {
-        // 获取验证码
-        new EduGetCaptchaTests().shouldSuccess();
-        PandaTestDBHelpful.executeInsertOrUpdateOrDelete("delete from hp_user_edu_info where edu_email =\"1538680572@qq.com\"");
+    String assert1 = "module/redpacket_group_view/response/assert_full_field.json";
 
-    }
     @DisplayName("正向流程")
     @Test
     void shouldSuccess() {
         // 步骤1: 设置请求头。基本固定写法，不需要修改
         var requestHeaders = TestCaseHelpful.getHeaders(headers);
         requestHeaders.put("Authorization",TestCaseHelpful.login("17700000066","123456"));
-        Map<String,Object> selectedRow=PandaTestDBHelpful.executeSelectOneSql("select * from panda_test.user_log where content=\"153868057jwqu.com\" order by create_time desc limit 1");
-        Object verfiyCode=selectedRow.get("verifycode");
         // 步骤2: 设置请求体。基本固定写法，不需要修改
         var requestBody = TestCaseHelpful.getJsonRequestBody(body);
-        requestBody=TestCaseHelpful.updateJsonValue(requestBody,"pd.captcha",verfiyCode);
         // 如果请求有参数，则设置参数。基本固定写法，不需要修改
         var requestParams = TestCaseHelpful.getJsonRequestParams(params);
 
@@ -69,5 +54,4 @@ public class EduVerifyTests {
         TestCaseHelpful.assertThatJson(responseBody).when(Option.IGNORING_EXTRA_FIELDS).isEqualTo(expectedStr);
 
     }
-
-    }
+} 
