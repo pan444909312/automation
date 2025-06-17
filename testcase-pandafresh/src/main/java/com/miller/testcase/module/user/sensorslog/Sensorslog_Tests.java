c30a2ba6507d6b7d1ca9fdb40a25617af7c62ae6
zhangpei <zhangpei@hungrypandagroup.com>
2025-06-17 14:45:22 +0800

    b2b分类、商品、搜索常买、设备绑定、红包弹窗接口

--- testcase-pandafresh/src/main/java/com/miller/testcase/module/user/sensorslog/Sensorslog_Tests.java
+++ testcase-pandafresh/src/main/java/com/miller/testcase/module/user/sensorslog/Sensorslog_Tests.java
@@ -1,4 +1,4 @@
-package com.miller.testcase.module.redpacketpop;
+package com.miller.testcase.module.user.sensorslog;
 
 import com.miller.service.framework.annotation.Scenario;
 import com.miller.testcase.config.TestcaseConfig;
@@ -8,31 +8,31 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 /**
- * redpacketPop
+ * sensorsLog
  *
  * @author zhangpei
  * @version 2.0
- * @since 2025/06/17 11:39:46
+ * @since 2025/06/17 13:56:13
  */
 @Scenario(
-        scenarioID = "01JXXZHCT3ND16VC2SWEJR6CNP", // 自动生成，不要修改
-        scenarioName = "红包待使用弹窗",
+        scenarioID = "01JXY7B7GZ3Y84QN7S2C9GNV1B", // 自动生成，不要修改
+        scenarioName = "神策用户属性埋点",
         author = "zhangpei@hungrypandagroup.com", // 配置本机 Git email 后可自动生成
         developmentTime = 10, maintenanceTime = 0, manualTestTime = 3)
-@DisplayName("红包待使用弹窗")
-public class RedpacketPop_Tests {
+@DisplayName("神策用户属性埋点")
+public class Sensorslog_Tests {
     // TestcaseConfig.HOST 是接口的请求域名。 后面的 + "是接口的请求路径"
-    String uri = TestcaseConfig.HOST + "/redPacket/user/use/pop";
+    String uri = TestcaseConfig.HOST + "/system/sensorsLog";
     // 接口请求方式。如： GET、POST、PUT、DELETE
     String method = "POST";
     // 请求头。默认从 resources 目录下读取文件。
-    String headers = "module/redpacketpop/request/headers.json";
+    String headers = "module/sensorslog/request/headers.json";
     // 请求参数。如果没有传 null 即可（params = null）。比如 POST 请求通常没有 params 参数
     String params = null;
     // 请求体。如果没有传 null 即可（body = null）。比如 GET 请求可能没有请求体。作用同请求头
-    String body = "module/redpacketpop/request/should_success.json";
+    String body = "module/sensorslog/request/should_success.json";
     // 断言。默认从resources目录下读取文件。下面的代码表示从 resource 的 module/xxx/response/assert_full_field.json 读取文件内容作为断言
-    String assert1 = "module/redpacketpop/response/assert_full_field.json";
+    String assert1 = "module/sensorslog/response/assert_full_field.json";
 
     @DisplayName("正向流程")
     @Test
@@ -50,6 +50,7 @@ public class RedpacketPop_Tests {
 
         // 步骤3: 发起请求,并获取响应结果。基本固定写法，不需要修改
         var responseBody = TestCaseHelpful.sendRequest(method, uri, requestParams, requestHeaders, requestBody);
+
 
         TestCaseHelpful.assertThatJson(responseBody).inPath("$.code").isEqualTo(1);
         TestCaseHelpful.assertThatJson(responseBody).inPath("$.data").isNotNull();
