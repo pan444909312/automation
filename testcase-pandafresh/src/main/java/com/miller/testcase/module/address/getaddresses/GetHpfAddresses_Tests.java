0b262aca611a704a311d9d903d977fa5f94e05ce
zhangpei <zhangpei@hungrypandagroup.com>
2025-06-19 16:12:00 +0800

    收货地址、配送时间、红包（列表、数量、领取、订单红包）、定位、特殊单接口

--- testcase-pandafresh/src/main/java/com/miller/testcase/module/address/getaddresses/GetHpfAddresses_Tests.java
+++ testcase-pandafresh/src/main/java/com/miller/testcase/module/address/getaddresses/GetHpfAddresses_Tests.java
@@ -1,4 +1,4 @@
-package com.miller.testcase.module.getdynamicdiscount;
+package com.miller.testcase.module.address.getaddresses;
 
 import com.miller.service.framework.annotation.Scenario;
 import com.miller.testcase.config.TestcaseConfig;
@@ -7,31 +7,31 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 /**
- * getDynamicDiscount1
+ * getAddresses
  *
  * @author zhangpei
  * @version 2.0
- * @since 2025/06/17 18:13:59
+ * @since 2025/06/19 10:45:29
  */
 @Scenario(
-        scenarioID = "01JXYP37VKYE3GBTD04C8K9CVH", // 自动生成，不要修改
-        scenarioName = "首页凑单提示",
+        scenarioID = "01JY317E0C3WXMCYKN6SPHGJH3", // 自动生成，不要修改
+        scenarioName = "融合订单结算页获取收货地址",
         author = "zhangpei@hungrypandagroup.com", // 配置本机 Git email 后可自动生成
         developmentTime = 10, maintenanceTime = 0, manualTestTime = 3)
-@DisplayName("首页凑单提示")
-public class GetDynamicDiscount_Tests {
+@DisplayName("融合订单结算页获取收货地址")
+public class GetHpfAddresses_Tests {
     // TestcaseConfig.HOST 是接口的请求域名。 后面的 + "是接口的请求路径"
-    String uri = TestcaseConfig.HpfHost + "/index/getDynamicDiscount";
+    String uri = TestcaseConfig.HpfHost + "/address/hpf/getAddresses";
     // 接口请求方式。如： GET、POST、PUT、DELETE
     String method = "POST";
     // 请求头。默认从 resources 目录下读取文件。
-    String headers = "module/getdynamicdiscount/request/headers.json";
+    String headers = "module/gethpfaddresses/request/headers.json";
     // 请求参数。如果没有传 null 即可（params = null）。比如 POST 请求通常没有 params 参数
     String params = null;
     // 请求体。如果没有传 null 即可（body = null）。比如 GET 请求可能没有请求体。作用同请求头
-    String body = "module/getdynamicdiscount/request/should_success.json";
+    String body = "module/gethpfaddresses/request/body.json";
     // 断言。默认从resources目录下读取文件。下面的代码表示从 resource 的 module/xxx/response/assert_full_field.json 读取文件内容作为断言
-    String assert1 = "module/getdynamicdiscount/response/assert_full_field.json";
+    String assertFullField = "module/gethpfaddresses/response/assert_full_field.json";
 
     @DisplayName("正向流程")
     @Test
@@ -50,8 +50,9 @@ public class GetDynamicDiscount_Tests {
         // 步骤3: 发起请求,并获取响应结果。基本固定写法，不需要修改
         var responseBody = TestCaseHelpful.sendRequest(method, uri, requestParams, requestHeaders, requestBody);
 
+
         TestCaseHelpful.assertThatJson(responseBody).inPath("$.code").isEqualTo(1000);
-        TestCaseHelpful.assertThatJson(responseBody).inPath("$.result").isNotNull();
+        TestCaseHelpful.assertThatJson(responseBody).inPath("$.result.deliverableAddressList").isNotNull();
 
     }
 } 
