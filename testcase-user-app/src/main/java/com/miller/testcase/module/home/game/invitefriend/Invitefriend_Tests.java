4a14c730acd5529d5ed797e881643b85e88cf593
yaoqianhu <yaoqianhu@hungrypandagroup.com>
2025-06-25 18:47:40 +0800

    新增用例

--- testcase-user-app/src/main/java/com/miller/testcase/module/home/game/invitefriend/Invitefriend_Tests.java
+++ testcase-user-app/src/main/java/com/miller/testcase/module/home/game/invitefriend/Invitefriend_Tests.java
@@ -1,4 +1,4 @@
-package com.miller.testcase.module.home.game.gameshare;
+package com.miller.testcase.module.home.game.invitefriend;
 
 import com.miller.service.framework.annotation.Scenario;
 import com.miller.testcase.config.TestcaseConfig;
@@ -11,27 +11,27 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 /**
- * gameshare
+ * invitefriend
  *
  * @author yaoqianhu
  * @version 2.0
- * @since 2025/06/23 18:39:06
+ * @since 2025/06/24 17:22:10
  */
 @Scenario(
-        scenarioID = "01JYE5XGZHJMP639TZCDSF9WWR", // 自动生成，不要修改
-        scenarioName = "分享游戏页面",
+        scenarioID = "01JYGKXBS6A8W5A5TV44EMPMTB", // 自动生成，不要修改
+        scenarioName = "invitefriend",
         author = "yaoqianhu@hungrypandagroup.com", // 配置本机 Git email 后可自动生成
-        developmentTime = 40, maintenanceTime = 0, manualTestTime = 3)
-@DisplayName("分享游戏页面")
-public class Gameshare_Tests {
+        developmentTime = 30, maintenanceTime = 0, manualTestTime = 3)
+@DisplayName("invitefriend")
+public class Invitefriend_Tests {
 
     @BeforeAll
-    void beforeAll(){
+    static void beforeAll(){
         // 所有 @Test 方法执行之前会执行  @BeforeAll 注解的方法, 这里的代码当前测试类期间只会执行一次
         // 你可以在这里执行前置的操作，比如: SQL 初始化用例的前置条件
     }
     @AfterAll
-    void afterAll(){
+    static void afterAll(){
         // 所有 @Test 方法执行之后会执行  @@AfterAll 注解的方法, 这里的代码当前测试类期间只会执行一次
         // 你可以在这里执行后置的操作，比如: 销毁测试数据、还原数据库、清理环境等
     }
@@ -40,20 +40,21 @@ public class Gameshare_Tests {
     @Test
     void shouldSuccess() {
         // TestcaseConfig.HOST 是接口的请求域名。 后面的 + "是接口的请求路径"
-        String uri = TestcaseConfig.HOST_APP + "/api/user/activity/game/share";
+        String uri = TestcaseConfig.HOST_APP + "/api/user/activity/game/inviteFriend";
         // 接口请求方式。如： GET、POST、PUT、DELETE
         String method = "POST";
         // 请求头。默认从 resources 目录下读取文件。
-        String headers = "module/home/game/gameshare/request/headers.json";
+        String headers = "module/home/game/invitefriend/request/headers.json";
         // 请求参数。如果没有传 null 即可（params = null）。比如 POST 请求通常没有 params 参数
         String params = null;
         // 请求体。如果没有传 null 即可（body = null）。比如 GET 请求可能没有请求体。作用同请求头
-        String body = "module/home/game/gameshare/request/body.json";
+        String body = "module/home/game/invitefriend/request/body.json";
         // 断言。默认从resources目录下读取文件。下面的代码表示从 resource 的 module/xxx/response/assert_full_field.json 读取文件内容作为断言
-        String assertFullField = "module/home/game/gameshare/response/assert_full_field.json";
+        String assertFullField = "module/home/game/invitefriend/response/assert_full_field.json";
 
         // 步骤1: 设置请求头。基本固定写法，不需要修改
         var requestHeaders = TestCaseHelpful.getHeaders(headers);
+        requestHeaders.put("Authorization", TestCaseHelpful.login("13711111111", "111111"));
 
         // 步骤2: 设置请求体。基本固定写法，不需要修改
         var requestBody = TestCaseHelpful.getJsonRequestBody(body);
