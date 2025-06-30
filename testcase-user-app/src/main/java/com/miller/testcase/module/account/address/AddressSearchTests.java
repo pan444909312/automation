a2a8e2847edc0de9c0b461c881b154863d3a188e
HuYang <huyang@hungrypandagroup.com>
2025-06-30 11:26:10 +0800

    更新地址搜索接口断言

--- testcase-user-app/src/main/java/com/miller/testcase/module/account/address/AddressSearchTests.java
+++ testcase-user-app/src/main/java/com/miller/testcase/module/account/address/AddressSearchTests.java
@@ -47,6 +47,6 @@ public class AddressSearchTests {
         // 步骤4: 断言响应结果，直接拷贝抓包响应结果作为断言。基本固定写法，不需要修改
         // 方式二：全匹配，断言 实际结果 包含 预期结果,排除掉额外字段。固定写法，不需要修改
         var expectedStr = TestCaseHelpful.getFileContent(assert1);
-        TestCaseHelpful.assertThatJson(responseBody).when(Option.IGNORING_EXTRA_FIELDS,Option.IGNORING_EXTRA_ARRAY_ITEMS).isEqualTo(expectedStr);
+        TestCaseHelpful.assertThatJson(responseBody).when(Option.IGNORING_EXTRA_FIELDS,Option.IGNORING_EXTRA_ARRAY_ITEMS,Option.IGNORING_ARRAY_ORDER).isEqualTo(expectedStr);
     }
 }
