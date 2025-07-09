8537751e87f50670e99ad419634b965e6f55bdec
panjuxiang <panjuxiang@hungrypandagroup.com>
2025-07-09 15:46:58 +0800

    修改xxlConfig账号会测试公共账号

--- service/src/main/java/com/miller/service/util/XXLConfUtils.java
+++ service/src/main/java/com/miller/service/util/XXLConfUtils.java
@@ -28,8 +28,8 @@ public class XXLConfUtils {
 
     private static void login() {
         var params = new HashMap<String, Object>();
-        params.put("userName", "shandongdong");
-        params.put("password", "123456");
+        params.put("userName", "autotest");
+        params.put("password", "auto_test2025");
         params.put("ifRemember", "on");
 
         Map<String, Object> stringObjectMap = HttpUtils.sendPostRequest(XXL_CONFIG_URL + "/login", null, headers, params, null);
