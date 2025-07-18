75d4de4a6f3d6fc993cc16c47b61f046b3473390
panjuxiang <panjuxiang@hungrypandagroup.com>
2025-07-18 17:01:14 +0800

    商卡营销标排序新增2条case

---
+++ user-app/src/main/java/com/miller/userapp/dto/TypeMapping.java
@@ -0,0 +1,18 @@
+package com.miller.userapp.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @Author: panjuxiang
+ * @Since: 2025/7/18
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TypeMapping {
+    private String type;
+    private Integer sort;
+
+}
