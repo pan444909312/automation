f56e0892ea2f948dfd8d4d0aacabeb086f7e9cb7
panjuxiang <panjuxiang@hungrypandagroup.com>
2025-07-23 17:58:19 +0800

    新商卡营销标模块用例新增

---
+++ user-app/src/main/java/com/miller/userapp/module/shop/card/version3/home/feature/lastOrderNum/ShopShouldHasNoLastOrderNumFeature.java
@@ -0,0 +1,78 @@
+package com.miller.userapp.module.shop.card.version3.home.feature.lastOrderNum;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.hungrypanda.app.server.common.enums.index.shopFeature.ShopFeatureEnum;
+import com.hungrypanda.app.server.entity.data.KanbanPreviewDaysEntity;
+import com.hungrypanda.app.server.vo.index.ShopFeatureVO;
+import com.hungrypanda.app.server.vo.index.ShopIndexVO;
+import com.miller.service.framework.annotation.EnvTag;
+import com.miller.service.framework.annotation.Scenario;
+import com.miller.service.framework.util.PropertiesUtils;
+import com.miller.userapp.mapper.shop.KanbanPreviewDaysMapper;
+import com.miller.userapp.module.home.login.flow.UserLoginFlow;
+import com.miller.userapp.module.shop.card.version3.home.flow.ShopListFlow;
+import com.miller.userapp.module.shop.card.version3.home.request.ShopListRequestDTO;
+import com.miller.userapp.module.shop.card.version3.home.response.ShopListResponseDTO;
+import com.miller.userapp.util.RedisUtils;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Scenario(scenarioID = "01K0V7PH8ZT17GZDKXCQGMNKC5",
+        scenarioName = "普通店铺配送商卡-SKYX01_营销标_下单人数标签_不满足下单人数，不返回下单人数标签",
+        author = "panjuxiang@hungrypandagroup.com", developmentTime = 30, maintenanceTime = 0, manualTestTime = 10)
+@EnvTag.Test
+@DisplayName("商卡(中文)")
+public class ShopShouldHasNoLastOrderNumFeature {
+
+    private final Long shopId = Long.parseLong(new PropertiesUtils().getProperty(this.getClass(), "user.app.for.test.shop.card.version2.blank.compare.shopId"));
+
+    private KanbanPreviewDaysMapper kanbanPreviewDaysMapper;
+
+
+    @BeforeAll
+    void beforeAll() {
+        UserLoginFlow.loginByDefaultUser();
+        SqlSession sqlSession = com.miller.userapp.util.DBUtils.getDBOfPandaTest();
+        kanbanPreviewDaysMapper = sqlSession.getMapper(KanbanPreviewDaysMapper.class);
+    }
+
+    @MethodSource("staticDataProvider")
+    @ParameterizedTest
+    @DisplayName("普通店铺配送商卡-SKYX01_营销标_人下单人数标签_不满足下单人数，不返回下单人数标签")
+    void shouldExistEvaluationFeature(ShopListRequestDTO shopListRequestDTO) {
+
+        ShopListResponseDTO shopList = ShopListFlow.getShopListByShopId(shopListRequestDTO, shopId);
+        ShopIndexVO shopIndexVO = shopList.getResult().getShopList().stream()
+                .filter(item -> item.getShopId().equals(shopId)).findFirst().get();
+
+        ShopFeatureVO shopFeatureVO = shopIndexVO.getShopFeatureList().stream().
+                filter(item -> item.getType().equals(ShopFeatureEnum.LAST_DAY_ORDER.getType())).findFirst().orElse(null);
+
+        KanbanPreviewDaysEntity kanbanPreviewDaysEntity = kanbanPreviewDaysMapper.selectOne(new QueryWrapper<KanbanPreviewDaysEntity>().eq("shop_id", shopId));
+        Long shopOrderNum = (long) (kanbanPreviewDaysEntity == null ? 0 : kanbanPreviewDaysEntity.getOrderNum() * Double.valueOf(RedisUtils.getRedisInstance().get("USA:SHOP_LAST_DAY_ORDER_NUM").toString()));
+
+        assertThat(shopOrderNum > 50).isFalse();
+        assertThat(shopFeatureVO).isNull();
+    }
+
+
+    /**
+     * 测试用例数据提供者
+     */
+    static Stream<Arguments> staticDataProvider() {
+        ShopListRequestDTO shopListRequestDTO = new ShopListRequestDTO();
+        // 可以不用传参数
+        shopListRequestDTO.setFiltering(false);
+
+        return Stream.of(Arguments.of(shopListRequestDTO));
+    }
+
+}
