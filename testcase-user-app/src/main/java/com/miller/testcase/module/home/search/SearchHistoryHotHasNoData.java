5fe6c63712ac887ee344a8f3295eddb04a8c8aaa
panjuxiang <panjuxiang@hungrypandagroup.com>
2025-08-07 11:23:00 +0800

    用例新增1条

--- testcase-user-app/src/main/java/com/miller/testcase/module/home/search/SearchHistoryHotHasNoData.java
+++ testcase-user-app/src/main/java/com/miller/testcase/module/home/search/SearchHistoryHotHasNoData.java
@@ -10,21 +10,21 @@ import org.junit.jupiter.api.Test;
 import java.util.Map;
 
 
-@Scenario(scenarioID = "01JVKR6DPZH7V8WK4B381AMV6E",
-        scenarioName = "热搜词-有热搜词",
+@Scenario(scenarioID = "01K1ZEBC6JD5BR0TE3TN2SNZJR",
+        scenarioName = "热搜词-无热搜词",
         author = "panjuxiang@hungrypandagroup.com", developmentTime = 30, maintenanceTime = 0, manualTestTime = 5)
 @DisplayName("/api/user/search/history/hot")
-public class SearchHistoryHotSuccess {
+public class SearchHistoryHotHasNoData {
     private static final String uri = TestcaseConfig.HOST_APP + "/api/user/search/history/hot";
 
-    @DisplayName("热搜词-有热搜词")
+    @DisplayName("热搜词-无热搜词")
     @Test
     void shouldReturnSuccessfully() {
         Map<String, Object> headers = TestCaseHelpful.getHeaders("module/headers.json");
         // 给请求头添加数据，例如这里添加token
         headers.put("Authorization", TestCaseHelpful.login("13999900002", "123456"));
         String responseBody = TestCaseHelpful.sendRequest("GET", uri, null, headers, null);
-        String expectedStr = TestCaseHelpful.getFileContent("module/home/search/response/SearchHistoryHotResp.json");
+        String expectedStr = TestCaseHelpful.getFileContent("module/home/search/response/SearchHistoryHotHasNoDataResp.json");
 
         TestCaseHelpful.assertThatJson(responseBody).when(Option.IGNORING_EXTRA_FIELDS).isEqualTo(expectedStr);
     }
