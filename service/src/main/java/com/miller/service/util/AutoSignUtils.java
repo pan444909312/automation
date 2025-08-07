2109d884365292cae14a10bb07e061f1e48f24d2
panjuxiang <panjuxiang@hungrypandagroup.com>
2025-08-07 11:21:33 +0800

    验签工具bugfix

--- service/src/main/java/com/miller/service/util/AutoSignUtils.java
+++ service/src/main/java/com/miller/service/util/AutoSignUtils.java
@@ -1,6 +1,7 @@
 package com.miller.service.util;
 
 import com.alibaba.fastjson.JSONObject;
+import org.apache.commons.lang3.math.NumberUtils;
 
 import java.util.Map;
 import java.util.Objects;
@@ -10,10 +11,10 @@ import java.util.Objects;
  * @Since: 2025/7/15
  */
 public class AutoSignUtils {
-    private static String signAuthKey = "hP*L8pp65_#1flvjk342589fdgjl34m";
-    private static Long timeStamp = System.currentTimeMillis();
+    private static final String signAuthKey = "hP*L8pp65_#1flvjk342589fdgjl34m";
 
     public static Map<String, Object> signHandler(Map<String, Object> headers, Object body){
+        Long timeStamp = System.currentTimeMillis();
         headers.put("_ts", timeStamp);
         JSONObject requestJsonObject = new JSONObject();
         if (!Objects.isNull(body) && body instanceof String) {
@@ -22,6 +23,7 @@ public class AutoSignUtils {
         requestJsonObject.put("_ts", timeStamp);
 
         requestJsonObject.put("authorization", headers.get("authorization") == null ? "" : headers.get("authorization"));
+        requestJsonObject = convertBooleanToNumber(requestJsonObject);
         // 老签名处理
         String sign = SignGenerateUtil.getSign(requestJsonObject, signAuthKey);
         headers.put("_sign", sign);
@@ -31,5 +33,20 @@ public class AutoSignUtils {
         String sig = SignUtil.getSign(signAuthKey, requestJsonObject);
         headers.put("_sig", sig);
         return headers;
+    }
+
+    protected static JSONObject convertBooleanToNumber(JSONObject jsonObject) {
+        // 将true和false转换成0和1
+        for (String key : jsonObject.keySet()) {
+            String value = jsonObject.getString(key);
+            if (Objects.equals(value, Boolean.TRUE.toString())) {
+                jsonObject.put(key, NumberUtils.BYTE_ONE);
+                continue;
+            }
+            if (Objects.equals(value, Boolean.FALSE.toString())) {
+                jsonObject.put(key, NumberUtils.BYTE_ZERO);
+            }
+        }
+        return jsonObject;
     }
 }
