bd49ad83828a8757f0050df3d89d69d2c4271ff6
panjuxiang <panjuxiang@hungrypandagroup.com>
2025-08-08 15:19:26 +0800

    发红包工具新增调用记录

--- user-app/src/main/java/com/miller/userapp/module/data/promotion/redpacket/SendRedPacket.java
+++ user-app/src/main/java/com/miller/userapp/module/data/promotion/redpacket/SendRedPacket.java
@@ -1,14 +1,35 @@
 package com.miller.userapp.module.data.promotion.redpacket;
 
 import com.miller.common.util.DateUtils;
+import com.miller.service.framework.annotation.Scenario;
+import com.miller.service.framework.launcher.TestCaseRunnerLauncher;
 import com.miller.userapp.module.data.promotion.redpacket.flow.AddUserCdkeyFlow;
 import com.miller.userapp.module.data.promotion.redpacket.request.AddUserCdkeyRequestDTO;
 import com.miller.userapp.util.DBUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.joda.time.DateTime;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 import java.util.Date;
 
+@Slf4j
+@Scenario(scenarioID = "01K23RNF4T0TGF37NF0V5PDBHZ", scenarioName = "红包工厂_一键给指定用户发放红包",
+        author = "zhangcheng@hungrypandagroup.com",
+        developmentTime = 8 * 60, maintenanceTime = 0, manualTestTime = 10)
 public class SendRedPacket {
+
+    private void tearDown() {
+        new TestCaseRunnerLauncher().runTestMethod(CouponGenerate.class, "reportedData");
+        // 搜索索引更新
+    }
+
+    @Test
+    @DisplayName("一键给指定用户发放红包")
+    void reportedData() {
+        // 什么都不需要做，仅仅是作为数据上报，复用现在测试框架功能
+    }
+
     public static void main(String[] args) {
         String startTime = "2024-10-01 12:30:45";
         String endTime = "2024-11-01 12:30:45";
@@ -42,5 +63,6 @@ public class SendRedPacket {
         addUserCdkeyFlow.initSql();
         addUserCdkeyFlow.addCdKey(addUserCdkeyRequestDTO);
 
+        tearDown();
     }
 }
