7ba76b92e551a1cce903b13e210c10a2963b9195
panjuxiang <panjuxiang@hungrypandagroup.com>
2025-08-18 14:47:03 +0800

    添加日志

--- user-app/src/main/java/com/miller/userapp/module/data/user/db/UserSql.java
+++ user-app/src/main/java/com/miller/userapp/module/data/user/db/UserSql.java
@@ -6,8 +6,10 @@ import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.hungrypanda.app.server.entity.user.UserEntity;
 import com.miller.userapp.mapper.user.UserMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.session.SqlSession;
 
+@Slf4j
 public class UserSql {
     private SqlSession sqlSession;
     public  UserSql(SqlSession sqlSession){
@@ -27,7 +29,9 @@ public class UserSql {
         lambda.eq(UserEntity::getRoleId,0);
         lambda.eq(UserEntity::getIsDel,0);
         lambda.eq(UserEntity::getUserTelphone,tel);
-        return getUserMapper().selectOne(queryWrapper);
+        log.info("查询sqlSession信息：{}",sqlSession);
+        UserMapper userMapper = getUserMapper();
+        return userMapper.selectOne(queryWrapper);
     }
     public UserEntity getUser(Long userId){
         QueryWrapper<UserEntity> queryWrapper = new QueryWrapper<>();
