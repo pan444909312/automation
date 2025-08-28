8bf8a408bf0e341e4d70a96e98404c032e34944f
zhangpei <zhangpei@hungrypandagroup.com>
2025-08-28 16:46:27 +0800

    浮窗广告4条用例

--- testcase-pandafresh/src/main/java/com/miller/pandafresh/testcase/module/ad/getadoffloatwindow/GetAdOfFloatWindowStatusNotFit_Tests.java
+++ testcase-pandafresh/src/main/java/com/miller/pandafresh/testcase/module/ad/getadoffloatwindow/GetAdOfFloatWindowStatusNotFit_Tests.java
@@ -1,25 +1,56 @@
 package com.miller.pandafresh.testcase.module.ad.getadoffloatwindow;
 
-import com.miller.service.framework.annotation.Scenario;
 import com.miller.pandafresh.testcase.config.TestcaseConfig;
+import com.miller.pandafresh.testcase.utils.FreshTestDBHelpful;
 import com.miller.pandafresh.testcase.utils.TestCaseHelpful;
+import com.miller.service.framework.annotation.Scenario;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.Map;
 
 /**
  * getAdOfFloatWindow
  *
  * @author zhangpei
  * @version 2.0
- * @since 2025/06/18 16:20:10
+ * @since 2025/08/28 16:20:10
  */
 @Scenario(
-        scenarioID = "01JY11ZHH1SF954GDVARTHMJ6G", // 自动生成，不要修改
-        scenarioName = "融合首页浮窗：浮窗广告",
+        scenarioID = "01JY11ZHH1SF954GDVARTHMJ6H", // 自动生成，不要修改
+        scenarioName = "融合首页浮窗：浮窗广告-状态不符",
         author = "zhangpei@hungrypandagroup.com", // 配置本机 Git email 后可自动生成
         developmentTime = 10, maintenanceTime = 0, manualTestTime = 3)
-@DisplayName("融合首页浮窗：浮窗广告")
-public class GetAdOfFloatWindow_Tests {
+@DisplayName("融合首页浮窗：浮窗广告-状态不符")
+public class GetAdOfFloatWindowStatusNotFit_Tests {
+
+    Map<String, Object> selectOneSql;
+    @BeforeAll
+    void beforeAll() throws InterruptedException {
+        //查找渠道包含app的生效的所有用户可见的优先级最高的浮窗广告
+        //达达-1、h5-2、h5&达达-3、app-4、app&达达-5、app&h5-6、app&h5&达达-7
+        //广告位置 0 首页弹窗 1 个人中心中通广告 2 订单详情广告 3 支付成功广告 4-首页浮窗 5-个人中心金刚区 6-首页瀑布流广告
+        String sql = "SELECT * FROM ad_manage a WHERE a.ad_position=4 and a.is_del=0 and a.ad_status=1 " +
+                "and a.sync_web>3 and a.portal_id=3 and a.push_user!=1 and a.end_date>NOW()  ORDER BY a.sort DESC LIMIT 1;";
+        try {
+            // 查询1条记录
+            selectOneSql = FreshTestDBHelpful.executeSelectOneSql(sql);
+            String sql1 = "update ad_manage a set a.ad_status=0 where a.id="+selectOneSql.get("id")+"";
+            //将此广告的更新为禁用
+            FreshTestDBHelpful.executeInsertOrUpdateOrDelete(sql1);
+        }catch (Exception e){
+            System.out.println("无符合的浮窗广告数据！！");
+        }
+    }
+    @AfterAll
+    void afterAll(){
+        String sql2 = "update ad_manage a set a.ad_status=1 where a.id="+selectOneSql.get("id")+"";
+        //将此广告更新为启用
+        FreshTestDBHelpful.executeInsertOrUpdateOrDelete(sql2);
+    }
+
     // TestcaseConfig.HOST 是接口的请求域名。 后面的 + "是接口的请求路径"
     String uri = TestcaseConfig.HpfHost + "/ad/getAdOfFloatWindow";
     // 接口请求方式。如： GET、POST、PUT、DELETE
@@ -49,9 +80,7 @@ public class GetAdOfFloatWindow_Tests {
 
 
         TestCaseHelpful.assertThatJson(responseBody).inPath("$.code").isEqualTo(1000);
-        TestCaseHelpful.assertThatJson(responseBody).inPath("$.result.adVO").isNotNull();
-        TestCaseHelpful.assertThatJson(responseBody).inPath("$.result.adVO.pic").isNotNull();
-        TestCaseHelpful.assertThatJson(responseBody).inPath("$.result.adVO.deepLink").isNotNull();
+        TestCaseHelpful.assertThatJson(responseBody).inPath("$.result").isEqualTo("{}");
 
     }
 } 
