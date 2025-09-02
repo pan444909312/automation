ee1c195181cabdb8f61fc24c603786329963d7dd
yancancan <yancancan@hungrypandagroup.com>
2025-09-02 18:23:24 +0800

    1.新增里程碑3七条测试用例

--- testcase-user-app/src/main/java/com/miller/testcase/module/account/login/wx_register_combine_login/WXRiskControlLoginFailTests.java
+++ testcase-user-app/src/main/java/com/miller/testcase/module/account/login/wx_register_combine_login/WXRiskControlLoginFailTests.java
@@ -1,7 +1,8 @@
-package com.miller.testcase.module.account.login.combine_login;
+package com.miller.testcase.module.account.login.wx_register_combine_login;
 
 import com.miller.service.framework.annotation.Scenario;
 import com.miller.testcase.config.TestcaseConfig;
+import com.miller.testcase.utils.PandaTestDBHelpful;
 import com.miller.testcase.utils.TestCaseHelpful;
 import net.javacrumbs.jsonunit.core.Option;
 import org.junit.jupiter.api.AfterAll;
@@ -9,52 +10,72 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static com.miller.testcase.utils.TestCaseHelpful.getPhoneNumber;
+
 /**
- * combine login old user password fail
+ * wx register combine login
  *
  * @author yancancan
  * @version 2.0
- * @since 2025/07/10 16:41:41
+ * @since 2025/08/19 14:55:40
  */
 @Scenario(
-        scenarioID = "01JZSQYQXXNDA8QC1BSKWVA60Z", // 自动生成，不要修改
-        scenarioName = "combine login old user password fail",
+        scenarioID = "01K44MH6AQSSDE6X8FP9ZEG9AW", // 自动生成，不要修改
+        scenarioName = "微信小程序渠道：新账户注册失败（极验设备注册账号已达上限）",
         author = "yancancan@hungrypandagroup.com", // 配置本机 Git email 后可自动生成
         developmentTime = 10, maintenanceTime = 0, manualTestTime = 3)
-@DisplayName("combine login old user password fail:错误密码登陆")
-public class CombineLoginOldUserPasswordFailTests {
+@DisplayName("微信小程序渠道：新账户注册失败（极验设备注册账号已达上限）")
+public class WXRiskControlLoginFailTests {
 
     @BeforeAll
     static void beforeAll(){
         // 所有 @Test 方法执行之前会执行  @BeforeAll 注解的方法, 这里的代码当前测试类期间只会执行一次
         // 你可以在这里执行前置的操作，比如: SQL 初始化用例的前置条件
+        PandaTestDBHelpful.executeInsertOrUpdateOrDelete(
+                "UPDATE `risk_test`.`risk_control_rule` \n" +
+                        "SET  `param` = '{\"maxAllowed\":1,\"useDeviceIdType\":\"GEE\"}', \n" +
+                        "     `rule_status` = 'ENABLE'  \n" +
+                        "WHERE rule_code=\"device_sign_up_account_limit_rule\";"
+        );
+
     }
     @AfterAll
     static void afterAll(){
         // 所有 @Test 方法执行之后会执行  @@AfterAll 注解的方法, 这里的代码当前测试类期间只会执行一次
         // 你可以在这里执行后置的操作，比如: 销毁测试数据、还原数据库、清理环境等
+        PandaTestDBHelpful.executeInsertOrUpdateOrDelete(
+                "UPDATE `risk_test`.`risk_control_rule` \n" +
+                        "SET  `param` = '{\"maxAllowed\":10,\"useDeviceIdType\":\"GEE\"}', \n" +
+                        "     `rule_status` = 'ENABLE'  \n" +
+                        "WHERE rule_code=\"device_sign_up_account_limit_rule\";"
+        );
     }
 
     @DisplayName("正向流程")
     @Test
     void shouldSuccess() {
+        String tel="15900000010";
         // TestcaseConfig.HOST 是接口的请求域名。 后面的 + "是接口的请求路径"
         String uri = TestcaseConfig.HOST_APP + "/api/user/combine/login";
         // 接口请求方式。如： GET、POST、PUT、DELETE
         String method = "POST";
         // 请求头。默认从 resources 目录下读取文件。
-        String headers = "module/combine_login_old_user_password_fail/request/headers.json";
+        String headers = "module/account/login/wx_register_combine_login/request/headers.json";
         // 请求参数。如果没有传 null 即可（params = null）。比如 POST 请求通常没有 params 参数
         String params = null;
         // 请求体。如果没有传 null 即可（body = null）。比如 GET 请求可能没有请求体。作用同请求头
-        String body = "module/combine_login_old_user_password_fail/request/body.json";
+        String body = "module/account/login/wx_register_combine_login/request/body.json";
         // 断言。默认从resources目录下读取文件。下面的代码表示从 resource 的 module/xxx/response/assert_full_field.json 读取文件内容作为断言
-        String assertFullField = "module/combine_login_old_user_password_fail/response/assert_full_field.json";
+        String assertFullField = "module/account/login/wx_register_combine_login/response/assert_full_field.json";
 
         // 步骤1: 设置请求头。基本固定写法，不需要修改
         var requestHeaders = TestCaseHelpful.getHeaders(headers);
+
         // 步骤2: 设置请求体。基本固定写法，不需要修改
         var requestBody = TestCaseHelpful.getJsonRequestBody(body);
+        var verification=TestCaseHelpful.getVerificationCode(tel);
+        System.out.println("获取到的验证码"+verification);
+        requestBody = TestCaseHelpful.updateJsonValue(requestBody, "$.pd.verification",verification);
         // 如果请求有参数，则设置参数。基本固定写法，不需要修改
         var requestParams = TestCaseHelpful.getJsonRequestParams(params);
 
@@ -64,7 +85,8 @@ public class CombineLoginOldUserPasswordFailTests {
         // 步骤4: 断言响应结果，直接拷贝抓包响应结果作为断言。基本固定写法，不需要修改
         // 方式二：全匹配，断言 实际结果 包含 预期结果,排除掉额外字段。固定写法，不需要修改
         var expectedStr = TestCaseHelpful.getFileContent(assertFullField);
-        TestCaseHelpful.assertThatJson(responseBody).when(Option.IGNORING_EXTRA_FIELDS).isEqualTo(expectedStr);
+        TestCaseHelpful.assertThatJson(responseBody).when(Option.IGNORING_EXTRA_FIELDS).inPath("$.resultCode").isEqualTo(500);
+        TestCaseHelpful.assertThatJson(responseBody).when(Option.IGNORING_EXTRA_FIELDS).inPath("$.error").isEqualTo("该设备登录账号已达上限无法登录/注册");
 
     }
 } 
