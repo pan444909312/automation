7c8f4a5f25f36cd86833158566767a995a9d392f
panjuxiang <panjuxiang@hungrypandagroup.com>
2025-09-09 18:04:03 +0800

    商家-品类专题页用例初始化

---
+++ user-app/src/main/java/com/miller/userapp/module/shop/card/version3/category/promotion/sortRule/ShopShouldRedPacketSortNotMemberTests.java
@@ -0,0 +1,106 @@
+package com.miller.userapp.module.shop.card.version3.category.promotion.sortRule;
+
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
+import com.hungrypanda.app.server.entity.redpacket.UserCdKeyEntity;
+import com.hungrypanda.app.server.entity.user.UserLabelEntity;
+import com.hungrypanda.app.server.vo.index.ShopIndexVO;
+import com.hungrypanda.app.server.vo.index.ShopPromoteVO;
+import com.miller.common.util.MD5Util;
+import com.miller.service.framework.annotation.EnvTag;
+import com.miller.service.framework.annotation.Scenario;
+import com.miller.service.framework.util.PropertiesUtils;
+import com.miller.userapp.mapper.shop.ShopNewUserLabelMapper;
+import com.miller.userapp.module.data.activity.UserCdkeyInfoSql;
+import com.miller.userapp.module.data.device.db.DeviceAutoRenewSql;
+import com.miller.userapp.module.home.login.flow.UserLoginFlow;
+import com.miller.userapp.module.home.login.request.UserLoginRequestDTO;
+import com.miller.userapp.module.shop.card.version3.category.flow.ShopListFlow;
+import com.miller.userapp.module.shop.card.version3.category.request.ShopListRequestDTO;
+import com.miller.userapp.module.shop.card.version3.category.response.ShopListResponseDTO;
+import com.miller.userapp.util.DBUtils;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static com.miller.service.framework.util.JsonUnitUtils.assertThat;
+
+@Scenario(scenarioID = "01K4A1JR1R2CCWGVRDV3TXVK7N", scenarioName = "首页-商卡二期-SKYX实验组：红包类型标签-优先级（非会员默认排序）",
+        author = "yancancan@hungrypandagroup.com", developmentTime = 30, maintenanceTime = 0, manualTestTime = 15)
+@EnvTag.Test
+@DisplayName("首页-商卡二期-SKYX实验组：红包类型标签-优先级（非会员默认排序）")
+public class ShopShouldRedPacketSortNotMemberTests {
+     private final Long shopId = Long.parseLong(new PropertiesUtils().getProperty(this.getClass(), "user.app.for.test.shop.card.version3.increase.shopId"));
+    UserLoginRequestDTO userLoginRequestDTO;
+    private final Long userId = Long.parseLong(new PropertiesUtils().getProperty(this.getClass(), "user.app.for.test.shop.card.version3.sort.userId"));
+    UserCdKeyEntity userCdKeyEntity;
+     @BeforeAll
+    void beforeAll() {
+        String distinctId=new PropertiesUtils().getProperty(this.getClass(), "user.app.account.for.shop.card.version2.first.order.user.distinctId");
+        //   用户登录
+        userLoginRequestDTO = new UserLoginRequestDTO();
+        userLoginRequestDTO.setAccount(new PropertiesUtils().getProperty(this.getClass(), "user.app.for.test.shop.card.version3.sort.account"));
+        userLoginRequestDTO.setPassword(MD5Util.string2MD5(new PropertiesUtils().getProperty(this.getClass(), "user.app.for.test.shop.card.version3.sort.password")));
+        userLoginRequestDTO.setDistinctId(distinctId);
+        userLoginRequestDTO.setType(Integer.valueOf(new PropertiesUtils().getProperty(this.getClass(), "user.app.account.of.public.login.type")));
+        userLoginRequestDTO.setAreaCode(new PropertiesUtils().getProperty(this.getClass(), "user.app.account.of.user002.account.callingCode"));
+        UserLoginFlow.loginAndPutToken(userLoginRequestDTO);
+
+//        更新数据库，将user_label表数据label_id设置为2,使其出新人首单标签35
+         SqlSession sqlSession = DBUtils.getDBOfPandaTest();
+         ShopNewUserLabelMapper shopNewUserLabelMapper = sqlSession.getMapper(ShopNewUserLabelMapper.class);
+         shopNewUserLabelMapper.update(null, new LambdaUpdateWrapper<UserLabelEntity>().eq(UserLabelEntity::getDeviceId,distinctId).eq(UserLabelEntity::getUserId,userId).set(UserLabelEntity::getLabelId,2)
+         );
+         //清除设备对应的活动数据
+         DeviceAutoRenewSql deviceAutoRenewSql = new DeviceAutoRenewSql();
+         deviceAutoRenewSql.deviceAutoRenew(distinctId);
+
+         //神券信息修改:改为已膨胀10元
+         UserCdkeyInfoSql userCdkeyInfoSql = new UserCdkeyInfoSql();
+         userCdkeyInfoSql.updateRedPacketScopeTypeAndPrice(String.valueOf(userId),Long.parseLong(new PropertiesUtils().getProperty(this.getClass(), "user.app.for.test.shop.card.version3.redpacketId")),2,1000);
+
+     }
+     @AfterAll
+     void afterAll(){
+         //神券信息修改:改为待膨胀4元
+         UserCdkeyInfoSql userCdkeyInfoSql = new UserCdkeyInfoSql();
+         userCdkeyInfoSql.updateRedPacketScopeTypeAndPrice(String.valueOf(userId),Long.parseLong(new PropertiesUtils().getProperty(this.getClass(), "user.app.for.test.shop.card.version3.redpacketId")),1,400);
+
+     }
+     @MethodSource("staticDataProvider")
+    @ParameterizedTest
+    @DisplayName("首页-商卡二期-SKYX实验组：红包类型标签-优先级（非会员默认排序）")
+     void couponGodDsicount(ShopListRequestDTO shopListRequestDTO) {
+          ShopListResponseDTO shopList = ShopListFlow.getShopListByShopId(shopListRequestDTO,shopId);
+          ShopIndexVO shopIndexVO = shopList.getResult().getShopList().stream()
+                .filter(item -> item.getShopId().equals(shopId)).findFirst().get();
+         
+         
+         // 查找所有红包标签
+         List<ShopPromoteVO> shopPromoteVOList = shopIndexVO.getShopPromoteList().stream().
+                  filter(item -> item.getTagType().equals(1)).collect(Collectors.toList());
+         List<Integer>tags= shopPromoteVOList.stream().map(item -> item.getType()).collect(Collectors.toList());
+         // 顺序：新人、神券、店铺红包、会员红包
+         assertThat( tags).isEqualTo(new ArrayList<Integer>(List.of(35, 41, 42, 42, 32) ));
+
+     }
+
+/*
+          * 测试用例数据提供者
+     */
+    static Stream<Arguments> staticDataProvider() {
+        ShopListRequestDTO shopListRequestDTO = new ShopListRequestDTO();
+        // 可以不用传参数
+        shopListRequestDTO.setFiltering(false);
+        return Stream.of(Arguments.of(shopListRequestDTO));
+    }
+
+}
---
+++ user-app/src/main/java/com/miller/userapp/module/shop/card/version3/category/sideInfo/sendMoney/ShopShouldHasSendMoneyCDeliveryOffScenarioTests.java
@@ -0,0 +1,65 @@
+package com.miller.userapp.module.shop.card.version3.category.sideInfo.sendMoney;
+
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
+import com.hungrypanda.app.server.entity.address.CityFunctionConfigEntity;
+import com.hungrypanda.app.server.vo.index.ShopIndexVO;
+import com.miller.service.framework.annotation.EnvTag;
+import com.miller.service.framework.annotation.Scenario;
+import com.miller.service.framework.annotation.TestFramework;
+import com.miller.service.framework.util.PropertiesUtils;
+import com.miller.userapp.mapper.shop.CityFunctionConfigMapper;
+import com.miller.userapp.module.home.login.flow.UserLoginFlow;
+import com.miller.userapp.module.shop.card.version3.category.flow.ShopListFlow;
+import com.miller.userapp.module.shop.card.version3.category.request.ShopListRequestDTO;
+import com.miller.userapp.module.shop.card.version3.category.response.ShopListResponseDTO;
+import com.miller.userapp.util.DBUtils;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@EnvTag.Test
+
+@TestFramework
+@Scenario(scenarioID = "01K0P29TRKSGG3SQFTREWAWKNA", scenarioName = "用户-首页店铺流-商卡(中文)-普通店铺配送商卡-SKYX01-辅助信息-配送价格-首页-商卡二期：配送价格 - 运费减免优惠"
+        , author = "yancancan@hungrypandagroup.com", developmentTime = 30, maintenanceTime = 0, manualTestTime = 15)
+@DisplayName("用户-首页店铺流-商卡(中文)-普通店铺配送商卡-SKYX01-辅助信息-配送价格-首页-商卡二期：配送价格 - 运费减免优惠")
+public class ShopShouldHasSendMoneyCDeliveryOffScenarioTests {
+    //    测试店铺
+    private final Long shopId = Long.parseLong(new PropertiesUtils().getProperty(this.getClass(),"user.app.for.test.shop.card.version2.04.shopId"));
+
+    @BeforeAll
+    void beforeAll() {
+        UserLoginFlow.loginByDefaultUser();
+//        关闭地址配置-城市功能管理-九江市-商卡配送费优惠开关
+        SqlSession sqlSession = DBUtils.getDBOfPandaTest();
+        CityFunctionConfigMapper cityFunctionConfigMapper = sqlSession.getMapper(CityFunctionConfigMapper.class);
+        cityFunctionConfigMapper.update(null, new LambdaUpdateWrapper<CityFunctionConfigEntity>().eq(CityFunctionConfigEntity::getCityId,508).eq(CityFunctionConfigEntity::getType,9).set(CityFunctionConfigEntity::getStatus,0)
+        );
+
+    }
+    @DisplayName("用户-首页店铺流-商卡(中文)-普通店铺配送商卡-SKYX01-辅助信息-配送价格-首页-商卡二期：配送价格 - 运费减免优惠")
+    @MethodSource("showLabelDataProvider")
+    @ParameterizedTest
+    void hasSendMoneyInfo(ShopListRequestDTO ShopListRequestdto){
+        ShopListResponseDTO ShopListResponsedto = ShopListFlow.getShopListByShopId(ShopListRequestdto,shopId);
+        Integer sendMoney =ShopListResponsedto.getResult().getShopList().stream().filter(item -> item.getShopId().equals(shopId)).findFirst().map( ShopIndexVO::getSendMoney).orElseThrow();
+        String sendMoneyMsg =ShopListResponsedto.getResult().getShopList().stream().filter(item -> item.getShopId().equals(shopId)).findFirst().map( ShopIndexVO::getSendMoneyMsg).orElseThrow();
+        assertThat(sendMoney).isEqualTo(110);
+        assertThat(sendMoneyMsg).isEqualTo("配送¥1.1起");
+
+    }
+    //    DataProvider改为在测试用例文件里写,提供测试数据
+    static Stream<Arguments> showLabelDataProvider() {
+        ShopListRequestDTO shopListRequestDTO = new ShopListRequestDTO();
+        // 可以不用传参数
+        shopListRequestDTO.setFiltering(false); // 开发代码Bug，没有对 null 进行判断，应该默认给false的
+        return Stream.of(Arguments.of(shopListRequestDTO));
+    }
+}
