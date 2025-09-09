4f92ba6a6d478873c138b8a26eaa560cb232f8a3
yaoqianhu <yaoqianhu@hungrypandagroup.com>
2025-09-09 20:04:42 +0800

    新增用例

---
+++ testcase-user-app/src/main/java/com/miller/testcase/module/business/productdetail/product_自取.java
@@ -0,0 +1,44 @@
+package com.miller.testcase.module.business.productdetail;
+import com.miller.service.framework.annotation.Scenario;
+import com.miller.testcase.config.TestcaseConfig;
+import com.miller.testcase.utils.TestCaseHelpful;
+import net.javacrumbs.jsonunit.core.Option;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@Scenario(
+        scenarioID = "01K4PWH7CPB4FKQEEJF4QN3P61",
+        scenarioName = "商品详情页-商品取货方式：自取",
+        author = "yaoqianhu@hungrypandagroup.com",
+        developmentTime = 10, maintenanceTime = 0, manualTestTime = 10)
+@DisplayName("商品详情页-商品取货方式：自取")
+public class product_自取 {
+    // 接口请求的 path
+    String uri = TestcaseConfig.HOST_APP + "/api/user/v1/product/detail?productId=82542043&deliveryType=2";
+    // 请求方式
+    String method = "GET";
+    // 请求头
+    String headers = "module/headers.json";
+    // 请求体。如果没有传 null 即可（body = null）。比如 GET 请求
+    String body = null;
+    // 断言
+    String assert1 = "module/home/shop/productDetail/product_自取.json";
+
+    @DisplayName("正向流程")
+    @Test
+    public void getMenuListSuccessfully() {
+        // 步骤1: 设置请求头。基本固定写法，不需要修改
+        var requestHeaders = TestCaseHelpful.getHeaders(headers);
+
+        // 步骤2: 设置请求体。基本固定写法，不需要修改
+        var requestBody = TestCaseHelpful.getJsonRequestBody(body);
+
+        // 步骤3: 发起请求,并获取响应结果。基本固定写法，不需要修改
+        var responseBody = TestCaseHelpful.sendRequest(method, uri, null, requestHeaders, requestBody);
+
+        // 步骤4: 断言响应结果，直接拷贝抓包响应结果作为断言。基本固定写法，不需要修改
+        // 方式一： 全匹配， 忽略部分动态字段值。固定写法，不需要修改
+        var expectedStr = TestCaseHelpful.getFileContent(assert1);
+        TestCaseHelpful.assertThatJson(responseBody).when(Option.IGNORING_EXTRA_FIELDS).isEqualTo(expectedStr);
+    }
+}
---
+++ testcase-user-app/src/main/java/com/miller/testcase/module/business/productdetail/product_近期x人已下单.java
@@ -0,0 +1,52 @@
+package com.miller.testcase.module.business.productdetail;
+import com.miller.service.framework.annotation.Scenario;
+import com.miller.testcase.config.TestcaseConfig;
+import com.miller.testcase.utils.TestCaseHelpful;
+import net.javacrumbs.jsonunit.core.Option;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@Scenario(
+        scenarioID = "01K4Q0B3KBC9J3M19VDQCS88W2",
+        scenarioName = "商品详情页-商品标签：近期x人已下单",
+        author = "yaoqianhu@hungrypandagroup.com",
+        developmentTime = 15, maintenanceTime = 0, manualTestTime = 10)
+@DisplayName("商品详情页-商品标签：近期x人已下单")
+public class product_近期x人已下单 {
+    // 接口请求的 path
+    String uri = TestcaseConfig.HOST_APP + "/api/user/v1/product/detail?productId=82539065&deliveryType=1";
+    // 请求方式
+    String method = "GET";
+    // 请求头
+    String headers = "module/home/shop/request/headers_new.json";
+    // 请求体。如果没有传 null 即可（body = null）。比如 GET 请求
+    String body = null;
+    // 断言
+    String assert2 = "module/home/shop/productDetail/ProductBaseInfolResp.json";
+
+    String assert1 = "module/home/shop/response/product_近期x人已下单.json";
+    @DisplayName("正向流程")
+    @Test
+    public void getMenuListSuccessfully() {
+        // 步骤1: 设置请求头。基本固定写法，不需要修改
+        var requestHeaders = TestCaseHelpful.getHeaders(headers);
+
+        // 步骤2: 设置请求体。基本固定写法，不需要修改
+        var requestBody = TestCaseHelpful.getJsonRequestBody(body);
+
+        // 步骤3: 发起请求,并获取响应结果。基本固定写法，不需要修改
+        var responseBody = TestCaseHelpful.sendRequest(method, uri, null, requestHeaders, requestBody);
+
+        // 直接提取productId=82539065商品中tagType=1的标签
+        Object result = TestCaseHelpful.extractValue(responseBody,"$.result.productDetail.systemMarketingTags[*][?(@.tagType==4)]");
+
+        var expectedStr = TestCaseHelpful.getFileContent(assert2);
+        TestCaseHelpful.assertThatJson(responseBody).when(Option.IGNORING_EXTRA_FIELDS,Option.IGNORING_EXTRA_ARRAY_ITEMS).isEqualTo(expectedStr);
+        TestCaseHelpful.assertThat(result.toString()).isNotEqualTo("[]");
+
+        var expectedStr2 = TestCaseHelpful.getFileContent(assert1);
+        TestCaseHelpful.assertThatJson(result).when(Option.IGNORING_EXTRA_FIELDS,Option.IGNORING_EXTRA_ARRAY_ITEMS).isEqualTo(expectedStr2);
+
+
+    }
+}
