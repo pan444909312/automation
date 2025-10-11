26c1ae6225c470dd7ca2408badaba3eda5b04af5
panjuxiang <panjuxiang@hungrypandagroup.com>
2025-10-11 11:39:34 +0800

    搜索自动化用例新增12条

---
+++ testcase-user-app/src/main/java/com/miller/testcase/module/business/search/搜索店铺中的店铺特色字段_能匹配到.java
@@ -0,0 +1,45 @@
+package com.miller.testcase.module.business.search;
+
+import com.miller.service.framework.annotation.Scenario;
+import com.miller.testcase.config.TestcaseConfig;
+import com.miller.testcase.utils.TestCaseHelpful;
+import net.javacrumbs.jsonunit.core.Option;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+import java.util.Objects;
+
+
+@Scenario(scenarioID = "01K5DTWH3PPBRQYGB344PDGREK",
+        scenarioName = "搜索店铺中的店铺特色字段_能匹配到",
+        author = "panjuxiang@hungrypandagroup.com", developmentTime = 30, maintenanceTime = 0, manualTestTime = 5)
+@DisplayName("/api/user/v2/search")
+public class 搜索店铺中的店铺特色字段_能匹配到 {
+    private static final String uri = TestcaseConfig.HOST_APP + "/api/user/v2/search";
+
+    private String shopId = "623420023";
+
+    @DisplayName("搜索店铺中的店铺特色字段_能匹配到")
+    @Test
+    void shouldReturnSuccessfully() {
+        Map<String, Object> headers = TestCaseHelpful.getHeaders("module/headers.json");
+
+        // 沈阳经纬度
+        headers.put("latitude", 41.80478);
+        headers.put("longitude", 123.43297);
+        // 给请求头添加数据，例如这里添加token
+        String requestBody = TestCaseHelpful.getJsonRequestBody("module/home/search/request/SearchBaseReq.json");
+        String newRequestBody = TestCaseHelpful.updateJsonValue(requestBody, "$.keywords", "店铺特色");
+        String responseBody = TestCaseHelpful.searchGetShopVOByShopId(shopId, newRequestBody, headers, null, null, null, null);
+        String expectedStr = TestCaseHelpful.getFileContent("module/home/search/response/SearchBaseResp.json");
+
+
+        Object result1 = TestCaseHelpful.extractValue(responseBody, "$.result.shopList[?(@.shopId==" + shopId + ")]");
+        Object result2 = TestCaseHelpful.extractValue(responseBody, "$.result.shopList[?(@.shopId==" + shopId + ")].shopName");
+
+        TestCaseHelpful.assertThat(result1.toString()).isNotEqualTo("[]");
+        TestCaseHelpful.assertThat(result2.toString().contains("店铺特色")).isEqualTo(true);
+        TestCaseHelpful.assertThatJson(responseBody).when(Option.IGNORING_EXTRA_FIELDS).isEqualTo(expectedStr);
+    }
+}
