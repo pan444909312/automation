8ca35c86e4d9821b288cedd43b8670bf1b168a8c
WXH <wangxiaohao@hungrypanda.onaliyun.com>
2025-10-23 11:58:11 +0800

    ApiFox：新增工具效能统计 - v1

--- web/src/main/java/com/miller/controller/tools/ToolEfficiencyController.java
+++ web/src/main/java/com/miller/controller/tools/ToolEfficiencyController.java
@@ -5,23 +5,22 @@ import com.miller.service.tools.excel.ToolEfficiencyStatsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.net.http.HttpHeaders;
 import java.util.List;
 
 @RestController
-@RequestMapping("/automation/tools/a/")
+@RequestMapping("/automation/tools/toolEfficiencyStats/")
 public class ToolEfficiencyController {
 
     @Autowired
     private ToolEfficiencyStatsService toolEfficiencyStatsService;
 
-    @PostMapping("/toolEfficiencyStats/save")
+    @PostMapping("/saveOrUpdate")
     public ResultVO toolEfficiencyStats(@RequestBody ToolEfficiencyStatsEntity entity, @RequestHeader("executor") String executor) {
         toolEfficiencyStatsService.toolEfficiencyStatsSavaOrUpdate(entity,executor);
         return ResultVO.success();
     }
 
-    @GetMapping("/toolEfficiencyStats/all")
+    @GetMapping("/all")
     public ResultVO getAllToolEfficiencyStats() {
         List<ToolEfficiencyStatsEntity> allToolEfficiencyStats = toolEfficiencyStatsService.getAllToolEfficiencyStats();
         return ResultVO.success(allToolEfficiencyStats);
