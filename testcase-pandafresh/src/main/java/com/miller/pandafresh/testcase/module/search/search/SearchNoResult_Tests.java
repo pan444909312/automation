3d4efb52067e6ca65610b968f0ac55adb5b9abca
zhangpei <zhangpei@hungrypandagroup.com>
2025-10-24 17:29:07 +0800

    搜索用例

--- testcase-pandafresh/src/main/java/com/miller/pandafresh/testcase/module/search/search/SearchNoResult_Tests.java
+++ testcase-pandafresh/src/main/java/com/miller/pandafresh/testcase/module/search/search/SearchNoResult_Tests.java
@@ -1,4 +1,4 @@
-package com.miller.pandafresh.testcase.module.b2b.search.search;
+package com.miller.pandafresh.testcase.module.search.search;
 
 import com.miller.pandafresh.testcase.config.TestcaseConfig;
 import com.miller.pandafresh.testcase.utils.TestCaseHelpful;
@@ -17,14 +17,14 @@ import java.util.Map;
  *
  * @author zhangpei
  * @version 2.0
- * @since 2025/10/23 15:36:01
+ * @since 2025/07/17 19:56:04
  */
 @Scenario(
-        scenarioID = "01K87ZXZEXH7XJZ72PP3CYWYWE", // 自动生成，不要修改
-        scenarioName = "b2b搜索:无结果匹配",
+        scenarioID = "01K0C3VPR6T8F1HQ70SVSH28JZ", // 自动生成，不要修改
+        scenarioName = "搜索-搜索无结果",
         author = "zhangpei@hungrypandagroup.com", // 配置本机 Git email 后可自动生成
-        developmentTime = 15, maintenanceTime = 0, manualTestTime = 3)
-@DisplayName("b2b搜索:无结果匹配")
+        developmentTime = 20, maintenanceTime = 0, manualTestTime = 3)
+@DisplayName("搜索-搜索无结果")
 public class SearchNoResult_Tests {
 
     @BeforeAll
@@ -42,37 +42,33 @@ public class SearchNoResult_Tests {
     @Test
     void shouldSuccess() {
         // TestcaseConfig.HOST 是接口的请求域名。 后面的 + "是接口的请求路径"
-        String uri = TestcaseConfig.H5HOST + "/search/b2b/search";
+        String uri = TestcaseConfig.HpfHost + "/search/search";
         // 接口请求方式。如： GET、POST、PUT、DELETE
         String method = "POST";
         // 请求头。默认从 resources 目录下读取文件。
-        String headers = "module/b2b/search/search/request/headers.json";
+        String headers = "module/search/search/request/headers.json";
         // 请求参数。如果没有传 null 即可（params = null）。比如 POST 请求通常没有 params 参数
         String params = null;
         // 请求体。如果没有传 null 即可（body = null）。比如 GET 请求可能没有请求体。作用同请求头
-        String body = "module/b2b/search/search/request/body.json";
+        String body = "module/search/search/request/body.json";
         // 断言。默认从resources目录下读取文件。下面的代码表示从 resource 的 module/xxx/response/assert_full_field.json 读取文件内容作为断言
-        String assertFullField = "module/b2b/search/search/response/assert_full_field.json";
+        String assertFullField = "module/search/search/response/assert_full_field.json";
 
         // 步骤1: 设置请求头。基本固定写法，不需要修改
         var requestHeaders = TestCaseHelpful.getHeaders(headers);
 
         // 步骤2: 设置请求体。基本固定写法，不需要修改
         var requestBody = TestCaseHelpful.getJsonRequestBody(body);
-        requestBody = JSONUtils.updateJsonValueByPath(requestBody,"$.pd.keyword","弗负");
-
+        requestBody = JSONUtils.updateJsonValue(requestBody,"keyword","邑");
         // 如果请求有参数，则设置参数。基本固定写法，不需要修改
         var requestParams = TestCaseHelpful.getJsonRequestParams(params);
 
         // 步骤3: 发起请求,并获取响应结果。基本固定写法，不需要修改
         var responseBody = TestCaseHelpful.sendRequest(method, uri, requestParams, requestHeaders, requestBody);
 
-        // 步骤4: 断言响应结果，直接拷贝抓包响应结果作为断言。基本固定写法，不需要修改
-        // 方式二：全匹配，断言 实际结果 包含 预期结果,排除掉额外字段。固定写法，不需要修改
-        var expectedStr = TestCaseHelpful.getFileContent(assertFullField);
+        TestCaseHelpful.assertThatJson(responseBody).inPath("$.code").isEqualTo(1000);
 
-        TestCaseHelpful.assertThatJson(responseBody).inPath("$.code").isEqualTo(1);
-        TestCaseHelpful.assertThatJson(responseBody).inPath("$.data.records").isEqualTo("[]");
+        TestCaseHelpful.assertThatJson(responseBody).inPath("$.result.records").isEqualTo("[]");
 
     }
 } 
