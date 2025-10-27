e30a7e0c02895e3244879ae8e1cbd1e38c70632a
zhangpei <zhangpei@hungrypandagroup.com>
2025-10-27 15:43:20 +0800

    B2B账期支付订单用例

--- testcase-pandafresh/src/main/java/com/miller/pandafresh/testcase/module/b2b/order/tocreateb2bordervirtual/ToCreateB2bOrderVirtualImmediate_Tests.java
+++ testcase-pandafresh/src/main/java/com/miller/pandafresh/testcase/module/b2b/order/tocreateb2bordervirtual/ToCreateB2bOrderVirtualImmediate_Tests.java
@@ -2,14 +2,7 @@ package com.miller.pandafresh.testcase.module.b2b.order.tocreateb2bordervirtual;
 
 import com.miller.pandafresh.testcase.module.b2b.add.AddGoods;
 import com.miller.service.framework.annotation.Scenario;
-import com.miller.pandafresh.testcase.config.TestcaseConfig;
-import com.miller.pandafresh.testcase.utils.PandaTestDBHelpful;
-import com.miller.pandafresh.testcase.utils.TestCaseHelpful;
-import net.javacrumbs.jsonunit.core.Option;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
 
@@ -22,7 +15,7 @@ import org.junit.platform.suite.api.Suite;
  */
 @SelectClasses({
         AddGoods.class,
-        ToCreateB2bOrderVirtual.class
+        ToCreateB2bOrderVirtualImmediate.class
 })
 @Scenario(
         scenarioID = "01K8A5GM02D7SSC8ZKS6AZ3T0B", // 自动生成，不要修改
@@ -31,6 +24,6 @@ import org.junit.platform.suite.api.Suite;
         developmentTime = 30, maintenanceTime = 0, manualTestTime = 3)
 @DisplayName("b2b结算页：立即支付订单")
 @Suite
-public class ToCreateB2bOrderVirtual_Tests {
+public class ToCreateB2bOrderVirtualImmediate_Tests {
 
 } 
