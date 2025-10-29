e8e3000ed0f16050ed1d98662ad67877e288ced3
panjuxiang <panjuxiang@hungrypandagroup.com>
2025-10-29 17:18:10 +0800

    测试

--- deploy.sh
+++ deploy.sh
@@ -52,7 +52,7 @@ health_check() {
 start_application() {
     echo "starting java process"
     export ENV_VAR="prod"
-    nohup java ${ERP_SERVER_JVM} -jar ${JAR_NAME} --spring.profiles.active=prod >> ${JAVA_OUT} 2>&1 &
+    nohup java ${ERP_SERVER_JVM} -jar ${JAR_NAME} >> ${JAVA_OUT} 2>&1 &
     echo "started java process"
 }
 
