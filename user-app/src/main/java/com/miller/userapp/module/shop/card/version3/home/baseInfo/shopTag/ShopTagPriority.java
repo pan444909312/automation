1e3e43281903985f939fdaba9a5c41c3141310fb
panjuxiang <panjuxiang@hungrypandagroup.com>
2025-11-11 13:50:05 +0800

    商卡用例新增1条 维护1条

--- user-app/src/main/java/com/miller/userapp/module/shop/card/version3/home/baseInfo/shopTag/ShopTagPriority.java
+++ user-app/src/main/java/com/miller/userapp/module/shop/card/version3/home/baseInfo/shopTag/ShopTagPriority.java
@@ -10,9 +10,7 @@ import com.miller.userapp.module.home.login.request.UserLoginRequestDTO;
 import com.miller.userapp.module.shop.card.version3.home.flow.ShopListFlow;
 import com.miller.userapp.module.shop.card.version3.home.request.ShopListRequestDTO;
 import com.miller.userapp.module.shop.card.version3.home.response.ShopListResponseDTO;
-import com.miller.userapp.util.PandaTestDBHelpful;
 import com.miller.userapp.util.RequestUtils;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -23,14 +21,14 @@ import java.util.stream.Stream;
 
 import static com.miller.service.framework.util.JsonUnitUtils.assertThat;
 
-@Scenario(scenarioID = "01K99BZNCN9YS2P3JC5AX6805W",
-        scenarioName = "普通店铺配送商卡-SKYX01_基础信息_店前标签：会场模版标签_首页-商卡二期：会场模版标签 - 会场关闭，不返回标签",
+@Scenario(scenarioID = "01K9RPEEDGAQ531N4DDMXMNTZ2",
+        scenarioName = "普通店铺配送商卡-SKYX01_基础信息_店前标签优先级-模板标签大于百万补贴标签",
         author = "panjuxiang@hungrypandagroup.com", developmentTime = 30, maintenanceTime = 0, manualTestTime = 15)
 
 @EnvTag.Test
 @DisplayName("商卡(中文)")
-public class ShopShouldHasNoVenueTagCauseVenueClose {
-    private final Long shopId = 633077616L;
+public class ShopTagPriority {
+    private final Long shopId = 880360312L;
 
 
     @BeforeAll
@@ -43,20 +41,12 @@ public class ShopShouldHasNoVenueTagCauseVenueClose {
         userLoginRequestDTO.setType(Integer.valueOf(new PropertiesUtils().getProperty(UserLoginFlow.class, "user.app.account.of.public.login.type")));
         UserLoginFlow.loginAndPutToken(userLoginRequestDTO);
 
-
-        PandaTestDBHelpful.executeInsertOrUpdateOrDelete("update hp_venue set venue_status = 0 where id = 411");
-
-    }
-
-    @AfterAll
-    void afterAll() {
-        PandaTestDBHelpful.executeInsertOrUpdateOrDelete("update hp_venue set venue_status = 2 where id = 411");
     }
 
 
     @MethodSource("staticDataProvider")
     @ParameterizedTest
-    @DisplayName("普通店铺配送商卡-SKYX01_基础信息_店前标签：会场模版标签_首页-商卡二期：会场模版标签 - 会场关闭，不返回标签")
+    @DisplayName("普通店铺配送商卡-SKYX01_基础信息_店前标签优先级-模板标签大于百万补贴标签")
     void memberBenefitDeliveryDsicount(ShopListRequestDTO shopListRequestDTO) {
 
         // 经纬度为沈阳
@@ -67,7 +57,9 @@ public class ShopShouldHasNoVenueTagCauseVenueClose {
         ShopIndexVO shopIndexVO = shopList.getResult().getShopList().stream()
                 .filter(item -> item.getShopId().equals(shopId)).findFirst().get();
 
-        assertThat(shopIndexVO.getActivityTagType()).isNotEqualTo(6);
+        assertThat(shopIndexVO.getActivityTag()).isNotNull();
+        assertThat(shopIndexVO.getActivityTag()).isNotEqualTo("");
+        assertThat(shopIndexVO.getActivityTagType()).isEqualTo(6);
 
     }
 
