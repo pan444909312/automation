7cdc47b44e7e6bd1169242be05ca32d0b1d3e8dc
WXH <wangxiaohao@hungrypanda.onaliyun.com>
2025-11-11 19:31:28 +0800

    ApiFox：getDBOfApifox 添加打印日志

--- service/src/main/java/com/miller/service/util/ApifoxDBUtils.java
+++ service/src/main/java/com/miller/service/util/ApifoxDBUtils.java
@@ -3,11 +3,13 @@ package com.miller.service.util;
 import com.miller.service.framework.db.mybatis.DataSourceConfig;
 import com.miller.service.framework.db.mybatis.MyBatisPlusConfig;
 import com.miller.service.framework.util.PropertiesUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@Slf4j
 public class ApifoxDBUtils {
 
     private static SqlSession sqlSessionOfApifox;
@@ -24,6 +26,10 @@ public class ApifoxDBUtils {
         var mySqlUrl = new PropertiesUtils().getProperty(com.miller.service.util.ApifoxDBUtils.class, "datasource.url.apifox");
         var userName = new PropertiesUtils().getProperty(com.miller.service.util.ApifoxDBUtils.class, "datasource.username.apifox");
         var passWord = new PropertiesUtils().getProperty(com.miller.service.util.ApifoxDBUtils.class, "datasource.password.apifox");
+        log.info("mySqlUrl:".concat(mySqlUrl));
+        log.info("userName:".concat(userName));
+        log.info("passWord:".concat(passWord));
+
         var myBatisPlusConfig = new MyBatisPlusConfig();
         sqlSessionOfApifox = myBatisPlusConfig.getSqlSession(new DataSourceConfig(mySqlUrl, userName, passWord).getDataSource(), com.miller.service.util.ApifoxDBUtils.class);
         return sqlSessionOfApifox;
