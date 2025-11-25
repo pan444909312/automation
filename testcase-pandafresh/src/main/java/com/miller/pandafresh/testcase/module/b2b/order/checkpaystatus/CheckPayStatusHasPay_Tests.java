757ca5401c5f6d5ee42fa70779cbe9f951c2c3f0
zhangpei <zhangpei@hungrypandagroup.com>
2025-11-25 17:59:04 +0800

    再来一单、支付状态、下载invoice等5条用例

---
+++ testcase-pandafresh/src/main/java/com/miller/pandafresh/testcase/module/b2b/order/checkpaystatus/CheckPayStatusHasPay_Tests.java
@@ -0,0 +1,76 @@
+package com.miller.pandafresh.testcase.module.b2b.order.checkpaystatus;
+
+import com.miller.pandafresh.testcase.config.TestcaseConfig;
+import com.miller.pandafresh.testcase.utils.FreshTestDBHelpful;
+import com.miller.pandafresh.testcase.utils.TestCaseHelpful;
+import com.miller.service.framework.annotation.Scenario;
+import com.miller.service.framework.util.JSONUtils;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+/**
+ * checkPayStatus
+ *
+ * @author zhangpei
+ * @version 2.0
+ * @since 2025/11/25 17:33:51
+ */
+@Scenario(
+        scenarioID = "01KAX5SEE0YYVRZCCCZFQGSQ7T", // 自动生成，不要修改
+        scenarioName = "b2b订单-检查支付状态：订单已支付",
+        author = "zhangpei@hungrypandagroup.com", // 配置本机 Git email 后可自动生成
+        developmentTime = 20, maintenanceTime = 0, manualTestTime = 3)
+@DisplayName("b2b订单-检查支付状态：订单已支付")
+public class CheckPayStatusHasPay_Tests {
+
+    static Map<String, Object> selectOneSql;
+    @BeforeAll
+    static void beforeAll(){
+        //查询已支付订单
+        String sql = "SELECT * FROM b2b_order o WHERE o.order_version>=1 and o.pay_way_type=1 and o.pay_status=1 and o.custom_id=8 limit 1;";
+        selectOneSql = FreshTestDBHelpful.executeSelectOneSql(sql);
+    }
+    @AfterAll
+    static void afterAll(){
+        // 所有 @Test 方法执行之后会执行  @@AfterAll 注解的方法, 这里的代码当前测试类期间只会执行一次
+        // 你可以在这里执行后置的操作，比如: 销毁测试数据、还原数据库、清理环境等
+    }
+
+    @DisplayName("正向流程")
+    @Test
+    void shouldSuccess() {
+        // TestcaseConfig.HOST 是接口的请求域名。 后面的 + "是接口的请求路径"
+        String uri = TestcaseConfig.H5HOST + "/api/b2b/order/checkPayStatus";
+        // 接口请求方式。如： GET、POST、PUT、DELETE
+        String method = "POST";
+        // 请求头。默认从 resources 目录下读取文件。
+        String headers = "module/b2b/order/checkpaystatus/request/headers.json";
+        // 请求参数。如果没有传 null 即可（params = null）。比如 POST 请求通常没有 params 参数
+        String params = null;
+        // 请求体。如果没有传 null 即可（body = null）。比如 GET 请求可能没有请求体。作用同请求头
+        String body = "module/b2b/order/checkpaystatus/request/body.json";
+        // 断言。默认从resources目录下读取文件。下面的代码表示从 resource 的 module/xxx/response/assert_full_field.json 读取文件内容作为断言
+        String assertFullField = "module/b2b/order/checkpaystatus/response/assert_full_field.json";
+
+        // 步骤1: 设置请求头。基本固定写法，不需要修改
+        var requestHeaders = TestCaseHelpful.getHeaders(headers);
+        //登录用户
+        requestHeaders.put("authorization",TestCaseHelpful.loginB2B("17700004444","888888"));
+
+        // 步骤2: 设置请求体。基本固定写法，不需要修改
+        var requestBody = TestCaseHelpful.getJsonRequestBody(body);
+        requestBody = JSONUtils.updateJsonValueByPath(requestBody,"$.pd.orderSn",selectOneSql.get("order_sn"));
+        // 如果请求有参数，则设置参数。基本固定写法，不需要修改
+        var requestParams = TestCaseHelpful.getJsonRequestParams(params);
+
+        // 步骤3: 发起请求,并获取响应结果。基本固定写法，不需要修改
+        var responseBody = TestCaseHelpful.sendRequest(method, uri, requestParams, requestHeaders, requestBody);
+
+        TestCaseHelpful.assertThatJson(responseBody).inPath("$.code").isEqualTo(505);
+        TestCaseHelpful.assertThatJson(responseBody).inPath("$.message").isEqualTo("该订单已确认付款，请联系客服(505)");
+    }
+} 
