00fa7de1f41a7386a90720416faaa345efeeb790
panjuxiang <panjuxiang@hungrypandagroup.com>
2025-11-26 18:29:59 +0800

    代码优化

--- testcase-user-app/src/main/java/com/miller/testcase/utils/TestCaseHelpful.java
+++ testcase-user-app/src/main/java/com/miller/testcase/utils/TestCaseHelpful.java
@@ -272,25 +272,25 @@ public class TestCaseHelpful {
                 body = JSONUtils.toJSONString(JSONUtils.parseObject(body.toString()).toJavaObject(Map.class));
             }
         }
-        // 处理验签
-        headers.put("_ts", timeStamp);
-        JSONObject requestJsonObject = new JSONObject();
-        if (!Objects.isNull(body) && body instanceof String) {
-            requestJsonObject = JSONObject.parseObject((String) body);
-        }
-        requestJsonObject.put("_ts", timeStamp);
-
-        requestJsonObject.put("authorization", headers.get("authorization") == null ? "" : headers.get("authorization"));
-        // 老签名处理
-        String sign = SignGenerateUtil.getSign(requestJsonObject, signAuthKey);
-        headers.put("_sign", sign);
-
-        // 新签名处理（老签名使用新的加密方法，且需要比老前面多传一个platform进行加密）
-        requestJsonObject.put("platform", headers.get("platform"));
-        String sig = SignUtil.getSign(signAuthKey, requestJsonObject);
-        headers.put("_sig", sig);
-
-        headers.put("enableSign", true);
+//        // 处理验签
+//        headers.put("_ts", timeStamp);
+//        JSONObject requestJsonObject = new JSONObject();
+//        if (!Objects.isNull(body) && body instanceof String) {
+//            requestJsonObject = JSONObject.parseObject((String) body);
+//        }
+//        requestJsonObject.put("_ts", timeStamp);
+//
+//        requestJsonObject.put("authorization", headers.get("authorization") == null ? "" : headers.get("authorization"));
+//        // 老签名处理
+//        String sign = SignGenerateUtil.getSign(requestJsonObject, signAuthKey);
+//        headers.put("_sign", sign);
+//
+//        // 新签名处理（老签名使用新的加密方法，且需要比老前面多传一个platform进行加密）
+//        requestJsonObject.put("platform", headers.get("platform"));
+//        String sig = SignUtil.getSign(signAuthKey, requestJsonObject);
+//        headers.put("_sig", sig);
+//
+//        headers.put("enableSign", true);
 
 
         method = method.toUpperCase();
