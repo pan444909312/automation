e2d7f6fe764bbba9bd481e3e093946a2d987f1e9
panjuxiang <panjuxiang@hungrypandagroup.com>
2025-12-03 18:08:54 +0800

    xxl测试

--- service/src/main/java/com/miller/service/util/XXLConfUtils.java
+++ service/src/main/java/com/miller/service/util/XXLConfUtils.java
@@ -50,10 +50,8 @@ public class XXLConfUtils {
 
     private static void login() {
         var params = new HashMap<String, Object>();
-        params.put("userName", "nianruifeng");
-//        params.put("userName", "autotest");
-        params.put("password", "123456");
-//        params.put("password", "auto_test2025");
+        params.put("userName", "autotest");
+        params.put("password", "auto_test2025");
         params.put("ifRemember", "on");
 
         Map<String, Object> stringObjectMap = HttpUtils.sendPostRequest(XXL_CONFIG_URL + "/login", null, headers, params, null);
