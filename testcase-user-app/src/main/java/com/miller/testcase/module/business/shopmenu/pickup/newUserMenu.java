547e4cdc81be6813a553d3003c609ad8f0d0f041
yaoqianhu <yaoqianhu@hungrypandagroup.com>
2025-12-05 17:56:32 +0800

    新增用例

---
+++ testcase-user-app/src/main/java/com/miller/testcase/module/business/shopmenu/pickup/newUserMenu.java
@@ -0,0 +1,45 @@
+package com.miller.testcase.module.business.shopmenu.pickup;
+import com.miller.service.framework.annotation.Scenario;
+import com.miller.testcase.config.TestcaseConfig;
+import com.miller.testcase.utils.TestCaseHelpful;
+import net.javacrumbs.jsonunit.core.Option;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@Scenario(
+        scenarioID = "01KBPXSVCMASZXWQ0FH463YTA4",
+        scenarioName = "进入店铺获取自取列表特殊菜单-新客专享菜单",
+        author = "yaoqianhu@hungrypandagroup.com",
+        developmentTime = 30, maintenanceTime = 0, manualTestTime = 10)
+@DisplayName("进入店铺获取自取列表特殊菜单-新客专享菜单")
+public class newUserMenu {
+    // 接口请求的 path
+    String uri = TestcaseConfig.HOST_APP + "/api/app/user/v1/shop/menuList";
+    // 请求方式
+    String method = "POST";
+    // 请求头
+    String headers = "module/headers.json";
+    // 请求体。如果没有传 null 即可（body = null）。比如 GET 请求
+    String body = "module/home/shop/request_pickup/DelivryMenulistReq.json";
+    // 断言
+    String assert2 = "module/home/shop/response/newUserMenuResp.json";
+
+    @DisplayName("正向流程")
+    @Test
+    public void getMenuListSuccessfully() {
+        // 步骤1: 设置请求头。基本固定写法，不需要修改
+        var requestHeaders = TestCaseHelpful.getHeaders(headers);
+
+        // 步骤2: 设置请求体。基本固定写法，不需要修改
+        var requestBody = TestCaseHelpful.getJsonRequestBody(body);
+
+        // 步骤3: 发起请求,并获取响应结果。基本固定写法，不需要修改
+        var responseBody = TestCaseHelpful.sendRequest(method, uri, null, requestHeaders, requestBody);
+        Object result = TestCaseHelpful.extractValue(responseBody,"$.result.menuList[0].subMenuList[?(@.menuInfo.menuType==10)].menuInfo");
+        // 步骤4: 断言响应结果，直接拷贝抓包响应结果作为断言。基本固定写法，不需要修改
+       // 方式二：全匹配，断言 实际结果 包含 预期结果,排除掉额外字段。固定写法，不需要修改
+        var expectedStr = TestCaseHelpful.getFileContent(assert2);
+        TestCaseHelpful.assertThatJson(responseBody).when(Option.IGNORING_EXTRA_FIELDS,Option.IGNORING_EXTRA_ARRAY_ITEMS).isEqualTo(expectedStr);
+
+    }
+}
---
+++ testcase-user-app/src/main/java/com/miller/testcase/module/business/shopmenu/pickup/product_上线.java
@@ -0,0 +1,52 @@
+package com.miller.testcase.module.business.shopmenu.pickup;
+import com.miller.service.framework.annotation.Scenario;
+import com.miller.testcase.config.TestcaseConfig;
+import com.miller.testcase.utils.TestCaseHelpful;
+import net.javacrumbs.jsonunit.core.Option;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@Scenario(
+        scenarioID = "01K4ACFB4JTYQX609X645CPM4T",
+        scenarioName = "自取列表商卡-商品信息-商品状态：上线",
+        author = "yaoqianhu@hungrypandagroup.com",
+        developmentTime = 10, maintenanceTime = 0, manualTestTime = 10)
+@DisplayName("自取列表商卡-商品信息-商品状态：上线")
+public class product_上线 {
+    // 接口请求的 path
+    String uri = TestcaseConfig.HOST_APP + "/api/app/user/v1/shop/menuList";
+    // 请求方式
+    String method = "POST";
+    // 请求头
+    String headers = "module/headers.json";
+    // 请求体。如果没有传 null 即可（body = null）。比如 GET 请求
+    String body = "module/home/shop/request_pickup/DelivryMenulistReq.json";
+    // 断言
+    String assert2 = "module/home/shop/response/product_info.json";
+
+    String assert1 = "module/home/shop/response/product_上线.json";
+    @DisplayName("正向流程")
+    @Test
+    public void getMenuListSuccessfully() {
+        // 步骤1: 设置请求头。基本固定写法，不需要修改
+        var requestHeaders = TestCaseHelpful.getHeaders(headers);
+
+        // 步骤2: 设置请求体。基本固定写法，不需要修改
+        var requestBody = TestCaseHelpful.getJsonRequestBody(body);
+
+        // 步骤3: 发起请求,并获取响应结果。基本固定写法，不需要修改
+        var responseBody = TestCaseHelpful.sendRequest(method, uri, null, requestHeaders, requestBody);
+
+        Object result = TestCaseHelpful.extractValue(responseBody,"$.result.menuList[0].subMenuList[*].productList[?(@.productId==82537906)]");        // 步骤4: 断言响应结果，直接拷贝抓包响应结果作为断言。基本固定写法，不需要修改
+        // 方式二：全匹配，断言 实际结果 包含 预期结果,排除掉额外字段。固定写法，不需要修改
+        var expectedStr = TestCaseHelpful.getFileContent(assert2);
+        TestCaseHelpful.assertThatJson(responseBody).when(Option.IGNORING_EXTRA_FIELDS,Option.IGNORING_EXTRA_ARRAY_ITEMS).isEqualTo(expectedStr);
+        TestCaseHelpful.assertThat(result.toString()).isNotEqualTo("[]");
+
+        var expectedStr2 = TestCaseHelpful.getFileContent(assert1);
+        TestCaseHelpful.assertThatJson(result).when(Option.IGNORING_EXTRA_FIELDS,Option.IGNORING_EXTRA_ARRAY_ITEMS).isEqualTo(expectedStr2);
+
+
+    }
+}
+
