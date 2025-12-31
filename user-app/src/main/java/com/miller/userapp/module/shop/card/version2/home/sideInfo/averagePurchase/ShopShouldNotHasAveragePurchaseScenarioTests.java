f053fd45704cac193deb2345b564abec6ad1c0c5
yancancan <yancancan@hungrypandagroup.com>
2025-12-31 11:50:56 +0800

    1.商卡用例增加等待时间

--- user-app/src/main/java/com/miller/userapp/module/shop/card/version2/home/sideInfo/averagePurchase/ShopShouldNotHasAveragePurchaseScenarioTests.java
+++ user-app/src/main/java/com/miller/userapp/module/shop/card/version2/home/sideInfo/averagePurchase/ShopShouldNotHasAveragePurchaseScenarioTests.java
@@ -45,6 +45,7 @@ public class ShopShouldNotHasAveragePurchaseScenarioTests {
         );
 //        调用搜索索引定时任务
         XXLJobUtils.triggerJob(new PropertiesUtils().getProperty(this.getClass(), "user.app.job.increment.shop.index.update.id"));
+        XXLJobUtils.triggerJob(new PropertiesUtils().getProperty(this.getClass(), "user.app.job.increment.shop.index.update.new.id"));
         Thread.sleep(15000);
 
     }
