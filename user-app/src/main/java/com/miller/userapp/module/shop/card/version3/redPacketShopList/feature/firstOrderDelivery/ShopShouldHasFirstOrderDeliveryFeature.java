d7a756a8fd5ff79d5c46442e4b583ad21ac84116
yancancan <yancancan@hungrypandagroup.com>
2026-01-06 20:09:15 +0800

    1.增加红包可使用商家列表用例

---
+++ user-app/src/main/java/com/miller/userapp/module/shop/card/version3/redPacketShopList/feature/firstOrderDelivery/ShopShouldHasFirstOrderDeliveryFeature.java
@@ -0,0 +1,87 @@
+package com.miller.userapp.module.shop.card.version3.redPacketShopList.feature.firstOrderDelivery;
+
+import com.hungrypanda.app.server.common.enums.index.shopFeature.ShopFeatureEnum;
+import com.hungrypanda.app.server.vo.index.ShopFeatureVO;
+import com.hungrypanda.app.server.vo.index.ShopIndexVO;
+import com.miller.common.util.MD5Util;
+import com.miller.service.framework.annotation.EnvTag;
+import com.miller.service.framework.annotation.Scenario;
+import com.miller.service.framework.cache.remote.redis.RedisService;
+import com.miller.service.framework.util.PropertiesUtils;
+import com.miller.userapp.mapper.search.ShopSearchMiddleMapper;
+import com.miller.userapp.module.home.login.flow.UserLoginFlow;
+import com.miller.userapp.module.home.login.request.UserLoginRequestDTO;
+import com.miller.userapp.module.shop.card.version3.redPacketShopList.flow.ShopListFlow;
+import com.miller.userapp.module.shop.card.version3.redPacketShopList.request.ShopListRequestDTO;
+import com.miller.userapp.module.shop.card.version3.redPacketShopList.response.ShopListResponseDTO;
+import com.miller.userapp.util.PandaTestDBHelpful;
+import com.miller.userapp.util.RedisUtils;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author panjuxiang
+ * @since 2024/8/28 17:54
+ */
+@Scenario(scenarioID = "01K4WC5P0M8Y3K88JD0RR00A65",
+        scenarioName = "普通店铺配送商卡-品类频道页-SKYX01_营销标_首单优先送_品类频道页-商卡二期：首单优先送",
+        author = "panjuxiang@hungrypandagroup.com", developmentTime = 30, maintenanceTime = 5, manualTestTime = 10)
+@EnvTag.Test
+@DisplayName("商卡(中文)")
+public class ShopShouldHasFirstOrderDeliveryFeature {
+    private final Long shopId = Long.parseLong(new PropertiesUtils().getProperty(this.getClass(), "user.app.for.test.shop.card.version2.shopId"));
+    private UserLoginRequestDTO userLoginRequestDTO ;
+    private RedisService redisInstance = RedisUtils.getRedisInstance();
+
+
+    @BeforeAll
+    void beforeAll() {
+        userLoginRequestDTO = new UserLoginRequestDTO();
+        userLoginRequestDTO.setAccount(new PropertiesUtils().getProperty(this.getClass(), "user.app.account.for.shop.card.version2.new.user02.account"));
+        userLoginRequestDTO.setPassword(MD5Util.string2MD5(new PropertiesUtils().getProperty(this.getClass(), "user.app.account.for.shop.card.version2.new.user02.password")));
+        userLoginRequestDTO.setDistinctId(new PropertiesUtils().getProperty(UserLoginFlow.class, "user.app.account.of.user002.account.distinctId"));
+        userLoginRequestDTO.setType(Integer.valueOf(new PropertiesUtils().getProperty(UserLoginFlow.class, "user.app.account.of.public.login.type")));
+        userLoginRequestDTO.setAreaCode(new PropertiesUtils().getProperty(UserLoginFlow.class, "user.app.account.of.user002.account.callingCode"));
+
+        UserLoginFlow.loginAndPutToken(userLoginRequestDTO);
+        PandaTestDBHelpful.executeInsertOrUpdateOrDelete("update hp_sys_app_config set config_value = 'all' where config_key='FIRST_ORDER_DELIVERY_CONFIG'");
+        redisInstance.set("FIRST_ORDER_DELIVERY_CONFIG","all");
+    }
+
+    @MethodSource("staticDataProvider")
+    @ParameterizedTest
+    @DisplayName("普通店铺配送商卡-品类频道页-SKYX01_营销标_首单优先送_品类频道页-商卡二期：首单优先送")
+    void shouldExistFirstOrderDeliveryFeature(ShopListRequestDTO shopListRequestDTO) {
+
+        ShopListResponseDTO shopList = ShopListFlow.getShopListByShopId(shopListRequestDTO,shopId);
+        ShopIndexVO shopIndexVO = shopList.getResult().getShopList().stream()
+                .filter(item -> item.getShopId().equals(shopId)).findFirst().get();
+
+        ShopFeatureVO shopFeatureVO = shopIndexVO.getShopFeatureList().stream().filter(item -> item.getType().equals(ShopFeatureEnum.FIRST_ORDER_DELIVERY.getType())).findFirst().orElse(null);
+
+
+        assertThat(shopIndexVO.getFirstOrderDelivery()).isEqualTo(1);
+        assertThat(shopFeatureVO.getShowContent()).isEqualTo(ShopFeatureEnum.FIRST_ORDER_DELIVERY.getName());
+    }
+
+
+    /**
+     * 测试用例数据提供者
+     */
+    static Stream<Arguments> staticDataProvider() {
+        ShopListRequestDTO shopListRequestDTO = new ShopListRequestDTO();
+        // 可以不用传参数
+        shopListRequestDTO.setCityName("九江市");// 开发代码Bug，没有对 null 进行判断，应该默认给false的
+        shopListRequestDTO.setShopCategoryIds("[3896,3914,5486]");
+
+        return Stream.of(Arguments.of(shopListRequestDTO));
+    }
+}
---
+++ user-app/src/main/java/com/miller/userapp/module/shop/card/version3/redPacketShopList/feature/highSale/ShopShouldHasNoHighSaleFeature.java
@@ -0,0 +1,77 @@
+package com.miller.userapp.module.shop.card.version3.redPacketShopList.feature.highSale;
+
+import com.hungrypanda.app.server.common.enums.index.shopFeature.ShopFeatureEnum;
+import com.hungrypanda.app.server.vo.index.ShopFeatureVO;
+import com.hungrypanda.app.server.vo.index.ShopIndexVO;
+import com.miller.service.framework.annotation.EnvTag;
+import com.miller.service.framework.annotation.Scenario;
+import com.miller.service.framework.util.PropertiesUtils;
+import com.miller.userapp.module.home.login.flow.UserLoginFlow;
+import com.miller.userapp.module.shop.card.version3.redPacketShopList.flow.ShopListFlow;
+import com.miller.userapp.module.shop.card.version3.redPacketShopList.request.ShopListRequestDTO;
+import com.miller.userapp.module.shop.card.version3.redPacketShopList.response.ShopListResponseDTO;
+import com.miller.userapp.util.PandaTestDBHelpful;
+import com.miller.userapp.util.RedisUtils;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Scenario(scenarioID = "01K4WC5J2Z9JCT299V571DCA1Z",
+        scenarioName = "普通店铺配送商卡-品类频道页-SKYX01_营销标_人气销量标签_不满足条件时，不返回：高月售人气门店",
+        author = "panjuxiang@hungrypandagroup.com", developmentTime = 30, maintenanceTime = 10, manualTestTime = 10)
+@EnvTag.Test
+@DisplayName("商卡(中文)")
+public class ShopShouldHasNoHighSaleFeature {
+
+    private final Long shopId = Long.parseLong(new PropertiesUtils().getProperty(this.getClass(), "user.app.for.test.shop.card.version2.02.shopId"));
+
+
+    @BeforeAll
+    void beforeAll() {
+        UserLoginFlow.loginByDefaultUser();
+    }
+
+    @MethodSource("staticDataProvider")
+    @ParameterizedTest
+    @DisplayName("普通店铺配送商卡-品类频道页-SKYX01_营销标_人气销量标签_不满足条件时，不返回：高月售人气门店")
+    void shouldExistEvaluationFeature(ShopListRequestDTO shopListRequestDTO) {
+
+        ShopListResponseDTO shopList = ShopListFlow.getShopListByShopId(shopListRequestDTO, shopId);
+        ShopIndexVO shopIndexVO = shopList.getResult().getShopList().stream()
+                .filter(item -> item.getShopId().equals(shopId)).findFirst().get();
+
+        ShopFeatureVO shopFeatureVO = shopIndexVO.getShopFeatureList().stream().
+                filter(item -> item.getType().equals(ShopFeatureEnum.POPULAR_STORE.getType())).findFirst().orElse(null);
+
+
+        List<Map<String, Object>> maps = PandaTestDBHelpful.executeSelectListSql("SELECT monthly_sales FROM hp_data_shop_home_recommend_label where shop_id = " + shopId);
+        Map<String, Object> map = maps.get(0);
+
+        int cardMonthSaleNum = Integer.parseInt(RedisUtils.getRedisInstance().get("CARD_MONTH_SALE_NUM").toString());
+        assertThat((int) map.get("monthly_sales") < cardMonthSaleNum).isEqualTo(true);
+        assertThat(shopFeatureVO).isEqualTo(null);
+    }
+
+
+    /**
+     * 测试用例数据提供者
+     */
+    static Stream<Arguments> staticDataProvider() {
+        ShopListRequestDTO shopListRequestDTO = new ShopListRequestDTO();
+        // 可以不用传参数
+        shopListRequestDTO.setCityName("九江市");// 开发代码Bug，没有对 null 进行判断，应该默认给false的
+        shopListRequestDTO.setShopCategoryIds("[3896,3914,5486]");
+
+        return Stream.of(Arguments.of(shopListRequestDTO));
+    }
+
+}
+
