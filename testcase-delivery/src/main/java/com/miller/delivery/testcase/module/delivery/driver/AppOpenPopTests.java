b4f3fc1998c34ef99e2fc2a22e38dba764c12378
panjuxiang <panjuxiang@hungrypandagroup.com>
2026-01-08 15:36:02 +0800

    D侧自动化迁移-骑手APP

---
+++ testcase-delivery/src/main/java/com/miller/delivery/testcase/module/delivery/driver/AppOpenPopTests.java
@@ -0,0 +1,75 @@
+package com.miller.delivery.testcase.module.delivery.driver;
+
+import com.miller.delivery.testcase.config.TestcaseConfig;
+import com.miller.delivery.testcase.utils.TestCaseHelpful;
+import com.miller.service.framework.annotation.Scenario;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 打开app是否需要弹窗
+ *
+ * @author chenchunxia@hungrypandagroup.com
+ * @version 2.0
+ * @since 2025/01/06
+ */
+@Scenario(
+        scenarioID = "01JW69Y823Y0ZF2XFE5K6R7A24",
+        scenarioName = "骑手app-打开app是否需要弹窗",
+        author = "chenchunxia@hungrypandagroup.com",
+        developmentTime = 10, maintenanceTime = 0, manualTestTime = 5)
+@DisplayName("打开app是否需要弹窗")
+public class AppOpenPopTests {
+
+    @DisplayName("打开app是否需要弹窗")
+    @Test
+    void shouldCheckAppOpenPop() {
+        // 1) 骑手登录获取 token
+        String driverAccessToken = TestCaseHelpful.deliveryLogin("13300010015", "Test1234");
+
+        // 2) 打开app是否需要弹窗
+        String uri = TestcaseConfig.HOST_DELIVERY_APP + "/api/delivery/app/appInfo/appOpenPop";
+        String method = "POST";
+        Map<String, Object> headers = createDriverAppHeaders();
+        headers.put("authorization", driverAccessToken);
+        
+        String body = "{}";
+        var responseBody = TestCaseHelpful.sendRequest(method, uri, null, headers, body);
+
+        // 3) 断言
+        TestCaseHelpful.assertThatJson(responseBody)
+                .node("resultCode").isEqualTo(1000);
+        TestCaseHelpful.assertThatJson(responseBody)
+                .node("reason").isEqualTo("成功");
+        TestCaseHelpful.assertThatJson(responseBody)
+                .node("success").isEqualTo(true);
+    }
+
+    /**
+     * 创建骑手app请求头（IOS平台）
+     */
+    private Map<String, Object> createDriverAppHeaders() {
+        Map<String, Object> headers = new HashMap<>();
+        headers.put("operatingsystem", "2");
+        headers.put("brand", "iPhone 11");
+        headers.put("latitude", "30.203602");
+        headers.put("locationstatus", "2");
+        headers.put("locale", "zh-CN");
+        headers.put("version", "5.64.0");
+        headers.put("countrycode", "CN");
+        headers.put("platform", "IOS");
+        headers.put("uniquetoken", "9A95A874-6493-4DFC-A5E1-BCE3C7C265D0");
+        headers.put("longitude", "120.216758");
+        headers.put("accept-language", "zh-Hans;q=1");
+        headers.put("devicesafetoken", "a0_b0_c0_h0_i0_j0_m0_n0_p0_s0");
+        headers.put("apptypeid", "2");
+        headers.put("accept", "*/*");
+        headers.put("enableSign", "false");
+        headers.put("content-type", "application/json");
+        return headers;
+    }
+}
+
