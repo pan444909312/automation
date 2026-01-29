db7e782e2c41a1e6fed738f2fcda75275de967bf
chenchunxia <chenchunxia@hungrypandagroup.com>
2026-01-29 19:12:56 +0800

    调试接口

--- testcase-delivery/src/main/java/com/miller/delivery/testcase/module/delivery/driver/OrderRemarkTranslateTests.java
+++ testcase-delivery/src/main/java/com/miller/delivery/testcase/module/delivery/driver/OrderRemarkTranslateTests.java
@@ -3,6 +3,7 @@ package com.miller.delivery.testcase.module.delivery.driver;
 import com.miller.delivery.testcase.config.TestcaseConfig;
 import com.miller.delivery.testcase.module.deliveryUtils.order.CreateInstantOrderWithHandoverTests;
 import com.miller.delivery.testcase.utils.TestCaseHelpful;
+import com.miller.delivery.testcase.utils.driverOffline;
 import com.miller.service.framework.annotation.Scenario;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -36,7 +37,9 @@ public class OrderRemarkTranslateTests {
         
         // ========== 第二部分：骑手操作流程 ==========
         // 步骤7: 骑手app-骑手登录
-        String driverAccessToken = TestCaseHelpful.deliveryLogin("13300010015", "Test1234");
+        String driverAccessToken = TestCaseHelpful.deliveryLogin("13300010676", "Test1234");
+        driverOffline driverOffline = new driverOffline();
+        driverOffline.cancelDispatchAndOffline("13300010676",driverAccessToken);
         
         // 步骤8: 骑手app-司机上线操作
         driverOnline(driverAccessToken);
