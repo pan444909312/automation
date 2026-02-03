bd1fcda9b8189a9d2e75cde3dbf5ae81de11170e
chenchunxia <chenchunxia@hungrypandagroup.com>
2026-02-03 15:25:49 +0800

    调试场景

--- testcase-delivery/src/main/java/com/miller/delivery/testcase/module/deliveryAdmin/driverManagement/station/StationDisableTests.java
+++ testcase-delivery/src/main/java/com/miller/delivery/testcase/module/deliveryAdmin/driverManagement/station/StationDisableTests.java
@@ -13,26 +13,36 @@ import java.util.Map;
  * 司管后台-骑手列表-禁用骑手驿站
  */
 @Scenario(
-        scenarioID = "01KDSTRHPEE7N52SW9M3ZC6Q6C",
+        scenarioID = "01KGH4WWW5KZZZE8901C6EAW4S",
         scenarioName = "骑手列表-禁用骑手驿站",
         author = "TestingConsultant@hungrypandagroup.com",
         developmentTime = 120, maintenanceTime = 0, manualTestTime = 5)
 @DisplayName("禁用骑手驿站")
 public class StationDisableTests {
 
-    private static final String CONFIG_NO = "123456"; // 假设一个configNo
 
-    @DisplayName("禁用驿站")
+
+    @DisplayName("禁用、启用驿站")
     @Test
     void shouldDisableStation() {
+
+
+        String ConfigNo="504646250062430432";
+        // 2) 开启驿站
+        disableStation(ConfigNo,1);
+        // 2) 禁用驿站
+        disableStation(ConfigNo,0);
+    }
+    private void disableStation(String ConfigNo,int configStatus) {
         // 1) 司管登录获取 token
         String token = erpLogin();
+
 
         // 2) 禁用驿站
         String uri = TestcaseConfig.HOST_ERP + "/api/deliveryAdmin/station/configStatus";
         String method = "POST";
         Map<String, Object> headers = createHeaders(token);
-        String body = "{\"configNo\":\"" + CONFIG_NO + "\",\"configStatus\":0}";
+        String body = "{\"configNo\":\"" + ConfigNo + "\",\"configStatus\":0}";
         var responseBody = TestCaseHelpful.sendRequest(method, uri, null, headers, body);
 
         // 3) 断言禁用成功
@@ -54,6 +64,30 @@ public class StationDisableTests {
         headers.put("User-Agent", "Apifox/1.0.0 (https://apifox.com)");
         headers.put("content-type", "application/json;charset=UTF-8");
         return headers;
+    }
+    private String getConfigNo(){
+        String token = erpLogin();
+
+
+        // 2) 获取骑手驿站列表
+        String uri = TestcaseConfig.HOST_ERP + "/api/deliveryAdmin/station/list";
+        String method = "POST";
+        Map<String, Object> headers = createHeaders(token);
+        String body = "{\n" +
+                "    \"cityList\": [\n" +
+                "        \"日照市\"\n" +
+                "    ],\n" +
+                "    \"configStatus\": 1,\n" +
+                "    \"pageNo\": 1,\n" +
+                "    \"pageSize\": 15\n" +
+                "}";
+
+        var responseBody = TestCaseHelpful.sendRequest(method, uri, null, headers, body);
+        TestCaseHelpful.assertThatJson(responseBody).node("code").isEqualTo(1);
+        TestCaseHelpful.assertThatJson(responseBody).node("message").isEqualTo("成功");
+
+        String configno = TestCaseHelpful.extractValue(responseBody, "$.data.list.[0].configNo").toString();
+        return configno;
     }
 }
 
