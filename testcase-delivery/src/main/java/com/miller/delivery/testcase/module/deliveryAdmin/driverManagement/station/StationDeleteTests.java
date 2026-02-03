cbb64820795d9a8594729a058550b1d95bd63765
chenchunxia <chenchunxia@hungrypandagroup.com>
2026-02-03 15:49:41 +0800

    调试场景

--- testcase-delivery/src/main/java/com/miller/delivery/testcase/module/deliveryAdmin/driverManagement/station/StationDeleteTests.java
+++ testcase-delivery/src/main/java/com/miller/delivery/testcase/module/deliveryAdmin/driverManagement/station/StationDeleteTests.java
@@ -3,6 +3,7 @@ package com.miller.delivery.testcase.module.deliveryAdmin.driverManagement.stati
 import com.miller.delivery.testcase.config.TestcaseConfig;
 import com.miller.delivery.testcase.utils.PandaTestDBHelpful;
 import com.miller.delivery.testcase.utils.TestCaseHelpful;
+import com.miller.service.framework.annotation.Scenario;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -13,34 +14,50 @@ import java.util.Map;
 /**
  * 司管后台-骑手列表-删除骑手驿站
  */
-//@Scenario(
-//        scenarioID = "01KG4KA0XGPF86Z4B75BE5JAQP",
-//        scenarioName = "骑手列表-删除骑手驿站",
-//        author = "TestingConsultant@hungrypandagroup.com",
-//        developmentTime = 120, maintenanceTime = 0, manualTestTime = 5)
-//@DisplayName("删除骑手驿站")
+@Scenario(
+        scenarioID = "01KG4KA0XGPF86Z4B75BE5JAQP",
+        scenarioName = "骑手列表-删除骑手驿站",
+        author = "TestingConsultant@hungrypandagroup.com",
+        developmentTime = 120, maintenanceTime = 0, manualTestTime = 5)
+@DisplayName("删除骑手驿站")
 public class StationDeleteTests {
 
-    //private static final String CONFIG_NO = "123456"; // 假设一个configNo
 
     @DisplayName("删除骑手驿站")
     @Test
     void shouldDeleteStation() {
         // 1) 司管登录获取 token
         String token = erpLogin();
-
-        // 前置数据
-        String Station_CONFIG_No_Str = getStaionData("奥克兰");
-        Long ETA_CONFStation_CONFIG_No = Long.parseLong(Station_CONFIG_No_Str);
+        String ConfigNo = getStaionData();
+        // 2) 禁用驿站
+        disableStation(ConfigNo,0);
 
         // 2) 删除骑手驿站
         String uri = TestcaseConfig.HOST_ERP + "/api/deliveryAdmin/station/del";
         String method = "POST";
         Map<String, Object> headers = createHeaders(token);
-        String body = "{\"configNo\":\"" + ETA_CONFStation_CONFIG_No + "\"}";
+        String body = "{\"configNo\":\"" + ConfigNo + "\"}";
         var responseBody = TestCaseHelpful.sendRequest(method, uri, null, headers, body);
 
         // 3) 断言删除成功
+        TestCaseHelpful.assertThatJson(responseBody)
+                .node("code").isEqualTo(1);
+        TestCaseHelpful.assertThatJson(responseBody)
+                .node("message").isEqualTo("成功");
+    }
+    private void disableStation(String ConfigNo,int configStatus) {
+        // 1) 司管登录获取 token
+        String token = erpLogin();
+
+
+        // 2) 禁用驿站
+        String uri = TestcaseConfig.HOST_ERP + "/api/deliveryAdmin/station/configStatus";
+        String method = "POST";
+        Map<String, Object> headers = createHeaders(token);
+        String body = "{\"configNo\":\"" + ConfigNo + "\",\"configStatus\":0}";
+        var responseBody = TestCaseHelpful.sendRequest(method, uri, null, headers, body);
+
+        // 3) 断言禁用成功
         TestCaseHelpful.assertThatJson(responseBody)
                 .node("code").isEqualTo(1);
         TestCaseHelpful.assertThatJson(responseBody)
@@ -51,17 +68,21 @@ public class StationDeleteTests {
         return TestCaseHelpful.erpLogin();
     }
 
-    private String getStaionData(String city) {
-        Object stationConfigNo = null;
-        String sql1 = String.format("select config_no from hp_delivery_driver_station where city = \"%s\" and is_del = 0 order by id desc limit 1;", city);
-        List<Map<String, Object>> resultList = PandaTestDBHelpful.executeSelectListSql(sql1);
-        String sql2 = String.format("update hp_delivery_driver_station set is_del = 0, config_status = 0 where config_no = %s;", stationConfigNo);
-        PandaTestDBHelpful.executeInsertOrUpdateOrDelete(sql2);
-        if (resultList != null && !resultList.isEmpty()) {
-            stationConfigNo = resultList.get(0).get("config_no");
-            return stationConfigNo != null ? stationConfigNo.toString() : "121";
-            }
-        return "121";
+    private String getStaionData() {
+        // 1) 司管登录获取 token
+        String token = erpLogin();
+
+        // 2) 新增骑手驿站
+        String uri = TestcaseConfig.HOST_ERP + "/api/deliveryAdmin/station/add";
+        String method = "POST";
+        Map<String, Object> headers = createHeaders(token);
+        String body = "{\"serviceTypeList\":[10],\"city\":\"奥克兰\",\"stationNameCn\":\"测试自动化\",\"stationAddress\":\"奥克兰主城区-自动化测试地址\"}";
+        var responseBody = TestCaseHelpful.sendRequest(method, uri, null, headers, body);
+
+        Map<String, Object> demandRecord = PandaTestDBHelpful.executeSelectOneSql(
+                "select * from panda_test.hp_delivery_driver_station where station_name_cn='测试自动化' order by id desc limit 1");
+        String configId = (demandRecord.get("config_no")).toString();
+        return configId;
     }
 
     private Map<String, Object> createHeaders(String token) {
