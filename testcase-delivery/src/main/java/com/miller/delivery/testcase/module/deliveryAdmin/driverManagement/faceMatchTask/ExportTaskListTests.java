9b493b4b9238b37a734d4db66b925a739c6506ac
LuLu <TestingConsultant@hungrypandagroup.com>
2026-02-27 19:10:37 +0800

    新增4个人脸检测相关接口

---
+++ testcase-delivery/src/main/java/com/miller/delivery/testcase/module/deliveryAdmin/driverManagement/faceMatchTask/ExportTaskListTests.java
@@ -0,0 +1,53 @@
+package com.miller.delivery.testcase.module.deliveryAdmin.driverManagement.faceMatchTask;
+
+import com.miller.delivery.testcase.config.TestcaseConfig;
+import com.miller.delivery.testcase.utils.TestCaseHelpful;
+import com.miller.service.framework.annotation.Scenario;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static com.miller.delivery.testcase.utils.DeliveryTestCaseUtils.createErpHeaders;
+import static com.miller.delivery.testcase.utils.TestCaseHelpful.erpLogin;
+
+/**
+ * 司管后台-骑手管理-人脸检测记录-导出人脸检测记录
+ */
+@Scenario(
+        scenarioID = "01KJF44C29GQVZ8D3Q7KZ4QEEE",
+        scenarioName = "导出人脸检测记录",
+        author = "TestingConsultant@hungrypandagroup.com",
+        developmentTime = 60, maintenanceTime = 0, manualTestTime = 5)
+@DisplayName("导出人脸检测记录")
+public class ExportTaskListTests {
+
+    @DisplayName("导出人脸检测记录")
+    @Test
+    void shouldAddDriverGroup() {
+        // 1) 司管登录获取 token
+        String token = erpLogin();
+        list(token);
+    }
+    //
+    public  void  list(String token) {
+
+        String uri = TestcaseConfig.HOST_ERP + "/api/deliveryAdmin/faceMatchTask/exportTaskList";
+        String method = "POST";
+        Map<String, Object> headers = createErpHeaders();
+        headers.put("authorization", token);
+        String body = "{\n" +
+                "    \"checkTimeStart\": \"2026-01-29\",\n" +
+                "    \"checkTimeEnd\": \"2026-02-27\"\n" +
+                "}";
+
+        var responseBody = TestCaseHelpful.sendRequest(method, uri, null, headers, body);
+        // 断言
+        TestCaseHelpful.assertThatJson(responseBody)
+                .node("code").isEqualTo(1);
+        TestCaseHelpful.assertThatJson(responseBody)
+                .node("message").isEqualTo("成功");
+
+    }
+
+}
---
+++ testcase-delivery/src/main/java/com/miller/delivery/testcase/module/deliveryAdmin/driverManagement/faceMatchTask/FaceMatchTaskDetailBaseUrlTests.java
@@ -0,0 +1,52 @@
+package com.miller.delivery.testcase.module.deliveryAdmin.driverManagement.faceMatchTask;
+
+import com.miller.delivery.testcase.config.TestcaseConfig;
+import com.miller.delivery.testcase.utils.TestCaseHelpful;
+import com.miller.service.framework.annotation.Scenario;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static com.miller.delivery.testcase.utils.DeliveryTestCaseUtils.createErpHeaders;
+import static com.miller.delivery.testcase.utils.TestCaseHelpful.erpLogin;
+
+/**
+ * 司管后台-骑手管理-人脸检测配置-查看基准照片
+ */
+@Scenario(
+        scenarioID = "01KJF8HQ3QME77JS0HPPE2FVXT",
+        scenarioName = "人脸检测记录查看基准照片",
+        author = "TestingConsultant@hungrypandagroup.com",
+        developmentTime = 60, maintenanceTime = 0, manualTestTime = 5)
+@DisplayName("人脸检测记录查看基准照片")
+public class FaceMatchTaskDetailBaseUrlTests {
+
+    @DisplayName("人脸检测记录查看基准照片")
+    @Test
+    void shouldAddDriverGroup() {
+        // 1) 司管登录获取 token
+        String token = erpLogin();
+        list(token);
+    }
+    //
+    public  void  list(String token) {
+
+        String uri = TestcaseConfig.HOST_ERP + "/api/deliveryAdmin/faceMatchTask/detailBaseUrl";
+        String method = "POST";
+        Map<String, Object> headers = createErpHeaders();
+        headers.put("authorization", token);
+        String body = "{\n" +
+                "    \"taskNo\": \"506824717099649824\"\n" +
+                "}";
+
+        var responseBody = TestCaseHelpful.sendRequest(method, uri, null, headers, body);
+        // 断言
+        TestCaseHelpful.assertThatJson(responseBody)
+                .node("code").isEqualTo(1);
+        TestCaseHelpful.assertThatJson(responseBody)
+                .node("message").isEqualTo("成功");
+
+    }
+
+}
