5f6f0c2005b02a5e9c9eb75613bcda4fd0b6e836
LuLu <TestingConsultant@hungrypandagroup.com>
2026-02-28 18:42:05 +0800

    城市地图网格标签获取

---
+++ testcase-delivery/src/main/java/com/miller/delivery/testcase/module/deliveryAdmin/DeliveryAreaSettings/city/MapPoint/MapPointTagConfigTests.java
@@ -0,0 +1,48 @@
+package com.miller.delivery.testcase.module.deliveryAdmin.DeliveryAreaSettings.city.MapPoint;
+
+import com.miller.delivery.testcase.config.TestcaseConfig;
+import com.miller.delivery.testcase.utils.TestCaseHelpful;
+import com.miller.service.framework.annotation.Scenario;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static com.miller.delivery.testcase.utils.DeliveryTestCaseUtils.createErpHeaders;
+import static com.miller.delivery.testcase.utils.TestCaseHelpful.erpLogin;
+
+/**
+ * 司管后台-城市区域配置-城市-城市地图网格标签获取
+ */
+@Scenario(
+        scenarioID = "01KJHXBVXKRPB3FF9TQZWHY09D",
+        scenarioName = "城市地图网格标签获取",
+        author = "TestingConsultant@hungrypandagroup.com",
+        developmentTime = 60, maintenanceTime = 0, manualTestTime = 5)
+@DisplayName("城市地图网格标签获取")
+public class MapPointTagConfigTests {
+    @DisplayName("城市地图网格标签获取")
+    @Test
+    void shouldAddDriverGroup() {
+        // 1) 司管登录获取 token
+        String token = erpLogin();
+        mapPointTagConfig(token);
+    }
+    //获取城市网格标签
+    public  void  mapPointTagConfig(String token) {
+
+        String uri = TestcaseConfig.HOST_ERP + "/api/deliveryAdmin/mapPoint/tag/config";
+        String method = "POST";
+        Map<String, Object> headers = createErpHeaders();
+        headers.put("authorization", token);
+        String body = "{}";
+        var responseBody = TestCaseHelpful.sendRequest(method, uri, null, headers, body);
+        // 断言
+        TestCaseHelpful.assertThatJson(responseBody)
+                .node("code").isEqualTo(1);
+        TestCaseHelpful.assertThatJson(responseBody)
+                .node("message").isEqualTo("成功");
+
+    }
+
+}
