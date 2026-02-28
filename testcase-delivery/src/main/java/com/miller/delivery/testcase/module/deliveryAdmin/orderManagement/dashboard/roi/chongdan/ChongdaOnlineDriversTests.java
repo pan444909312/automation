e6cb0c997068d73f4a2aa51dae7043bfcd79e7c4
chenchunxia <chenchunxia@hungrypandagroup.com>
2026-02-28 16:47:20 +0800

    增加场景

---
+++ testcase-delivery/src/main/java/com/miller/delivery/testcase/module/deliveryAdmin/orderManagement/dashboard/roi/chongdan/ChongdaOnlineDriversTests.java
@@ -0,0 +1,65 @@
+package com.miller.delivery.testcase.module.deliveryAdmin.orderManagement.dashboard.roi.chongdan;
+
+import com.miller.delivery.testcase.config.TestcaseConfig;
+import com.miller.delivery.testcase.utils.PandaTestDBHelpful;
+import com.miller.delivery.testcase.utils.TestCaseHelpful;
+import com.miller.service.framework.annotation.Scenario;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static com.miller.delivery.testcase.utils.DeliveryTestCaseUtils.createErpHeaders;
+import static com.miller.delivery.testcase.utils.TestCaseHelpful.erpLogin;
+
+/**
+ * 司管后台-新增骑手禁止上线配置
+ */
+@Scenario(
+        scenarioID = "01KJHNG2YSVQ8KH8QR5DA148SD",
+        scenarioName = "司管后台-roi-冲单-活动群组在线司机数",
+        author = "chenchunxia@hungrypandagroup.com",
+        developmentTime = 120, maintenanceTime = 0, manualTestTime = 5)
+@DisplayName("司管后台-roi-冲单-活动群组在线司机数")
+public class ChongdaOnlineDriversTests {
+
+    @DisplayName("司管后台-roi-冲单-活动群组在线司机数")
+    @Test
+    void shouldAddDriverGroup() {
+        // 1) 司管登录获取 token
+        String token = erpLogin();
+        capacityAreaInfo(token);
+
+
+    }
+
+    public void capacityAreaInfo(String token) {
+
+
+        String uri = TestcaseConfig.HOST_ERP + "/api/deliveryDashboard/activity/roi/onlineDrivers";
+        String method = "POST";
+        Map<String, Object> headers = createErpHeaders();
+        //数据库中查询
+        Map<String, Object> roiResult = PandaTestDBHelpful.executeSelectOneSql(
+                String.format("select * from panda_delivery_dashboard_test.hp_delivery_activity_roi where activity_type=2 and city='杭州市' order by id desc limit 1"));
+        String ROIid = String.valueOf(roiResult.get("id"));
+        headers.put("authorization", token);
+        String body = "{\n" +
+                "    \"id\": "+ROIid+",\n" +
+                "    \"activityType\": 2,\n" +
+                "    \"pageNo\": 1,\n" +
+                "    \"pageSize\": 10\n" +
+                "}";
+
+        var responseBody = TestCaseHelpful.sendRequest(method, uri, null, headers, body);
+
+
+
+        TestCaseHelpful.assertThatJson(responseBody).node("code").isEqualTo(1);
+        TestCaseHelpful.assertThatJson(responseBody).node("message").isEqualTo("成功");
+
+
+    }
+
+}
+
---
+++ testcase-delivery/src/main/java/com/miller/delivery/testcase/module/deliveryAdmin/orderManagement/dashboard/roi/chongdan/ChongdaOpenAppOnlineDriversImportTests.java
@@ -0,0 +1,60 @@
+package com.miller.delivery.testcase.module.deliveryAdmin.orderManagement.dashboard.roi.chongdan;
+
+import com.miller.delivery.testcase.config.TestcaseConfig;
+import com.miller.delivery.testcase.utils.PandaTestDBHelpful;
+import com.miller.delivery.testcase.utils.TestCaseHelpful;
+import com.miller.service.framework.annotation.Scenario;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static com.miller.delivery.testcase.utils.DeliveryTestCaseUtils.createErpHeaders;
+import static com.miller.delivery.testcase.utils.TestCaseHelpful.erpLogin;
+
+/**
+ * 司管后台-新增骑手禁止上线配置
+ */
+@Scenario(
+        scenarioID = "01KJHKZVZRM0J05QHVVJEMQZJA",
+        scenarioName = "司管后台-roi-冲单-打开APP上线司机数-导出",
+        author = "chenchunxia@hungrypandagroup.com",
+        developmentTime = 60, maintenanceTime = 0, manualTestTime = 5)
+@DisplayName("司管后台-roi-冲单-打开APP上线司机数-导出")
+public class ChongdaOpenAppOnlineDriversImportTests {
+
+    @DisplayName("司管后台-roi-冲单-打开APP上线司机数-导出")
+    @Test
+    void shouldAddDriverGroup() {
+        // 1) 司管登录获取 token
+        String token = erpLogin();
+        capacityAreaInfo(token);
+
+
+    }
+
+    public void capacityAreaInfo(String token) {
+
+
+        String uri = TestcaseConfig.HOST_ERP + "/api/deliveryDashboard/activity/roi/openAppOnlineDrivers/export";
+        String method = "POST";
+        Map<String, Object> headers = createErpHeaders();
+        //数据库中查询
+        Map<String, Object> roiResult = PandaTestDBHelpful.executeSelectOneSql(
+                String.format("select * from panda_delivery_dashboard_test.hp_delivery_activity_roi where activity_type=2 and city='杭州市' order by id desc limit 1"));
+        String ROIid = String.valueOf(roiResult.get("id"));
+        headers.put("authorization", token);
+        String body = "{\"id\":"+ROIid+",\"activityType\":2}";
+
+        var responseBody = TestCaseHelpful.sendRequest(method, uri, null, headers, body);
+
+
+
+        TestCaseHelpful.assertThatJson(responseBody).node("code").isEqualTo(1);
+        TestCaseHelpful.assertThatJson(responseBody).node("message").isEqualTo("成功");
+
+
+    }
+
+}
+
