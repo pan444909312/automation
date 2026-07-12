2d1c8de31ab8be9df55026346ea713967662c179
pjx <444909312@qq.com>
2026-07-12 15:48:02 +0800

    删除无用模块

--- merchant/src/main/java/com/miller/merchant/summi.login/request/MerchantLoginRequestDTO.java
+++
@@ -1,9 +0,0 @@
-package com.miller.merchant.summi.login.request;
-
-import com.hungrypanda.voucher.api.req.BaseMerchantReq;
-import com.miller.merchant.dto.BasicResponseDTO;
-import com.panda.merchant.server.api.vo.app.merchant.req.AppMerchantVirtualLoginRespVO;
-import com.panda.merchant.server.api.vo.webApp.BaseMerchantLoginReqVO;
-
-public class MerchantLoginRequestDTO extends BaseMerchantLoginReqVO {
-}
